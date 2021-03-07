8fd6ae6fd7b1e97e3e89afd4106f7090876a5029
René T. Nielsen <renetdev@gmail.com>
2021-03-07 23:12:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# YOUR DOMAIN README
+
+This should be replaced by content that fits your domain.
+
+Before you start editing this repository, please read the [TEMPLATE README](./docs/template-readme/README.md)
---
+++ source/hello-world/source/HelloWorld.HelloWorldFunction/HelloWorldHttpTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HelloWorld.HelloWorldFunction
+{
+    public static class HelloWorldHttpTrigger
+    {
+        [FunctionName("HelloWorldHttpTrigger")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            if (req == null) throw new InvalidOperationException();
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string");
+        }
+    }
+}
