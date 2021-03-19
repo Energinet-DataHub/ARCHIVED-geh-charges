5395021677cc6066ca97d27fb7afd76558d9d60e
LasseRinggrenNielsen <lrn@energinet.dk>
2021-03-19 14:27:02 +0100

    added ChargeHttpTrigger to the solution

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace GreenEnergyHub.Charges.MessageReceiver
+{
+    public static class ChargeHttpTrigger
+    {
+        [FunctionName(nameof(ChargeHttpTrigger))]
+        public static Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            [NotNull] HttpRequest req)
+        {
+            return Task.FromResult((IActionResult)new OkObjectResult(req.Body));
+        }
+    }
+}
