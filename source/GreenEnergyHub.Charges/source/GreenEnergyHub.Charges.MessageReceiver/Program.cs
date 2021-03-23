f8db4f77775ea8520708c0966b346d50bf7252f3
Lasse Ringgren Nielsen <lrn@energinet.dk>
2021-03-23 13:00:12 +0100

    added new MessageReveiver project with .NET isolated function

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace GreenEnergyHub.Charges.MessageReceiver
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
