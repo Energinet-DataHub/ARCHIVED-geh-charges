a0b38bf055625888fd70aa4ee905de42cc294470
Lasse Ringgren Nielsen <lrn@energinet.dk>
2021-03-23 14:20:45 +0100

    changed welcome message from the ChargeMessageHttpTrigger

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
@@ -18,7 +18,7 @@ namespace GreenEnergyHub.Charges.MessageReceiver
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString($"Welcome to {nameof(ChargeHttpTrigger)}");
 
             return response;
         }
