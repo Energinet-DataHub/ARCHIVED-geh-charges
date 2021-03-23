6451c2bad3000e2087a88500484980110ee420ad
Lasse Ringgren Nielsen <lrn@energinet.dk>
2021-03-23 13:29:47 +0100

    test with removeing the HttpTrigger on the AZ func

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
@@ -7,20 +7,20 @@ namespace GreenEnergyHub.Charges.MessageReceiver
 {
     public static class ChargeHttpTrigger
     {
-        [Function("ChargeHttpTrigger")]
-        public static HttpResponseData Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            var logger = executionContext.GetLogger("ChargeHttpTrigger");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
-        }
+        // [Function("ChargeHttpTrigger")]
+        // public static HttpResponseData Run(
+        //     [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        //     FunctionContext executionContext)
+        // {
+        //     var logger = executionContext.GetLogger("ChargeHttpTrigger");
+        //     logger.LogInformation("C# HTTP trigger function processed a request.");
+        //
+        //     var response = req.CreateResponse(HttpStatusCode.OK);
+        //     response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        //
+        //     response.WriteString("Welcome to Azure Functions!");
+        //
+        //     return response;
+        // }
     }
 }
--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/Program.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker.Configuration;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 
 namespace GreenEnergyHub.Charges.MessageReceiver
 {
