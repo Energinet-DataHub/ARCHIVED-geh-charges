8a367965a3d9256109f4f72342cca7b38eb77296
Lasse Ringgren Nielsen <LRN@energinet.dk>
2021-03-23 14:59:43 +0100

    Setup ci / cd pipeline for ChargeHttpTrigger (#18)
    * added message-receiver-ci.yml
    
    * added custom run to message-receiver-ci.yml
    
    * removed comments from paths
    
    * bumped MessageReceivers SDK.Functions from 3.0.0 to 3.0.9
    
    * bumped AzureFunctionsVersions from V3 to V3-prerelease AND Microsoft.Azure.Functions.Extensions from 1.0.0 to 1.1.0
    
    * removed old MessageReceiver project from the Charges solution
    
    * added new MessageReveiver project with .NET isolated function
    
    * test with removeing the HttpTrigger on the AZ func
    
    * attempt at loading the azure core tools on message-receiver-ci.yml
    
    * removed wrong chars and whitespace from message-receiver-ci
    
    * moved azure core tools install to own action and used it in message-receiver-ci.yml
    
    * changed welcome message from the ChargeMessageHttpTrigger
    
    * removed the temp push trigger for our branch
    
    * added message-receiver-cd AND added path for Azure Core Tools to message-receiver-ci
    
    * removed custom run for the branch we are working on in message-receiver-cd.yml
    
    * removed trailing , from paths in message-receiver-ci.yml
    
    * changed ChargeHttpTriggers auth level to anon

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
@@ -1,34 +1,26 @@
-﻿// Copyright 2020 Energinet DataHub A/S
-//
-// Licensed under the Apache License, Version 2.0 (the "License2");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
 
 namespace GreenEnergyHub.Charges.MessageReceiver
 {
     public static class ChargeHttpTrigger
     {
-        [FunctionName(nameof(ChargeHttpTrigger))]
-        public static Task<IActionResult> RunAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
-            [NotNull] HttpRequest req)
+        [Function("ChargeHttpTrigger")]
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
         {
-            return Task.FromResult((IActionResult)new OkObjectResult(req.Body));
+            var logger = executionContext.GetLogger("ChargeHttpTrigger");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString($"Welcome to {nameof(ChargeHttpTrigger)}");
+
+            return response;
         }
     }
 }
