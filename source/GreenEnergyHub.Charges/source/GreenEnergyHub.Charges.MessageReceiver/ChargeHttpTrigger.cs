b84bbfd246639ef473cb9fefb71207710a794ba8
BjarkeMeier <hej@bjarkemeier.dk>
2021-03-23 15:51:29 +0100

    Finish merge of function code

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/ChargeHttpTrigger.cs
@@ -12,15 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
-using GreenEnergyHub.Charges.Application;
 using GreenEnergyHub.Charges.Application.ChangeOfCharges;
 using GreenEnergyHub.Charges.Domain.ChangeOfCharges.Message;
 using GreenEnergyHub.Charges.Domain.ChangeOfCharges.Transaction;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace GreenEnergyHub.Charges.MessageReceiver
@@ -28,33 +29,38 @@ namespace GreenEnergyHub.Charges.MessageReceiver
     public static class ChargeHttpTrigger
     {
         [Function("ChargeHttpTrigger")]
-        public static HttpResponseData Run(
+        public static async Task<HttpResponseData> RunAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
+            if (executionContext == null) throw new ArgumentNullException(nameof(executionContext));
+            if (req == null) throw new ArgumentNullException(nameof(req));
+
             var logger = executionContext.GetLogger("ChargeHttpTrigger");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            logger.LogInformation("C# HTTP trigger function processed a request");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString($"Welcome to {nameof(ChargeHttpTrigger)}");
+            var message = await GetChangeOfChargesMessageAsync(req).ConfigureAwait(false);
+
+            var changeOfChargesMessageHandler =
+                executionContext.InstanceServices.GetRequiredService<IChangeOfChargesMessageHandler>();
+            var status = await changeOfChargesMessageHandler.HandleAsync(message).ConfigureAwait(false);
+
+            // Future features/stories are expected to specify the expected output
+            await response.WriteAsJsonAsync(status).ConfigureAwait(false);
 
             return response;
-
-            // var message = await GetChangeOfChargesMessageAsync(req).ConfigureAwait(false);
-            //
-            // var status = await _changeOfChargesMessageHandler.HandleAsync(message).ConfigureAwait(false);
-            //
-            // // Future features/stories are expected to specify the expected output
-            // return new OkObjectResult(status);
         }
 
         private static async Task<ChangeOfChargesMessage> GetChangeOfChargesMessageAsync(HttpRequestData req)
         {
             // Mimic that we receive a message that can contain multiple transactions.
             // In upcoming stories we'll instead receive multi-transactions eBIX messages.
-            var transaction = await JsonSerializer.DeserializeAsync<ChangeOfChargesTransaction>(req.Body).ConfigureAwait(false);
+            var transaction = await JsonSerializer
+                .DeserializeAsync<ChangeOfChargesTransaction>(req.Body)
+                .ConfigureAwait(false);
             var message = new ChangeOfChargesMessage();
             message.Transactions.Add(transaction!);
             return message;
--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/Program.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageReceiver/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using GreenEnergyHub.Charges.Application.ChangeOfCharges;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace GreenEnergyHub.Charges.MessageReceiver
 {
@@ -8,9 +10,15 @@ namespace GreenEnergyHub.Charges.MessageReceiver
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(RegisterServices)
                 .Build();
 
             host.Run();
+        }
+
+        private static void RegisterServices(IServiceCollection collection)
+        {
+            collection.AddScoped<IChangeOfChargesMessageHandler, ChangeOfChargesMessageHandler>();
         }
     }
 }
