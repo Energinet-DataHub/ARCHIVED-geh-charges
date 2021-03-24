651dce38dfa5b6b8ec43eee2f6b5f4db930da26e
Lasse Ringgren Nielsen <LRN@energinet.dk>
2021-03-24 12:33:58 +0100

    Azfun charge.tf FUNTIONS_WORKER_RUNTIME changed to dotnet-isolated AND host.json version set back to 2.0 (#30)

--- build/infrastructure/azfun-charges-message-receiver.tf
+++ build/infrastructure/azfun-charges-message-receiver.tf
@@ -26,7 +26,7 @@ module "azfun_message_receiver" {
     WEBSITE_ENABLE_SYNC_UPDATE_SITE                   = true
     WEBSITE_RUN_FROM_PACKAGE                          = 1
     WEBSITES_ENABLE_APP_SERVICE_STORAGE               = true
-    FUNCTIONS_WORKER_RUNTIME                          = "dotnet"
+    FUNCTIONS_WORKER_RUNTIME                          = "dotnet-isolated"
   }
   dependencies                              = [
     module.appi.dependent_on,
