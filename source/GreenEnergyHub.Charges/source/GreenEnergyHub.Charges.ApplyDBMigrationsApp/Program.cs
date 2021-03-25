56b1625fe48bb0a628a420577d54235a9c7e7250
Michael Dalgaard Jepsen <xmdje@energinet.dk>
2021-03-25 08:54:41 +0100

    Add new project to solution

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GreenEnergyHub.Charges.ApplyDBMigrationsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConnectionStringFactory.GetConnectionString(args);
+            var filter = EnvironmentFilter.GetFilter(args);
+            var isDryRun = args.Contains("dryRun");
+
+            // This is the new way of doing it, but for backwards compatibility, the "old way" is still default.
+            // var upgrader = UpgradeFactory.GetUpgradeEngine(connectionString, filter, isDryRun);
+            var upgrader = UpgradeFactory.GetUpgradeEngine(connectionString);
+
+            var result = upgrader.PerformUpgrade();
+
+            return ResultReporter.ReportResult(result);
+        }
+    }
+}
