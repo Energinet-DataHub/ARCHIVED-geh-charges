42995f0fae8dcd5cb0742ee859cd06e7dec88c21
Lasse Ringgren Nielsen <lrn@energinet.dk>
2021-03-25 11:18:05 +0100

    added MarketParticipants.sql to TestData

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Test/202103251113 MarketParticipants.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000108', 'Supplier 1A', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000115', 'Supplier 1B', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000122', 'Supplier 1C', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8510000000006', 'Supplier 1D', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8510000000013', 'Supplier 1E', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8510000000020', 'Supplier 1F', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8510000000037', 'Supplier 1G', 'DDQ');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000016', 'Grid Operator 1', 'DDM');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000023', 'Grid Operator 2', 'DDM');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('8100000000030', 'Grid Operator 3', 'DDM');
+INSERT INTO [dbo].[MarketParticipant] VALUES ('5790000432752', 'System Operator', 'EZ');
+GO
