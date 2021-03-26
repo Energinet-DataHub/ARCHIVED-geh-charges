84239345d7eb817ad16dae583603597a91abf383
Lasse Ringgren Nielsen <lrn@energinet.dk>
2021-03-26 11:31:55 +0100

    started on adding keyvault to charges domain

--- build/infrastructure/kv-charges.tf
+++ build/infrastructure/kv-charges.tf
@@ -1,6 +1,6 @@
 module "kv_charges" {
   source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//key-vault?ref=1.0.0"
-  name                            = "kv${var.project)${var.organisation}${var.environment}"
+  name                            = "kv${var.project}${var.organisation}${var.environment}"
   resource_group_name             = data.azurerm_resource_group.main.name
   location                        = data.azurerm_resource_group.main.location
   tags                            = data.azurerm_resource_group.main.tags
@@ -9,8 +9,8 @@ module "kv_charges" {
   
   access_policy = [
     {
-      tenant_id               = var.current_tenant_id
-      object_id               = var.current_spn_object_id
+      tenant_id               = var.tenant_id
+      object_id               = var.spn_object_id
       secret_permissions      = ["set", "get", "list", "delete"]
       certificate_permissions = []
       key_permissions         = []
