55d4f5ab1299cfee7974a441ac31a679144e8288
MHT <mht@GoApplicate.com>
2021-04-20 02:03:54 +0200

    Improve AddGreenEnergyHub performance
    The algoritm for finding and registering IHubMessages was O(n^2) or worse.
    For every found type, the algoritm had to check the ever growing list of previously found types.
    
    Given that the algorithm checks an unknown number of assemblies with an unknown number of
    IHubMessage implementations - this can quickly get out of hand.
    
    Replace the code with an algorithm that runs in O(n).
    The code now only handles each type once, as we rely on the dictionary to filter out duplicates
    and enforcy priority based on assembly order.

--- source/messaging/source/GreenEnergyHub.Messaging.Integration.ServiceCollection/HandlerExtensions.cs
+++ source/messaging/source/GreenEnergyHub.Messaging.Integration.ServiceCollection/HandlerExtensions.cs
@@ -31,47 +31,33 @@ namespace GreenEnergyHub.Messaging
     {
         /// <summary>
         /// Searches the provided assemblies, as well as the common assemblies,
-        /// to find the HubRequestHandlers, HubCommandHandlers, IHubMessages,
-        /// IRuleEngines, the rules (represented as a list of
-        /// Types), the IEndpointResolver, and the IHubMessageTypeMap, and to
-        /// automatically register and provide this instances on function
-        /// startup.
+        /// to find
+        /// - HubRequestHandlers,
+        /// - HubCommandHandlers,
+        /// - IHubMessages,
+        /// - IRuleEngines,
+        /// - Rules (represented as a list of Types),
+        /// - IEndpointResolver,
+        /// - IHubMessageTypeMap,
+        /// and automatically register and provide the instances on function startup.
+        ///
+        /// Note, in case of multiple implementations of IHubMessages with the same name - only one will be registered.
+        /// See <see cref="FindImplementations"/> for more information
         /// </summary>
-        /// <param name="services">The IServiceCollection services provided by
-        /// the Azure Functions runtime to register our interfaces with.</param>
-        /// <param name="customerAssemblies">A list of assemblies to search for
-        /// interfaces to automatically register.</param>
-        public static void AddGreenEnergyHub(this IServiceCollection services, params System.Reflection.Assembly[] customerAssemblies)
+        /// <param name="services">The IServiceCollection services provided by the Azure Functions runtime to register our interfaces with.</param>
+        /// <param name="customerAssemblies">A list of assemblies to search for interfaces to automatically register.</param>
+        public static void AddGreenEnergyHub(
+            this IServiceCollection services,
+            params System.Reflection.Assembly[] customerAssemblies)
         {
-            // Register framework-provided classes last
-            var assemblies = customerAssemblies.Append(typeof(IHubMessage).Assembly).ToArray();
+            // Register framework-provided classes last, this is done to allow customerAssemblies to take priority over framework when finding implementations of IHubMessage
+            var assemblies = customerAssemblies.Append(typeof(IHubMessage).Assembly).ToArray(); // creates a new sequence, not altering input.
 
             services.AddMediatR(assemblies);
 
-            // Collection of IHubMessage types to discover related classes for
-            var messageTypesToRegister = new List<Type>();
-
-            // Walk assemblies in order passed, discovering unique (by name)
-            // declared message types
-            foreach (var assembly in assemblies)
+            var implementationFound = FindImplementations(typeof(IHubMessage), assemblies);
+            foreach (var messageType in implementationFound)
             {
-                var assemblyTypes = assembly.GetTypes();
-
-                // Discover message types but omit any already pending
-                // registration by same name from another assembly
-                var messageTypes = assemblyTypes
-                    .Where(type =>
-                        type.GetInterfaces().Contains(typeof(IHubMessage)));
-                var newMessageTypes = messageTypes
-                    .Where(existingMessageType => messageTypesToRegister
-                        .All(messageType => messageType.Name != existingMessageType.Name));
-                messageTypesToRegister.AddRange(newMessageTypes);
-            }
-
-            foreach (var messageType in messageTypesToRegister)
-            {
-                if (messageType.IsInterface) continue;
-
                 // Register with mapping class for the message type
                 services.AddTransient(_ => new MessageRegistration(messageType));
             }
@@ -81,6 +67,35 @@ namespace GreenEnergyHub.Messaging
             services.AddSingleton<IHubMessageTypeMap, HubMessageTypeMap>();
 
             services.DiscoverValidation(assemblies);
+        }
+
+        /// <summary>
+        /// Searches the provided <paramref name="assemblies"/> for implementations of <paramref name="interfaceToCheckFor"/>.
+        ///
+        /// Note, in case multiple types are found with the same Name, only one will be returned.
+        /// Which Type is selected, is based on the owning assembly's position in <paramref name="assemblies"/>.
+        /// Types from assemblies with lower indexes are prioritized.
+        /// </summary>
+        /// <param name="interfaceToCheckFor">A type to use, as a basis, for finding the types to register</param>
+        /// <param name="assemblies">A sequence of assemblies, in which to search for implementations of <paramref name="interfaceToCheckFor"/></param>
+        private static List<Type> FindImplementations(Type interfaceToCheckFor, System.Reflection.Assembly[] assemblies)
+        {
+            var hubMessageNameToTypeDictionary = new Dictionary<string, Type>();
+
+            // Check the assemblies in reverse order to satisfy the post condition that the first encounter of a given name wins.
+            // this works since '[type.Name] = type' adds/updates the type value stored, overriding values from less significant assemblies.
+            for (var i = assemblies.Length - 1; i >= 0; i--)
+            {
+                var messageTypes = assemblies[i].GetTypes()
+                    .Where(type => !type.IsInterface && type.GetInterfaces().Contains(interfaceToCheckFor));
+
+                foreach (var type in messageTypes)
+                {
+                    hubMessageNameToTypeDictionary[type.Name] = type;
+                }
+            }
+
+            return hubMessageNameToTypeDictionary.Values.ToList();
         }
     }
 }
