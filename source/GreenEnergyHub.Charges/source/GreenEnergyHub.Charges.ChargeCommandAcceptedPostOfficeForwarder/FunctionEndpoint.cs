a013a04cacc8719717b22b34e7d33d31a58d75da
BjarkeMeier <hej@bjarkemeier.dk>
2021-05-04 17:56:35 +0200

    Add charge forwarder function

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ChargeCommandAcceptedPostOfficeForwarder/FunctionEndpoint.cs
@@ -0,0 +1,63 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using GreenEnergyHub.Charges.Application;
+using GreenEnergyHub.Charges.Application.PostOffice;
+using GreenEnergyHub.Charges.Domain.Events.Local;
+using GreenEnergyHub.Charges.Infrastructure.Messaging;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace GreenEnergyHub.Charges.ChargeCommandAcceptedPostOfficeForwarder
+{
+    public class FunctionEndpoint
+    {
+        private const string FunctionName = nameof(FunctionEndpoint);
+        private readonly IChargeCommandAcceptedPostOfficeForwarder _chargeCommandAcceptedPostOfficeForwarder;
+        private readonly ICorrelationContext _correlationContext;
+
+        public FunctionEndpoint(
+            IChargeCommandAcceptedPostOfficeForwarder chargeCommandAcceptedPostOfficeForwarder,
+            ICorrelationContext correlationContext)
+        {
+            _chargeCommandAcceptedPostOfficeForwarder = chargeCommandAcceptedPostOfficeForwarder;
+            _correlationContext = correlationContext;
+        }
+
+        [FunctionName(FunctionName)]
+        public async Task RunAsync(
+            [ServiceBusTrigger(
+            "%COMMAND_ACCEPTED_TOPIC_NAME%",
+            "%COMMAND_ACCEPTED_SUBSCRIPTION_NAME%",
+            Connection = "COMMAND_ACCEPTED_LISTENER_CONNECTION_STRING")]
+            byte[] message,
+            ILogger log)
+        {
+            var jsonSerializedQueueItem = System.Text.Encoding.UTF8.GetString(message);
+            var serviceBusMessage = JsonConvert.DeserializeObject<ServiceBusMessageWrapper<ChargeCommandAcceptedEvent>>(jsonSerializedQueueItem);
+            var acceptedEvent = serviceBusMessage.Message;
+            SetCorrelationContext(acceptedEvent);
+            await _chargeCommandAcceptedPostOfficeForwarder.HandleAsync(acceptedEvent!).ConfigureAwait(false);
+
+            log.LogDebug("Received event with correlation ID '{CorrelationId}'", acceptedEvent.CorrelationId);
+        }
+
+        private void SetCorrelationContext(ChargeCommandAcceptedEvent acceptedEvent)
+        {
+            _correlationContext.CorrelationId = acceptedEvent.CorrelationId;
+        }
+    }
+}
---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ChargeCommandAcceptedPostOfficeForwarder/Startup.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using GreenEnergyHub.Charges.Application.PostOffice;
+using GreenEnergyHub.Charges.ChargeCommandAcceptedPostOfficeForwarder;
+using GreenEnergyHub.Charges.Infrastructure.Messaging;
+using GreenEnergyHub.Charges.Infrastructure.PostOffice;
+using GreenEnergyHub.Json;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NodaTime;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace GreenEnergyHub.Charges.ChargeCommandAcceptedPostOfficeForwarder
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure([NotNull] IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddOptions<PostOfficeConfiguration>().Configure(
+                configuration =>
+                {
+                    configuration.ConnectionString = Environment.GetEnvironmentVariable("POST_OFFICE_SENDER_CONNECTION_STRING");
+                    configuration.TopicName = Environment.GetEnvironmentVariable("POST_OFFICE_TOPIC_NAME");
+                });
+            builder.Services.AddScoped<ICorrelationContext, CorrelationContext>();
+            builder.Services.AddSingleton<IJsonSerializer, JsonSerializer>();
+            builder.Services.AddScoped(typeof(IClock), _ => SystemClock.Instance);
+            builder.Services.AddScoped<IPostOfficeService, PostOfficeService>();
+        }
+    }
+}
