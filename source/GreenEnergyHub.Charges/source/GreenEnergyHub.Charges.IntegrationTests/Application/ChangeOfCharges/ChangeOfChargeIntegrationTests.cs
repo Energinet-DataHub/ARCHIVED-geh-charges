52299d8c3f3d166fb326264804e429862f5f7e9a
HenrikSommer <sommerh@gmail.com>
2021-05-05 11:11:02 +0200

    Request to MessageReceiver ChargeHttpTrigger as first part of Change of charge integrationtest

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/Application/ChangeOfCharges/ChangeOfChargeIntegrationTests.cs
@@ -0,0 +1,112 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using GreenEnergyHub.Charges.Application.ChangeOfCharges;
+using GreenEnergyHub.Charges.MessageReceiver;
+using GreenEnergyHub.Json;
+using GreenEnergyHub.TestHelpers;
+using GreenEnergyHub.TestHelpers.Traits;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NodaTime;
+using Xunit;
+
+namespace GreenEnergyHub.Charges.IntegrationTests.Application.ChangeOfCharges
+{
+    [Trait(TraitNames.Category, TraitValues.IntegrationTest)]
+    public class ChangeOfChargesMessageHandlerTests
+    {
+        private readonly ChargeHttpTrigger _sut;
+
+        public ChangeOfChargesMessageHandlerTests()
+        {
+            var startup = new Startup();
+            var host = new HostBuilder().ConfigureWebJobs(startup.Configure).Build();
+
+            _sut = new ChargeHttpTrigger(
+                host.Services.GetRequiredService<IJsonSerializer>(),
+                host.Services.GetRequiredService<IChangeOfChargesMessageHandler>());
+        }
+
+        [Theory]
+        [InlineAutoDomainData]
+        public async Task ChargeCommandAccepted_Is_True(
+            [NotNull] [Frozen] Mock<ILogger> logger,
+            [NotNull] ExecutionContext executionContext,
+            [NotNull] IClock clock)
+        {
+            // arrange
+            var stream = GetInputStream("TestFiles\\ChargeAddition.json", clock);
+
+            var defaultHttpContext = new DefaultHttpContext();
+
+            defaultHttpContext.Request.Body = stream;
+            var req = new DefaultHttpRequest(defaultHttpContext);
+            executionContext.InvocationId = Guid.NewGuid();
+
+            // act
+            var result = (OkObjectResult)await _sut.RunAsync(req, executionContext, logger.Object).ConfigureAwait(false);
+
+            // assert
+            Assert.True(result!.StatusCode!.Value == 200);
+            // localEventPublisher.Verify(x => x.PublishAsync(It.Is<ChargeCommandReceivedEvent>(localEvent =>
+            //     localEvent.Command == transaction && localEvent.CorrelationId == transaction.CorrelationId)));
+        }
+
+        [Theory]
+        [InlineAutoDomainData]
+        public async Task ChargeCommandRejected_Is_True(
+            [NotNull] [Frozen] Mock<IChangeOfChargesTransactionHandler> changeOfChargesTransactionHandler,
+            [NotNull] ChangeOfChargesMessageHandler sut)
+        {
+            // Arrange
+            var testCoCh = changeOfChargesTransactionHandler;
+            var newSut = sut;
+
+            // Act
+
+            // Assert
+            await Task.Run(() => Assert.True(true)).ConfigureAwait(false);
+        }
+
+        private static Stream GetInputStream(string filePath, IClock clock)
+        {
+            var basePath = Assembly.GetExecutingAssembly().Location;
+            var path = Path.Combine(Directory.GetParent(basePath).FullName, filePath);
+            var fileText = File.ReadAllText(path);
+            var replaced = ReplaceMergeFields(clock, fileText);
+
+            return new MemoryStream(Encoding.ASCII.GetBytes(replaced));
+        }
+
+        private static string ReplaceMergeFields(IClock clock, string file)
+        {
+            var now = clock.GetCurrentInstant().ToString();
+            return file.Replace("{{$isoTimestamp}}", now);
+        }
+    }
+}
