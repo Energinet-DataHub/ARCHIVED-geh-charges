2f595a6750207f7095b486588e6fc0fa477c91b0
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2021-05-17 15:22:58 +0200

    Better name of InvalidResolutionTypeOnFeeOnCreate

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Validation/ValidationRuleIdentifier.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Validation/ValidationRuleIdentifier.cs
@@ -23,6 +23,6 @@ namespace GreenEnergyHub.Charges.Application.Validation
         SenderIsMandatory, // VR150
         RecipientIsMandatory, // VR153
         ChargeOperationIdIsRequired, // VR223
-        InvalidResolutionTypeOnFee, // VR505-2
+        InvalidResolutionTypeOnFeeOnCreate, // VR505-2
     }
 }
