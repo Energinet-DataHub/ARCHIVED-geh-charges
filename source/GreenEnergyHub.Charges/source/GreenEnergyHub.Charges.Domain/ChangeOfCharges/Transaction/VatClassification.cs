7e0d2ae432f38722ada469977d8a11f1be0e2679
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2021-05-17 11:27:58 +0200

    Added new VatClassification.Unknown as default value, added VR488 VatClassificationValidationRule.cs and unittests of that rule

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/ChangeOfCharges/Transaction/VatClassification.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/ChangeOfCharges/Transaction/VatClassification.cs
@@ -20,6 +20,8 @@ namespace GreenEnergyHub.Charges.Domain.ChangeOfCharges.Transaction
     /// </summary>
     public enum VatClassification
     {
+        // ReSharper disable once UnusedMember.Global
+        Unknown = 0,
         NoVat,
         Vat25,
     }
