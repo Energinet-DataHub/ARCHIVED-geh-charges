44b6b692119048b13d2c485f0705ec8e1e810117
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-07-01 12:55:20 +0200

    Added SQL scripts for creating Metering Point table

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202107011226 New Metering Point.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Charges].[MeteringPoint]
+(
+    RowId bigint NOT NULL IDENTITY PRIMARY KEY,
+    MeteringPointId NVARCHAR(50) NOT NULL,
+    MeteringPointType smallint NOT NULL,
+    MeteringGridArea NVARCHAR(30) NOT NULL,
+    EffectiveDate DateTime NOT NULL,
+    ConnectionState smallint NOT NULL,
+    SettlementMethod smallint NOT NULL,
+)
+GO
+
+CREATE INDEX i1 ON [Charges].[MeteringPoint] (RowId DESC);
+GO
