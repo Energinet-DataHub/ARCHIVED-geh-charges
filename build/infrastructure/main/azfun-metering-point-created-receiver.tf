b2256d9f20c7dbe14a6ca079e51e731899dabf15
Michael Jepsen <mdj@systemate.dk>
2021-07-07 13:17:13 +0200

    Fix name of storage accounts (#406)
    * Fix storage account names
    
    * Use own storage module

--- build/infrastructure/main/azfun-metering-point-created-receiver.tf
+++ build/infrastructure/main/azfun-metering-point-created-receiver.tf
@@ -52,8 +52,8 @@ module "azfun_metering_point_created_receiver_plan" {
 }
 
 module "azfun_metering_point_created_receiver_stor" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//storage-account?ref=1.3.0"
-  name                      = "stormsgrcvr${random_string.metering_point_created_receiver.result}"
+  source                    = "../modules/storage-account"
+  name                      = "stormpcre${random_string.metering_point_created_receiver.result}"
   resource_group_name       = data.azurerm_resource_group.main.name
   location                  = data.azurerm_resource_group.main.location
   account_replication_type  = "LRS"
