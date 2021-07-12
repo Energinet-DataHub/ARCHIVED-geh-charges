e10223e84bfdc35d28ffbc924912deac72df2066
Lasse Ringgren Nielsen <LRN@energinet.dk>
2021-07-12 10:37:31 +0200

    build - Terraform action outputs the charges sql server connection string (#413)
    After the Terraform part of infrastructre-CD.yml was moved into
    its own action. Some variables were unreachable. This is a temp fix
    to be able to run the ApplyDBMigrationgs step in the workflow.

--- build/infrastructure/main/outputs.tf
+++ build/infrastructure/main/outputs.tf
@@ -11,32 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-output "sql_server_url" {
-  description = "URL for the shared sql server"
+output "sql-server-connection-string" {
+  description = "The charges sql server connection string"
   value = module.sqlsrv_charges.fully_qualified_domain_name
-  sensitive = true
-}
-
-output "charges_sql_database_name" {
-  description = "Name of the charges database in the shared sql server"
-  value = module.sqldb_charges.name
-  sensitive = true
-}
-
-output "charges_user_keyvaultname" {
-  description = "Name of the secret in the keyvault containing the username for the charges sql database"
-  value = module.sqlsrv_admin_username.name
-  sensitive = true
-}
-
-output "charges_password_keyvaultname" {
-  description = "Name of the secret in the keyvault containing the password for the charges sql database"
-  value = module.sqlsrv_admin_password.name
-  sensitive = true
-}
-
-output "kv_charges_name" {
-  description = "Name of the keyvault"
-  value = module.kv_charges.name
   sensitive = true
 }
