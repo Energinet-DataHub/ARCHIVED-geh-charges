13a550617c8a417bec0ab9baeb0e092265c5c9d9
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-07-13 08:30:16 +0200

    Updated config files to topic

--- build/infrastructure/main/locals.tf
+++ build/infrastructure/main/locals.tf
@@ -17,5 +17,5 @@ locals {
 	CHARGE_DB_CONNECTION_STRING               = "Server=${module.sqlsrv_charges.fully_qualified_domain_name};Database=${module.sqldb_charges.name};Uid=${local.sqlServerAdminName};Pwd=${random_password.sqlsrv_admin_password.result};"
     LOCAL_TIMEZONENAME                        = "Europe/Copenhagen"
     # Must match the name used in the repo geh-shared-resources
-    METERING_POINT_CREATED_QUEUE_NAME         = "metering-point-created"
+    METERING_POINT_CREATED_TOPIC_NAME         = "metering-point-created"
 }
