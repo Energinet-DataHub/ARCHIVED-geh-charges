d22a919eb3f3376c7c8690dbccf12260cc7de9ba
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-07-13 15:29:22 +0200

    test: Using InlineAutoMoqData to generate test data

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Infrastructure/Internal/Mappers/ChargeLinkCommandOutboundMapperTests.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Infrastructure/Internal/Mappers/ChargeLinkCommandOutboundMapperTests.cs
@@ -12,16 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using Energinet.DataHub.ChargeLinks.InternalContracts;
 using FluentAssertions;
 using GreenEnergyHub.Charges.Domain.ChargeLinks;
-using GreenEnergyHub.Charges.Domain.MarketDocument;
 using GreenEnergyHub.Charges.Infrastructure.Internal.Mappers;
-using GreenEnergyHub.Messaging.MessageTypes.Common;
+using GreenEnergyHub.Charges.TestCore;
 using GreenEnergyHub.TestHelpers.FluentAssertionsExtensions;
-using NodaTime;
 using Xunit;
 using Xunit.Categories;
 
@@ -30,55 +28,18 @@ namespace GreenEnergyHub.Charges.Tests.Infrastructure.Internal.Mappers
     [UnitTest]
     public class ChargeLinkCommandOutboundMapperTests
     {
-        [Fact]
-        public void Convert_WhenCalled_ShouldMapToProtobufWithCorrectValues()
+        [Theory]
+        [InlineAutoMoqData]
+        public void Convert_WhenCalled_ShouldMapToProtobufWithCorrectValues([NotNull] ChargeLinkCommand chargeLinkCommand)
         {
             // Arrange
-            var correlationId = Guid.NewGuid();
-            var chargeLink = new ChargeLinkCommand(correlationId.ToString())
-            {
-                Document = new Document
-                {
-                    Id = "1",
-                    Sender = new Domain.MarketDocument.MarketParticipant
-                    {
-                        Id = "sender",
-                        Name = "senderName",
-                        BusinessProcessRole = MarketParticipantRole.EnergyAgency,
-                    },
-                    Recipient = new Domain.MarketDocument.MarketParticipant
-                    {
-                        Id = "recipient",
-                        Name = "recipientName",
-                        BusinessProcessRole = MarketParticipantRole.EnergyAgency,
-                    },
-                    BusinessReasonCode = BusinessReasonCode.UpdateChargeInformation,
-                    CreatedDateTime = SystemClock.Instance.GetCurrentInstant(),
-                    Type = DocumentType.RequestChangeBillingMasterData,
-                    RequestDate = SystemClock.Instance.GetCurrentInstant(),
-                    IndustryClassification = IndustryClassification.Electricity,
-                },
-                ChargeLink = new ChargeLink
-                {
-                    Id = "11",
-                    ChargeId = "12",
-                    ChargeOwner = "13",
-                    ChargeType = Domain.ChangeOfCharges.Transaction.ChargeType.Tariff,
-                    Factor = 15,
-                    MeteringPointId = "16",
-                    EndDateTime = null,
-                    StartDateTime = SystemClock.Instance.GetCurrentInstant(),
-                    Transaction = Transaction.NewTransaction(),
-                },
-                Transaction = Transaction.NewTransaction(),
-            };
             var mapper = new ChargeLinkDomainOutboundMapper();
 
             // Act
-            var converted = (ChargeLinkCommandDomain)mapper.Convert(chargeLink);
+            var converted = (ChargeLinkCommandDomain)mapper.Convert(chargeLinkCommand);
 
             // Assert
-            var chargeLinkDocument = chargeLink.Document;
+            var chargeLinkDocument = chargeLinkCommand.Document;
             var convertedDocument = converted.Document;
             convertedDocument.Id.Should().BeEquivalentTo(chargeLinkDocument.Id);
             convertedDocument.Sender.Id.Should().BeEquivalentTo(chargeLinkDocument.Sender.Id);
@@ -94,12 +55,12 @@ namespace GreenEnergyHub.Charges.Tests.Infrastructure.Internal.Mappers
             convertedDocument.IndustryClassification.Should().BeEquivalentTo(chargeLinkDocument.IndustryClassification.ToString());
             convertedDocument.Should().NotContainNullsOrEmptyEnumerables();
             chargeLinkDocument.Should().NotContainNullsOrEmptyEnumerables();
-            converted.ChargeLink.Id.Should().Be(chargeLink.ChargeLink.Id);
-            converted.ChargeLink.ChargeId.Should().Be(chargeLink.ChargeLink.ChargeId);
-            converted.ChargeLink.ChargeOwner.Should().Be(chargeLink.ChargeLink.ChargeOwner);
-            converted.ChargeLink.ChargeType.Should().Be(chargeLink.ChargeLink.ChargeType.ToString());
-            converted.ChargeLink.Factor.Should().Be(chargeLink.ChargeLink.Factor.ToString(CultureInfo.InvariantCulture));
-            converted.ChargeLink.MeteringPointId.Should().Be(chargeLink.ChargeLink.MeteringPointId);
+            converted.ChargeLink.Id.Should().Be(chargeLinkCommand.ChargeLink.Id);
+            converted.ChargeLink.ChargeId.Should().Be(chargeLinkCommand.ChargeLink.ChargeId);
+            converted.ChargeLink.ChargeOwner.Should().Be(chargeLinkCommand.ChargeLink.ChargeOwner);
+            converted.ChargeLink.ChargeType.Should().Be(chargeLinkCommand.ChargeLink.ChargeType.ToString());
+            converted.ChargeLink.Factor.Should().Be(chargeLinkCommand.ChargeLink.Factor.ToString(CultureInfo.InvariantCulture));
+            converted.ChargeLink.MeteringPointId.Should().Be(chargeLinkCommand.ChargeLink.MeteringPointId);
             converted.ChargeLink.StartDateTime.Should().BeEquivalentTo(converted.ChargeLink.StartDateTime);
             converted.ChargeLink.EndDateTime.Should().BeEquivalentTo(converted.ChargeLink.EndDateTime);
         }
