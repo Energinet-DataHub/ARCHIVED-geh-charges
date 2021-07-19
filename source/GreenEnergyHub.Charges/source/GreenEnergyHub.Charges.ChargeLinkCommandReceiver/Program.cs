9f3c82740d2c9ed755058d469ee40569a1ed9201
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-07-19 13:33:02 +0200

    add charge link command receiver function

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ChargeLinkCommandReceiver/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace GreenEnergyHub.Charges.ChargeLinkCommandReceiver
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
