e86baca884d82d27a3ca3284ca476943238a2393
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-07-22 09:23:47 +0200

    Dependency inject charge link created event factory

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ChargeLinkEventPublisher/Program.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ChargeLinkEventPublisher/Program.cs
@@ -14,6 +14,7 @@
 
 using System;
 using GreenEnergyHub.Charges.Application.ChargeLinks;
+using GreenEnergyHub.Charges.Application.Factories;
 using GreenEnergyHub.Charges.Infrastructure.Internal.ChargeLinkCommandAccepted;
 using GreenEnergyHub.Charges.Infrastructure.Messaging;
 using GreenEnergyHub.Messaging.Protobuf;
@@ -40,6 +41,7 @@ namespace GreenEnergyHub.Charges.ChargeLinkEventPublisher
 
             ConfigureMessaging(serviceCollection);
 
+            serviceCollection.AddScoped<IChargeLinkCreatedEventFactory, ChargeLinkCreatedEventFactory>();
             serviceCollection.AddScoped<IChargeLinkEventPublishHandler, ChargeLinkEventPublishHandler>();
         }
 
