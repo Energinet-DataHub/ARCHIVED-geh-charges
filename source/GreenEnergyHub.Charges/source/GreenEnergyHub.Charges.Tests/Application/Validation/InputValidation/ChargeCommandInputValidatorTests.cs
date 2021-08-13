19414f9d911f1ac4d90e191de759b811bbb21bba
Peter Tandrup <PTA@energinet.dk>
2021-08-13 10:46:11 +0200

    Added unit test for ChargeCommandInputValidator

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/Validation/InputValidation/ChargeCommandInputValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AutoFixture.Xunit2;
+using GreenEnergyHub.Charges.Application.Validation;
+using GreenEnergyHub.Charges.Application.Validation.InputValidation;
+using GreenEnergyHub.Charges.Domain.ChangeOfCharges.Transaction;
+using GreenEnergyHub.Charges.TestCore;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.Tests.Application.Validation.InputValidation
+{
+    [UnitTest]
+    public class ChargeCommandInputValidatorTests
+    {
+        [Theory]
+        [InlineAutoData]
+        public void Validate_WhenValidatingChargeCommand_ReturnsChargeCommandValidationResult(
+            [NotNull] InputValidationRulesFactory inputValidationRulesFactory,
+            [NotNull] ChargeCommand chargeCommand)
+        {
+            // Arrange
+            var sut = new ChargeCommandInputValidator(inputValidationRulesFactory);
+
+            // Act
+            var result = sut.Validate(chargeCommand);
+
+            // Assert
+            Assert.IsType<ChargeCommandValidationResult>(result);
+        }
+    }
+}
