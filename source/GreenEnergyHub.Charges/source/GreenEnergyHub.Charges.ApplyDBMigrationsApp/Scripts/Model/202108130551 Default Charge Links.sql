9a62032a1807d561765eb38348f93ca37f6e0643
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-08-13 12:29:03 +0200

    Add foregin key to setting table

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202108130551 Default Charge Links.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202108130551 Default Charge Links.sql
@@ -2,7 +2,7 @@
 (
     RowId int NOT NULL IDENTITY PRIMARY KEY,
     MeteringPointType int NOT NULL,
-    ChargeRowId int NOT NULL,
+    ChargeRowId int NOT NULL FOREIGN KEY REFERENCES [Charges].Charge(RowId),
     StartDateTime DateTime NOT NULL,
     EndDateTime DateTime,
 )
