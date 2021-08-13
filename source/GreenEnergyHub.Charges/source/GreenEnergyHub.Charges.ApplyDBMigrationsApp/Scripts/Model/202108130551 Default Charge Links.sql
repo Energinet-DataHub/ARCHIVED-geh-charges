781f9d545aea16d3ba2b843a98eca918d02bbf23
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-08-13 08:08:00 +0200

    Add default charge link setting table

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202108130551 Default Charge Links.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Charges].[DefaultChargeLinkSetting]
+(
+    RowId int NOT NULL IDENTITY PRIMARY KEY,
+    MeteringPointType int NOT NULL,
+    ChargeRowId int NOT NULL,
+    StartDateTime DateTime NOT NULL,
+    EndDateTime DateTime,
+)
+GO
+
+CREATE INDEX i1 ON [Charges].[DefaultChargeLinkSetting] (MeteringPointType ASC, StartDateTime DESC, EndDateTime DESC);
+GO
