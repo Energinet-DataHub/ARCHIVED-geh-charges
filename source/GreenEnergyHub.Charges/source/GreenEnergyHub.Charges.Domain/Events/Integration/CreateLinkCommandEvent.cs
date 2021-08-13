641c6cf6d93d09163327b43c1d60a5daa00645dd
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-08-13 10:21:59 +0200

    Add comments to create link command event

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Events/Integration/CreateLinkCommandEvent.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Events/Integration/CreateLinkCommandEvent.cs
@@ -20,6 +20,13 @@ namespace GreenEnergyHub.Charges.Domain.Events.Integration
 {
     public class CreateLinkCommandEvent : InboundIntegrationEvent
     {
+        /// <summary>
+        /// Event raised by the MeteringPoint domain when the charge domain
+        /// is asked to add links to a metering point
+        /// </summary>
+        /// <param name="meteringPointId">The metering point to add links to</param>
+        /// <param name="meteringPointType">The type of the metering point, which will influence which links are added</param>
+        /// <param name="startDateTime">The date from which links should be added</param>
         public CreateLinkCommandEvent(
             string meteringPointId,
             MeteringPointType meteringPointType,
@@ -31,10 +38,19 @@ namespace GreenEnergyHub.Charges.Domain.Events.Integration
             StartDateTime = startDateTime;
         }
 
+        /// <summary>
+        /// Metering point ID to add links to
+        /// </summary>
         public string MeteringPointId { get; }
 
+        /// <summary>
+        /// Type of the metering point to add links to
+        /// </summary>
         public MeteringPointType MeteringPointType { get; }
 
+        /// <summary>
+        /// The date from which links should be added
+        /// </summary>
         public Instant StartDateTime { get; }
     }
 }
