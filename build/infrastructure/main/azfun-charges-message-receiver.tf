23af69c608a7bba6d9216f5a8d129d941fa51a72
Peter Tandrup <PTA@energinet.dk>
2021-08-30 14:41:01 +0200

    tf - rename message receiver to charge receiver

--- build/infrastructure/main/azfun-charges-message-receiver.tf
+++ build/infrastructure/main/azfun-charges-message-receiver.tf
@@ -11,14 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "azfun_message_receiver" {
+module "azfun_charge_receiver" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//function-app?ref=1.7.0"
-  name                                      = "azfun-message-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                                      = "azfun-charge-receiver-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name                       = data.azurerm_resource_group.main.name
   location                                  = data.azurerm_resource_group.main.location
-  storage_account_access_key                = module.azfun_message_receiver_stor.primary_access_key
-  app_service_plan_id                       = module.azfun_message_receiver_plan.id
-  storage_account_name                      = module.azfun_message_receiver_stor.name
+  storage_account_access_key                = module.azfun_charge_receiver_stor.primary_access_key
+  app_service_plan_id                       = module.azfun_charge_receiver_plan.id
+  storage_account_name                      = module.azfun_charge_receiver_stor.name
   application_insights_instrumentation_key  = module.appi.instrumentation_key
   tags                                      = data.azurerm_resource_group.main.tags
   app_settings                              = {
@@ -32,15 +32,15 @@ module "azfun_message_receiver" {
   }
   dependencies                              = [
     module.appi.dependent_on,
-    module.azfun_message_receiver_plan.dependent_on,
-    module.azfun_message_receiver_stor.dependent_on,
+    module.azfun_charge_receiver_plan.dependent_on,
+    module.azfun_charge_receiver_stor.dependent_on,
     module.sbtar_command_received_sender.dependent_on,
   ]
 }
 
-module "azfun_message_receiver_plan" {
+module "azfun_charge_receiver_plan" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//app-service-plan?ref=1.7.0"
-  name                = "asp-message-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                = "asp-charge-receiver-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   kind                = "FunctionApp"
@@ -51,9 +51,9 @@ module "azfun_message_receiver_plan" {
   tags                = data.azurerm_resource_group.main.tags
 }
 
-module "azfun_message_receiver_stor" {
+module "azfun_charge_receiver_stor" {
   source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//storage-account?ref=1.7.0"
-  name                      = "stormsgrcvr${random_string.message_receiver.result}"
+  name                      = "stormsgrcvr${random_string.charge_receiver.result}"
   resource_group_name       = data.azurerm_resource_group.main.name
   location                  = data.azurerm_resource_group.main.location
   account_replication_type  = "LRS"
@@ -62,44 +62,44 @@ module "azfun_message_receiver_stor" {
   tags                      = data.azurerm_resource_group.main.tags
 }
 
-module "kv_azfun_message_receiver_hostname" {
+module "kv_azfun_charge_receiver_hostname" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//key-vault-secret?ref=1.7.0"
   name          = "MESSAGE-RECEIVER-HOSTNAME"
-  value         = module.azfun_message_receiver.default_hostname
+  value         = module.azfun_charge_receiver.default_hostname
   key_vault_id  = module.kv_charges.id
   tags          = data.azurerm_resource_group.main.tags
   dependencies  = [module.kv_charges.dependent_on]
 }
 
 # Since all functions need a storage connected we just generate a random name
-resource "random_string" "message_receiver" {
+resource "random_string" "charge_receiver" {
   length  = 6
   special = false
   upper   = false
 }
 
-module "ping_webtest_message_receiver" {
+module "ping_webtest_charge_receiver" {
   source                          = "../modules/ping-webtest" # Repo geh-terraform-modules doesn't have a webtest module at the time of this writing
-  name                            = "ping-webtest-message-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                            = "ping-webtest-charge-receiver-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name             = data.azurerm_resource_group.main.name
   location                        = data.azurerm_resource_group.main.location
   tags                            = data.azurerm_resource_group.main.tags
   application_insights_id         = module.appi.id
-  url                             = "https://${module.azfun_message_receiver.default_hostname}/api/HealthStatus"
-  dependencies                    = [module.azfun_message_receiver.dependent_on]
+  url                             = "https://${module.azfun_charge_receiver.default_hostname}/api/HealthStatus"
+  dependencies                    = [module.azfun_charge_receiver.dependent_on]
 }
 
-module "mma_ping_webtest_message_receiver" {
+module "mma_ping_webtest_charge_receiver" {
   source                   = "../modules/availability-alert"
-  name                     = "mma-charge-message-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                     = "mma-charge-charge-receiver-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name      = data.azurerm_resource_group.main.name
   application_insight_id   = module.appi.id
-  ping_test_name           = module.ping_webtest_message_receiver.name
+  ping_test_name           = module.ping_webtest_charge_receiver.name
   action_group_id          = module.mag_availability_group.id
   tags                     = data.azurerm_resource_group.main.tags
   dependencies             = [
     module.appi.dependent_on,
-    module.ping_webtest_message_receiver.dependent_on,
+    module.ping_webtest_charge_receiver.dependent_on,
     module.mag_availability_group.dependent_on
   ]
 }
