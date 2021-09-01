a07177d09b902950fa9aa101fed5c115b78b615b
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-09-01 10:44:23 +0200

    nits

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure/Repositories/ChargeRepository.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure/Repositories/ChargeRepository.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using GreenEnergyHub.Charges.Application.Charges.Repositories;
 using GreenEnergyHub.Charges.Domain.Charges;
