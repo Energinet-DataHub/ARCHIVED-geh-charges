f608784542b7ace802454c502d25403dfaa2421d
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-09-22 13:29:18 +0200

    Replace link receiver and metering point created terraform with new host function

--- build/infrastructure/main/azfun-charges.tf
+++ build/infrastructure/main/azfun-charges.tf
@@ -11,37 +11,41 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "azfun_link_receiver" {
+module "azfun_charges" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//function-app?ref=1.7.0"
   name                                      = "azfun-link-receiver-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name                       = data.azurerm_resource_group.main.name
   location                                  = data.azurerm_resource_group.main.location
-  storage_account_access_key                = module.azfun_link_receiver_stor.primary_access_key
+  storage_account_access_key                = module.azfun_charges_stor.primary_access_key
   app_service_plan_id                       = module.asp_charges.id
-  storage_account_name                      = module.azfun_link_receiver_stor.name
+  storage_account_name                      = module.azfun_charges_stor.name
   application_insights_instrumentation_key  = module.appi.instrumentation_key
   tags                                      = data.azurerm_resource_group.main.tags
   app_settings                              = {
     # Region: Default Values
-    WEBSITE_ENABLE_SYNC_UPDATE_SITE              = true
-    WEBSITE_RUN_FROM_PACKAGE                     = 1
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE          = true
-    FUNCTIONS_WORKER_RUNTIME                     = "dotnet-isolated"
+    WEBSITE_ENABLE_SYNC_UPDATE_SITE               = true
+    WEBSITE_RUN_FROM_PACKAGE                      = 1
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE           = true
+    FUNCTIONS_WORKER_RUNTIME                      = "dotnet-isolated"
+    CHARGE_DB_CONNECTION_STRING                   = local.CHARGE_DB_CONNECTION_STRING
+    INTEGRATIONEVENT_LISTENER_CONNECTION_STRING   = data.azurerm_key_vault_secret.integration_events_listener_connection_string.value
+    DOMAINEVENT_SENDER_CONNECTION_STRING          = module.sbnar_charges_sender.primary_connection_string
     CHARGE_LINK_RECEIVED_TOPIC_NAME               = module.sbt_link_command_received.name
-    CHARGE_LINK_RECEIVED_SENDER_CONNECTION_STRING = trimsuffix(module.sbtar_link_command_received_sender.primary_connection_string, ";EntityPath=${module.sbt_link_command_received.name}")
+    METERING_POINT_CREATED_TOPIC_NAME             = local.METERING_POINT_CREATED_TOPIC_NAME
+    METERING_POINT_CREATED_SUBSCRIPTION_NAME      = local.METERING_POINT_CREATED_SUBSCRIPTION_NAME
   }
   dependencies                              = [
     module.appi.dependent_on,
     module.asp_charges.dependent_on,
-    module.azfun_link_receiver_stor.dependent_on,
-    module.sbtar_link_command_received_sender.dependent_on,
+    module.azfun_charges_stor.dependent_on,
+    module.sbnar_charges_sender.dependent_on,
     module.sbt_link_command_received.dependent_on,
   ]
 }
 
-module "azfun_link_receiver_stor" {
+module "azfun_charges_stor" {
   source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//storage-account?ref=1.7.0"
-  name                      = "storlinkrcvr${random_string.link_receiver.result}"
+  name                      = "storcharges${random_string.charges.result}"
   resource_group_name       = data.azurerm_resource_group.main.name
   location                  = data.azurerm_resource_group.main.location
   account_replication_type  = "LRS"
@@ -51,34 +55,34 @@ module "azfun_link_receiver_stor" {
 }
 
 # Since all functions need a storage connected we just generate a random name
-resource "random_string" "link_receiver" {
+resource "random_string" "charges" {
   length  = 6
   special = false
   upper   = false
 }
 
-module "ping_webtest_link_receiver" {
+module "ping_webtest_charges" {
   source                          = "../modules/ping-webtest" # Repo geh-terraform-modules doesn't have a webtest module at the time of this writing
-  name                            = "ping-webtest-link-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                            = "ping-webtest-charges-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name             = data.azurerm_resource_group.main.name
   location                        = data.azurerm_resource_group.main.location
   tags                            = data.azurerm_resource_group.main.tags
   application_insights_id         = module.appi.id
-  url                             = "https://${module.azfun_link_receiver.default_hostname}/api/HealthStatus"
-  dependencies                    = [module.azfun_link_receiver.dependent_on]
+  url                             = "https://${module.azfun_charges.default_hostname}/api/HealthStatus"
+  dependencies                    = [module.azfun_charges.dependent_on]
 }
 
-module "mma_ping_webtest_link_receiver" {
+module "mma_ping_webtest_charges" {
   source                   = "../modules/availability-alert"
-  name                     = "mma-charge-link-receiver-${var.project}-${var.organisation}-${var.environment}"
+  name                     = "mma-charges-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name      = data.azurerm_resource_group.main.name
   application_insight_id   = module.appi.id
-  ping_test_name           = module.ping_webtest_link_receiver.name
+  ping_test_name           = module.ping_webtest_charges.name
   action_group_id          = module.mag_availability_group.id
   tags                     = data.azurerm_resource_group.main.tags
   dependencies             = [
     module.appi.dependent_on,
-    module.ping_webtest_link_receiver.dependent_on,
+    module.ping_webtest_charges.dependent_on,
     module.mag_availability_group.dependent_on
   ]
 }
