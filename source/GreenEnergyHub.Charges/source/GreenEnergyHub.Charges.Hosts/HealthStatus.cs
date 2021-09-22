ecb87547970d55a72a305e9b18b183f9433cdb4f
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-09-22 07:42:21 +0200

    Rename ChargeLinkReceiver project to Hosts

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Hosts/HealthStatus.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Hosts/HealthStatus.cs
@@ -19,7 +19,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace GreenEnergyHub.Charges.ChargeLinkReceiver
+namespace GreenEnergyHub.Charges.Hosts
 {
     public class HealthStatus
     {
