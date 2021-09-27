7679275af2c3cda657b1c00230dd535692a7e252
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-09-27 14:24:50 +0200

    Remove charge receiver and charge command receiver from developer deployment script

--- source/GreenEnergyHub.Charges/deploy-from-localhost.cmd
+++ source/GreenEnergyHub.Charges/deploy-from-localhost.cmd
@@ -14,9 +14,7 @@ set /p organization=Enter organization used with Terraform (perhaps your initial
 set /p doBuild=Build solution ([y]/n)?
 rem If you don't know the password, perhaps you can obtain it from the configuration settings of the deployed ChargeCommandReceiver function in Azure portal
 set /p sqlPassword=Enter SQL password for 'gehdbadmin' to update db or empty to skip: 
-set /p deployChargeReceiver=Deploy charge receiver ([y]/n)?
 set /p functionHost=Deploy function host ([y]/n)?
-set /p deployCommandReceiver=Deploy command receiver ([y]/n)?
 
 IF /I not "%doBuild%" == "n" (
     rem Clean is necessary if e.g. a function project name has changed because otherwise both assemblies will be picked up by deployment
@@ -30,23 +28,9 @@ IF not "%sqlPassword%" == "" (
         includeSeedData includeTestData
 )
 
-rem All (but the last) deployments are opened in separate windows in order to execute in parallel
-
-IF /I not "%deployChargeReceiver%" == "n" (
-    pushd source\GreenEnergyHub.Charges.ChargeReceiver\bin\Release\netcoreapp3.1
-    start "Deploy: Charge Receiver" cmd /c "func azure functionapp publish azfun-charge-receiver-charges-%organization%-s & pause"
-    popd
-)
-
 IF /I not "%functionHost%" == "n" (
     pushd source\GreenEnergyHub.Charges.FunctionHost\bin\Release\net5.0
     start "Deploy: FunctionHosts" cmd /c "func azure functionapp publish azfun-functionhost-charges-%organization%-s & pause"
-    popd
-)
-
-IF /I not "%deployCommandReceiver%" == "n" (
-    pushd source\GreenEnergyHub.Charges.ChargeCommandReceiver\bin\Release\netcoreapp3.1
-    start "Deploy: Charge Command Receiver" cmd /c "func azure functionapp publish azfun-charge-command-receiver-charges-%organization%-s & pause"
     popd
 )
 
