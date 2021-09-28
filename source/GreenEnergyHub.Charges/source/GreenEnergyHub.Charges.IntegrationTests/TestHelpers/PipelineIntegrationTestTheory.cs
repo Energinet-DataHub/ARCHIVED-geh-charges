ccc2db802cf0aa49b0bf98bb1a933f853052078d
Michael Jepsen <mdj@systemate.dk>
2021-09-28 12:15:28 +0200

    refac: move endpoints to functionhost (#650)
    Moves the other endpoints to the function and eliminates
    the extra pipelines and terraform.
    
    This will allow for a much simpler deployment and maintaince.

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/TestHelpers/PipelineIntegrationTestTheory.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/TestHelpers/PipelineIntegrationTestTheory.cs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/*
 using Xunit;
 
 namespace GreenEnergyHub.Charges.IntegrationTests.TestHelpers
@@ -33,3 +33,4 @@ namespace GreenEnergyHub.Charges.IntegrationTests.TestHelpers
         }
     }
 }
+*/
