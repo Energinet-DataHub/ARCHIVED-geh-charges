e6322096dff47968fff847136ddc6218cc0eae8b
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-09-30 09:17:23 +0200

    Extract subscriptions to own terraform files to align with link and shared repo

--- build/infrastructure/main/sb-charges.tf
+++ build/infrastructure/main/sb-charges.tf
@@ -46,14 +46,6 @@ module "sbt_command_received" {
   dependencies        = [module.sbn_charges]
 }
 
-resource "azurerm_servicebus_subscription" "sbs_command_received" {
-  name                = "sbs-command-received"
-  resource_group_name = data.azurerm_resource_group.main.name
-  namespace_name      = module.sbn_charges.name
-  topic_name          = module.sbt_command_received.name
-  max_delivery_count  = 1
-}
-
 module "sbt_command_accepted" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic?ref=1.7.0"
   name                = "sbt-command-accepted"
@@ -62,36 +54,10 @@ module "sbt_command_accepted" {
   dependencies        = [module.sbn_charges]
 }
 
-resource "azurerm_servicebus_subscription" "sbs_command_accepted" {
-  name                = "sbs-command-accepted"
-  resource_group_name = data.azurerm_resource_group.main.name
-  namespace_name      = module.sbn_charges.name
-  topic_name          = module.sbt_command_accepted.name
-  max_delivery_count  = 1
-}
-
 module "sbt_command_rejected" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic?ref=1.7.0"
   name                = "sbt-command-rejected"
   namespace_name      = module.sbn_charges.name
   resource_group_name = data.azurerm_resource_group.main.name
   dependencies        = [module.sbn_charges]
-}
-
-module "sbtar_command_rejected_listener" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-rejected-listener"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  listen                    = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_rejected.name
-}
-
-resource "azurerm_servicebus_subscription" "sbs_command_rejected" {
-  name                = "sbs-command-rejected"
-  resource_group_name = data.azurerm_resource_group.main.name
-  namespace_name      = module.sbn_charges.name
-  topic_name          = module.sbt_command_rejected.name
-  max_delivery_count  = 1
 }
