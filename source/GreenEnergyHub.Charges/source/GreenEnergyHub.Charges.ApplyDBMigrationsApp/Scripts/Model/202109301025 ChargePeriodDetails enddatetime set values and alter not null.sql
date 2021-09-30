ceb0e7ca2b5afb3192e8f78b092dc0f73e96db09
Peter Tandrup <71757561+prtandrup@users.noreply.github.com>
2021-09-30 13:02:25 +0200

    Fix: Alter `EndDateTime` to NOT NULL in `DefaultChargeLink` and `ChargePeriodDetails` tables (#663)

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202109301025 ChargePeriodDetails enddatetime set values and alter not null.sql
@@ -0,0 +1,13 @@
+UPDATE [Charges].[ChargePeriodDetails]
+SET EndDateTime = '9999-12-31 23:59:59' -- Equivalent to InstantExtensions.TimeOrEndDefault()
+WHERE EndDateTime is null
+
+DROP INDEX [IX_ChargeId_StartDateTime_EndDateTime_Retired] ON [Charges].[ChargePeriodDetails]
+GO
+
+ALTER TABLE [Charges].[ChargePeriodDetails] ALTER COLUMN EndDateTime datetime2 NOT NULL
+GO
+
+CREATE INDEX [IX_ChargeId_StartDateTime_EndDateTime_Retired] ON [Charges].[ChargePeriodDetails]
+    (ChargeId DESC, StartDateTime DESC, EndDateTime DESC, Retired DESC);
+GO
