a4af778e299dea64c53e2fde4e06d89c3d15fa52
Peter Tandrup <PTA@energinet.dk>
2021-09-30 11:04:00 +0200

    Changed existing indexes to drop to i1

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202109301025 ChargePeriodDetails enddatetime set values and alter not null.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202109301025 ChargePeriodDetails enddatetime set values and alter not null.sql
@@ -2,7 +2,7 @@ UPDATE [Charges].[ChargePeriodDetails]
 SET EndDateTime = '9999-12-31 23:59:59' -- Equivalent to InstantExtensions.TimeOrEndDefault()
 WHERE EndDateTime is null
 
-DROP INDEX [IX_ChargeRowId_StartDateTime_EndDateTime_Retired] ON [Charges].[ChargePeriodDetails]
+DROP INDEX [i1] ON [Charges].[ChargePeriodDetails]
 GO
 
 ALTER TABLE [Charges].[ChargePeriodDetails] ALTER COLUMN EndDateTime datetime2 NOT NULL
