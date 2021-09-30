3d7e0dd29e268c026247b9e0160f10ea582c41fc
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-09-30 08:34:42 +0200

    Remove unused service bus auth rules

--- build/infrastructure/main/sb-charges.tf
+++ build/infrastructure/main/sb-charges.tf
@@ -46,26 +46,6 @@ module "sbt_command_received" {
   dependencies        = [module.sbn_charges]
 }
 
-module "sbtar_command_received_listener" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-received-listener"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  listen                    = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_received.name
-}
-
-module "sbtar_command_received_sender" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-received-sender"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  send                      = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_received.name
-}
-
 resource "azurerm_servicebus_subscription" "sbs_command_received" {
   name                = "sbs-command-received"
   resource_group_name = data.azurerm_resource_group.main.name
@@ -80,26 +60,6 @@ module "sbt_command_accepted" {
   namespace_name      = module.sbn_charges.name
   resource_group_name = data.azurerm_resource_group.main.name
   dependencies        = [module.sbn_charges]
-}
-
-module "sbtar_command_accepted_listener" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-accepted-listener"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  listen                    = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_accepted.name
-}
-
-module "sbtar_command_accepted_sender" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-accepted-sender"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  send                      = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_accepted.name
 }
 
 resource "azurerm_servicebus_subscription" "sbs_command_accepted" {
@@ -118,42 +78,12 @@ module "sbt_command_rejected" {
   dependencies        = [module.sbn_charges]
 }
 
-module "sbtar_link_command_received_sender" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.3.0"
-  name                      = "sbtar-link-command-received-sender"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  send                      = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_link_command_received.name
-}
-
-module "sbtar_link_command_received_listener" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.3.0"
-  name                      = "sbtar-link-command-received-listener"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  listen                    = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_link_command_received.name
-}
-
 module "sbtar_command_rejected_listener" {
   source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
   name                      = "sbtar-command-rejected-listener"
   namespace_name            = module.sbn_charges.name
   resource_group_name       = data.azurerm_resource_group.main.name
   listen                    = true
-  dependencies              = [module.sbn_charges]
-  topic_name                = module.sbt_command_rejected.name
-}
-
-module "sbtar_command_rejected_sender" {
-  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                      = "sbtar-command-rejected-sender"
-  namespace_name            = module.sbn_charges.name
-  resource_group_name       = data.azurerm_resource_group.main.name
-  send                      = true
   dependencies              = [module.sbn_charges]
   topic_name                = module.sbt_command_rejected.name
 }
