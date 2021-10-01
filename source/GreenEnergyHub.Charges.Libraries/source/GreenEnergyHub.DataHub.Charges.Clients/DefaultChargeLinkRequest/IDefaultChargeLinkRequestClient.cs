acbc0dfcaf16f49135b56dc898428705d97b08ad
Lasse Nielsen <lrn@energinet.dk>
2021-10-01 14:34:37 +0200

    added additional documentation to the Client package

--- source/GreenEnergyHub.Charges.Libraries/source/GreenEnergyHub.DataHub.Charges.Clients/DefaultChargeLinkRequest/IDefaultChargeLinkRequestClient.cs
+++ source/GreenEnergyHub.Charges.Libraries/source/GreenEnergyHub.DataHub.Charges.Clients/DefaultChargeLinkRequest/IDefaultChargeLinkRequestClient.cs
@@ -1,12 +1,26 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
 
 namespace GreenEnergyHub.DataHub.Charges.Libraries.DefaultChargeLinkRequest
 {
     public interface IDefaultChargeLinkRequestClient
     {
         /// <summary>
-        /// Calling this method with required parameters will request the Charges domain to create
-        /// Default charge links based on the supplied meteringPointId's MeteringPointType.
+        /// Request the Charges domain to create default charge links
+        /// based on the supplied meteringPointIds entity's MeteringPointType.
         /// </summary>
         /// <param name="meteringPointId"></param>
         /// <param name="correlationId"></param>
