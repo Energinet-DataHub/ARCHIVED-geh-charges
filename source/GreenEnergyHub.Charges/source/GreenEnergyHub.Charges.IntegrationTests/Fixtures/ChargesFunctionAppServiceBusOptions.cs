017cc3beb351aba7c17e080c9798096e681a56ce
Dan Stenrøjl <51327761+dstsyst@users.noreply.github.com>
2021-10-05 11:28:12 +0200

    POC for integration testing Azure Functions locally (#672)
    * What: Script to start/stop function apps
    Why: Fast way of starting/stopping all functions in staging env
    
    * What: Updated StyleCop  reference in Directory.Build.props
    Why: Misconfiguration lead to invalid dependency on StyleCop (consumer should not be forced to use StyleCop).
    
    * What: Added empty TestCommon project
    Why: For reuseable components to support integration tests of a Function App
    
    * What: Change StyleCop configuration
    Why: To get error if file is missing file header
    
    * What: Updated TestCommon projects
    Why: To cleanup structure and naming
    
    * What: Remove FxCop Analyzers
    Why: It is deprecated
    
    * What: AzuriteManager tests
    Why: To verify AzuriteManager
    
    * What: Add FunctionHostManager and dependencies
    Why: Supports testing of function app
    
    * What: Implemented FunctionAppTestBase and dependencies
    Why: Support integration testing of functions
    
    * What: Added ChargeReceiverFunctionAppFixture
    Why: To support integration tests of ChargeReceiver
    
    * What: Test .NET 5.0 function
    Why: To verify FunctionHostManager can be used for both .NET 3.1 and .NET 5.0
    
    * What: Additional comment in ChargeReceiverFunctionAppFixture
    Whe: To explain usage expicit
    
    * What: Multiple fixtures
    Why: To be able to overwrite FunctionApplicationPath
    
    * What: Added comment
    Why: To explain the usage of the test collection
    
    * What: Renamed folder and projects
    Why: Follow naming of other folders/projects
    
    * What: Fix project reference
    Why: Build fails
    
    * What: Copied Service Bus Listener Mock code
    Why: Simple fluent API for listening for service bus messages
    
    * What: Updated Directory.Build.props
    Why: To avoid forcing dependencies to Analyzers on consumer
    
    * What: CollectionFixture documentation
    Why: Was missing
    
    * What: Added source for TestCommon
    Why: To reuse a bunch of components
    
    * Rebased with main
    
    * Do not copy sample file to output
    
    * What: Aligned test with changes from main
    Why: So they are green
    
    * What: Removed cmd to start/stop functions
    Why: Not needed anymore
    
    * What: Integration test of charge ingestion
    Why: To prove it works
    
    * What: Refactored function integration tests
    Why: Improve maintainability
    
    * What: Reduce wait time
    Why: Optimize test execution time
    
    * What: Added comment
    Why: Explain use of environment setting
    
    * What: Add example of extracting body
    Why: To show developers how this can be done
    
    * What: Extract service bus namespace from local settings
    Why: To reuse namespace already provisioned
    
    * What: Extract retrieval of namespace into method
    Why: Maintainability
    
    * Comment
    
    * What: Manage database from tests
    Why: So function uses the database we control
    
    * What: Cleanup integrationtest.local.json
    Why: Only keep settings that is necessary
    
    * What: Function App Host Path
    Why: Not sure what this value is on other machines
    
    * What: Fix link to xUnit collection fixture
    Why: Link was broken
    
    * What: Comment on timeout
    Why: So we don't forget
    
    * What: Update comment for FunctionAppHostPath
    Why: Make it clear that we expect developers to use nvm for managing Node.js + npm and modules
    
    * What: Added license to sln file again
    
    * What: Reverted changes to solution file
    
    * What: Added TestCommon solution folder and projects
    
    * What: Move projects to solution folders
    
    * What: Added license header to solution (again)
    
    * What: Updated TestCommon test projects to .NET 5.0
    
    * What: Removed unnecessary TODO's
    
    * What: Implement OnConfigureHostSettings
    
    * What: Added IsTestProject property to TestCommon libraries

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/Fixtures/ChargesFunctionAppServiceBusOptions.cs
@@ -0,0 +1,122 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using GreenEnergyHub.Charges.FunctionHost.Common;
+using GreenEnergyHub.FunctionApp.TestCommon;
+using GreenEnergyHub.FunctionApp.TestCommon.FunctionAppHost;
+using Squadron;
+using Squadron.AzureCloud;
+
+namespace GreenEnergyHub.Charges.IntegrationTests.Fixtures
+{
+    public class ChargesFunctionAppServiceBusOptions : AzureCloudServiceBusOptions
+    {
+        public const string PostOfficeTopicKey = "postOffice";
+
+        public const string PostOfficeTopicSubscriptionName = "defaultSubscription";
+
+        public override void Configure(ServiceBusOptionsBuilder builder)
+        {
+            builder.SetConfigResolver(ConfigurationResolver);
+
+            var domainEventListenerNamespace = GetNamespaceFromSetting(EnvironmentSettingNames.DomainEventSenderConnectionString);
+
+            builder
+                .Namespace(domainEventListenerNamespace)
+                .AddTopic(PostOfficeTopicKey)
+                .AddSubscription(PostOfficeTopicSubscriptionName);
+        }
+
+        private static string GetNamespaceFromSetting(string settingName)
+        {
+            var localSettingsSnapshot = new FunctionAppHostConfigurationBuilder().BuildLocalSettingsConfiguration();
+            var domainEventListenerConnectionString = localSettingsSnapshot.GetValue(settingName);
+
+            // Example connection string: 'Endpoint=sb://xxx.servicebus.windows.net/;'
+            var namespaceMatchPattern = @"Endpoint=sb://(.*?).servicebus.windows.net/";
+            var match = Regex.Match(domainEventListenerConnectionString, namespaceMatchPattern, RegexOptions.IgnoreCase);
+            var domainEventListenerNamespace = match.Groups[1].Value;
+            return domainEventListenerNamespace;
+        }
+
+        private AzureResourceConfiguration ConfigurationResolver()
+        {
+            ConfigureEnvironmentVariables();
+
+            var secret = Environment.GetEnvironmentVariable("CLIENT_SECRET") ?? string.Empty;
+            var clientId = Environment.GetEnvironmentVariable("CLIENT_ID") ?? string.Empty;
+            var tenantId = Environment.GetEnvironmentVariable("TENANT_ID") ?? string.Empty;
+            var defaultLocation = Environment.GetEnvironmentVariable("DEFAULT_LOCATION") ?? string.Empty;
+            var resourceGroup = Environment.GetEnvironmentVariable("RESOURCE_GROUP_NAME") ?? string.Empty;
+            var subscriptionId = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID") ?? string.Empty;
+
+            if (string.IsNullOrWhiteSpace(secret)
+                || string.IsNullOrWhiteSpace(clientId)
+                || string.IsNullOrWhiteSpace(tenantId)
+                || string.IsNullOrWhiteSpace(defaultLocation)
+                || string.IsNullOrWhiteSpace(resourceGroup)
+                || string.IsNullOrWhiteSpace(subscriptionId))
+            {
+                var errormessage =
+                    $"{nameof(ChargesFunctionAppServiceBusOptions)} Missing configuration: " +
+                    $"Length of {nameof(secret)}: {secret.Length}," +
+                    $"Length of {nameof(clientId)}: {clientId.Length}," +
+                    $"Length of {nameof(tenantId)}: {tenantId.Length}," +
+                    $"Length of {nameof(subscriptionId)}: {subscriptionId.Length}," +
+                    $"Default location: {defaultLocation}, {resourceGroup}";
+
+                throw new InvalidOperationException(errormessage);
+            }
+
+            var azureResourceConfiguration = new AzureResourceConfiguration
+            {
+                Credentials = new AzureCredentials
+                {
+                    Secret = secret,
+                    ClientId = clientId,
+                    TenantId = tenantId,
+                },
+                DefaultLocation = defaultLocation,
+                ResourceGroup = resourceGroup,
+                SubscriptionId = subscriptionId,
+            };
+
+            return azureResourceConfiguration;
+        }
+
+        /// <summary>
+        /// EnvironmentVariables are not automatically loaded when running xUnit integrationstests.
+        /// This method follows the suggested workaround mentioned here:
+        /// https://github.com/Azure/azure-functions-host/issues/6953
+        /// </summary>
+        private static void ConfigureEnvironmentVariables()
+        {
+            var path = Path.GetDirectoryName(typeof(ChargesFunctionAppServiceBusOptions).Assembly.Location);
+            var settingsFile = Path.Join(path, "integrationtest.local.settings.json");
+            if (!File.Exists(settingsFile))
+                return;
+
+            var json = File.ReadAllText(settingsFile);
+            var parsed = Newtonsoft.Json.Linq.JObject.Parse(json).Value<Newtonsoft.Json.Linq.JObject>("Values");
+
+            foreach (var item in parsed!)
+            {
+                Environment.SetEnvironmentVariable(item.Key, item.Value?.ToString());
+            }
+        }
+    }
+}
