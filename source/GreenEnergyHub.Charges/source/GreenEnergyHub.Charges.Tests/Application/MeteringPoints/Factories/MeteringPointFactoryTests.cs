2318b3914f919bd216f6300a32d7d04cf751c0dd
Peter Tandrup <PTA@energinet.dk>
2021-10-06 08:57:19 +0200

    Add unit test for MeteringPointFactory

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/MeteringPoints/Factories/MeteringPointFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreenEnergyHub.Charges.Domain.MeteringPointCreatedEvents;
+using GreenEnergyHub.Charges.Domain.MeteringPoints;
+using GreenEnergyHub.TestHelpers;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.Tests.Application.MeteringPoints.Factories
+{
+    [UnitTest]
+    public class MeteringPointFactoryTests
+    {
+        [Fact]
+        public void Create_WhenCalledWithNull_ThrowsException()
+        {
+            ConsumptionMeteringPointCreatedEvent? input = null;
+            Assert.Throws<ArgumentNullException>(() => MeteringPointFactory.Create(input!));
+        }
+    }
+}
