c01e13d59f0ae9934d95e90d1b8de245fcdf4477
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-10-12 16:41:58 +0200

    Add serializer from temporary object to XML stream

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure/ChargeLinks/Cim/ChargeLinkPostOfficeSerializer.cs
@@ -0,0 +1,180 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using GreenEnergyHub.Charges.Application;
+using GreenEnergyHub.Charges.Application.ChargeLinks.MessageHub;
+using GreenEnergyHub.Charges.Core.DateTime;
+using GreenEnergyHub.Charges.Domain.ChargeLinks;
+using GreenEnergyHub.Charges.Domain.MarketParticipants;
+using GreenEnergyHub.Charges.Infrastructure.MarketDocument.Cim;
+using NodaTime;
+
+namespace GreenEnergyHub.Charges.Infrastructure.ChargeLinks.Cim
+{
+    public class ChargeLinkPostOfficeSerializer : IChargeLinkPostOfficeSerializer
+    {
+        private IHubSenderConfiguration _hubSenderConfiguration;
+        private IClock _clock;
+
+        public ChargeLinkPostOfficeSerializer(
+            IHubSenderConfiguration hubSenderConfiguration,
+            IClock clock)
+        {
+            _hubSenderConfiguration = hubSenderConfiguration;
+            _clock = clock;
+        }
+
+        public async Task<Stream> SerializeAsync(IEnumerable<ChargeLinkTransmissionDto> chargeLinks)
+        {
+            var stream = new MemoryStream();
+
+            var document = GetDocument(chargeLinks);
+            await document.SaveAsync(stream, SaveOptions.None, CancellationToken.None);
+
+            stream.Position = 0;
+
+            return stream;
+        }
+
+        private XDocument GetDocument(IEnumerable<ChargeLinkTransmissionDto> chargeLinks)
+        {
+            XNamespace cimNamespace = CimChargeLinkConstants.NotifyNamespace;
+            XNamespace xmlSchemaNamespace = CimMarketDocumentConstants.SchemaValidationNamespace;
+            XNamespace xmlSchemaLocation = CimChargeLinkConstants.NotifySchemaLocation;
+
+            return new XDocument(
+                new XElement(
+                    cimNamespace + CimChargeLinkConstants.NotifyRootElement,
+                    new XAttribute(
+                        XNamespace.Xmlns + CimMarketDocumentConstants.SchemaNamespaceAbbreviation,
+                        xmlSchemaNamespace),
+                    new XAttribute(
+                        XNamespace.Xmlns + CimMarketDocumentConstants.CimNamespaceAbbreviation,
+                        cimNamespace),
+                    new XAttribute(
+                        xmlSchemaNamespace + CimMarketDocumentConstants.SchemaLocation,
+                        xmlSchemaLocation),
+                    GetMarketDocumentHeader(cimNamespace, chargeLinks.First()), // Note: The list will always have same recipient and business reason code, so we just take those values from the first element
+                    GetActivityRecords(cimNamespace, chargeLinks)));
+        }
+
+        private IEnumerable<XElement> GetMarketDocumentHeader(XNamespace cimNamespace, ChargeLinkTransmissionDto chargeLink)
+        {
+            return new List<XElement>()
+            {
+                new XElement(cimNamespace + CimMarketDocumentConstants.Id, Guid.NewGuid()),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.Type,
+                    DocumentTypeMapper.Map(DocumentType.NotifyBillingMasterData)),
+                new XElement(
+                    cimNamespace +
+                    CimMarketDocumentConstants.BusinessReasonCode,
+                    BusinessReasonCodeMapper.Map(chargeLink.BusinessReasonCode)),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.IndustryClassification,
+                    IndustryClassificationMapper.Map(IndustryClassification.Electricity)),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.SenderId,
+                    new XAttribute(
+                        CimMarketDocumentConstants.CodingScheme,
+                        CodingSchemeMapper.Map(CodingScheme.GS1)),
+                    _hubSenderConfiguration.GetSenderMarketParticipant().Id),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.SenderBusinessProcessRole,
+                    MarketParticipantRoleMapper.Map(
+                        _hubSenderConfiguration.GetSenderMarketParticipant().BusinessProcessRole)),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.RecipientId,
+                    new XAttribute(
+                        CimMarketDocumentConstants.CodingScheme,
+                        CodingSchemeMapper.Map(CodingScheme.GS1)),
+                    chargeLink.Recipient),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.RecipientBusinessProcessRole,
+                    MarketParticipantRoleMapper.Map(chargeLink.MarketParticipantRole)),
+                new XElement(
+                    cimNamespace + CimMarketDocumentConstants.CreatedDateTime,
+                    _clock.GetCurrentInstant().ToString()),
+            };
+        }
+
+        private static IEnumerable<XElement> GetActivityRecords(
+            XNamespace cimNamespace,
+            IEnumerable<ChargeLinkTransmissionDto> chargeLinks)
+        {
+            return chargeLinks.Select(chargeLink => GetActivityRecord(cimNamespace, chargeLink));
+        }
+
+        private static XElement GetActivityRecord(
+            XNamespace cimNamespace,
+            ChargeLinkTransmissionDto chargeLink)
+        {
+            return new XElement(
+                cimNamespace + CimMarketDocumentConstants.MarketActivityRecord,
+                new XElement(cimNamespace + CimChargeLinkConstants.MarketActivityRecordId, Guid.NewGuid().ToString()),
+                new XElement(
+                    cimNamespace + CimChargeLinkConstants.MeteringPointId,
+                    new XAttribute(
+                        CimMarketDocumentConstants.CodingScheme,
+                        CodingSchemeMapper.Map(CodingScheme.GS1)),
+                    chargeLink.MeteringPointId),
+                new XElement(cimNamespace + CimChargeLinkConstants.StartDateTime, chargeLink.ValidFrom.ToString()),
+                GetEndDateTimeOnlyIfNotEndDefault(cimNamespace, chargeLink.ValidTo),
+                GetChargeGroupElement(cimNamespace, chargeLink));
+        }
+
+        private static IEnumerable<XElement> GetEndDateTimeOnlyIfNotEndDefault(XNamespace cimNamespace, Instant endDateTime)
+        {
+            return endDateTime.IsEndDefault()
+                ? new List<XElement>()
+                : new List<XElement>
+                {
+                    new XElement(cimNamespace + CimChargeLinkConstants.EndDateTime, endDateTime.ToString()),
+                };
+        }
+
+        private static XElement GetChargeGroupElement(
+            XNamespace cimNamespace,
+            ChargeLinkTransmissionDto chargeLink)
+        {
+            return new XElement(
+                cimNamespace + CimChargeLinkConstants.ChargeGroup,
+                GetChargeTypeElement(cimNamespace, chargeLink));
+        }
+
+        private static XElement GetChargeTypeElement(
+            XNamespace cimNamespace,
+            ChargeLinkTransmissionDto chargeLink)
+        {
+            return new XElement(
+                cimNamespace + CimChargeLinkConstants.ChargeTypeElement,
+                new XElement(
+                    cimNamespace + CimChargeLinkConstants.Owner,
+                    new XAttribute(
+                        CimMarketDocumentConstants.CodingScheme,
+                        CodingSchemeMapper.Map(CodingScheme.GS1)),
+                    chargeLink.Owner),
+                new XElement(cimNamespace + CimChargeLinkConstants.ChargeType, ChargeTypeMapper.Map(chargeLink.ChargeType)),
+                new XElement(cimNamespace + CimChargeLinkConstants.ChargeId, chargeLink.ChargeId),
+                new XElement(cimNamespace + CimChargeLinkConstants.Factor, chargeLink.Factor));
+        }
+    }
+}
---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Infrastructure/ChargeLinks/Cim/ChargeLinkPostOfficeSerializerTests.cs
@@ -0,0 +1,98 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using GreenEnergyHub.Charges.Application;
+using GreenEnergyHub.Charges.Domain.ChargeLinks;
+using GreenEnergyHub.Charges.Domain.Charges;
+using GreenEnergyHub.Charges.Domain.MarketParticipants;
+using GreenEnergyHub.Charges.Infrastructure.ChargeLinks.Cim;
+using GreenEnergyHub.TestHelpers;
+using Moq;
+using NodaTime;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.Tests.Infrastructure.ChargeLinks.Cim
+{
+    [UnitTest]
+    public class ChargeLinkPostOfficeSerializerTests
+    {
+        private const int NoOfLinksInBundle = 10;
+
+        [Theory/*(Skip = "Manually run test to save the generated file to disk")*/]
+        [InlineAutoDomainData]
+        public async Task SerializeAsync_WhenCalled_ReturnsSerializedStream(
+            [NotNull] [Frozen] Mock<IHubSenderConfiguration> hubSenderConfigration,
+            [NotNull] [Frozen] Mock<IClock> clock,
+            [NotNull] ChargeLinkPostOfficeSerializer sut)
+        {
+            hubSenderConfigration.Setup(
+                    h => h.GetSenderMarketParticipant())
+                .Returns(new MarketParticipant
+                {
+                    Id = "5790001330552", BusinessProcessRole = MarketParticipantRole.MeteringPointAdministrator,
+                });
+
+            var currentTime = Instant.FromUtc(2021, 10, 12, 13, 37, 43).PlusNanoseconds(4);
+            clock.Setup(
+                    c => c.GetCurrentInstant())
+                .Returns(currentTime);
+
+            var chargeLinks = GetChargeLinks();
+
+            await using var stream = await sut.SerializeAsync(chargeLinks);
+
+            await using var fileStream = File.Create("C:\\Temp\\Test" + Guid.NewGuid() + ".txt");
+
+            await stream.CopyToAsync(fileStream);
+        }
+
+        private List<ChargeLinkTransmissionDto> GetChargeLinks()
+        {
+            var chargeLinks = new List<ChargeLinkTransmissionDto>();
+
+            for (var i = 1; i <= NoOfLinksInBundle; i++)
+            {
+                chargeLinks.Add(GetChargeLink(i));
+            }
+
+            return chargeLinks;
+        }
+
+        private ChargeLinkTransmissionDto GetChargeLink(int no)
+        {
+            var validTo = no % 2 == 0 ?
+                Instant.FromUtc(9999, 12, 31, 23, 59, 59) :
+                Instant.FromUtc(2021, 4, 30, 22, 0, 0);
+
+            return new ChargeLinkTransmissionDto(
+                "TestRecipient1111",
+                MarketParticipantRole.GridAccessProvider,
+                BusinessReasonCode.UpdateMasterDataSettlement,
+                "Charge" + no,
+                ChargeType.Tariff,
+                "MeteringPoint" + no,
+                "Owner" + no,
+                no,
+                Instant.FromUtc(2020, 12, 31, 23, 0, 0),
+                validTo);
+        }
+    }
+}
