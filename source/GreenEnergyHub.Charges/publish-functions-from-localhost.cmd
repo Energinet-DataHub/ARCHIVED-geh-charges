b082447aa475c1a2614ccad02cd263356c5c58fe
BjarkeMeier <hej@bjarkemeier.dk>
2021-10-19 11:24:05 +0200

    Improve .NET publish script
    Sometimes (often?) syncing function triggers fails.
    So let's update database before so we make sure it happens.

--- source/GreenEnergyHub.Charges/publish-functions-from-localhost.cmd
+++ source/GreenEnergyHub.Charges/publish-functions-from-localhost.cmd
@@ -1,31 +1,31 @@
 @echo off
 
-echo =======================================================================================
+echo ========================================================================================
 echo Deploy Azure functions to your private sandbox
 echo .
 echo Assuming: Domain=charges, Environment=s
 echo *** Make sure that you created all local.settings.json settings files ***
-echo *** Deployments are executed in separate windows in order to execute in parallel ***
-echo =======================================================================================
+echo *** Bad request error while syncing function triggers doesn't seem to cause problems ***
+echo ========================================================================================
 
 setlocal
 
-set /p organization=Enter organization used with Terraform (perhaps your initials?): 
+set /p organization=Enter organization used with Terraform (perhaps your initials uppercased?): 
 set /p doBuild=Build solution ([y]/n)?
 rem If you don't know the password, perhaps you can obtain it from the configuration settings of the deployed ChargeCommandReceiver function in Azure portal
 set /p sqlPassword=Enter SQL password for 'gehdbadmin' to update db or empty to skip: 
 set /p functionHost=Deploy function host ([y]/n)?
 
-IF /I not "%doBuild%" == "n" (
-    rem Clean is necessary if e.g. a function project name has changed because otherwise both assemblies will be picked up by deployment
-    dotnet clean GreenEnergyHub.Charges.sln -c Release
-    dotnet build GreenEnergyHub.Charges.sln -c Release
-)
-
 IF not "%sqlPassword%" == "" (
     dotnet run --project source\GreenEnergyHub.Charges.ApplyDBMigrationsApp --configuration Release --^
         "Server=sqlsrv-charges-%organization%-s.database.windows.net;Database=sqldb-charges;Uid=gehdbadmin;Pwd=%sqlPassword%;TrustServerCertificate=true;Persist Security Info=True;"^
         includeSeedData includeTestData
+)
+
+IF /I not "%doBuild%" == "n" (
+    rem Clean is necessary if e.g. a function project name has changed because otherwise both assemblies will be picked up by deployment
+    dotnet clean GreenEnergyHub.Charges.sln -c Release
+    dotnet build GreenEnergyHub.Charges.sln -c Release
 )
 
 IF /I not "%functionHost%" == "n" (
