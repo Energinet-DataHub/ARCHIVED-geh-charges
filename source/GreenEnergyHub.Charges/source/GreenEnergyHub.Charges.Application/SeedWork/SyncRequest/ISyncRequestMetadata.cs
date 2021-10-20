fa446249e06148095f83ee64846f06562bd599ce
BjarkeMeier <hej@bjarkemeier.dk>
2021-10-20 08:15:08 +0200

    Document ISyncRequestMetaData

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/SeedWork/SyncRequest/ISyncRequestMetadata.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/SeedWork/SyncRequest/ISyncRequestMetadata.cs
@@ -14,8 +14,20 @@
 
 namespace GreenEnergyHub.Charges.Application.SeedWork.SyncRequest
 {
+    /// <summary>
+    /// Meta data available for synchronous inter-domain requests.
+    /// That is for requests in a request-response communication.
+    ///
+    /// This is in contrast to the asynchronous request-reply
+    /// communication pattern.
+    /// </summary>
     public interface ISyncRequestMetadata
     {
+        /// <summary>
+        /// Session ID is needed by the underlying communication
+        /// protocol (Azure Service Bus message sessions, see
+        /// https://docs.microsoft.com/en-us/azure/service-bus-messaging/message-sessions).
+        /// </summary>
         string SessionId { get; }
     }
 }
