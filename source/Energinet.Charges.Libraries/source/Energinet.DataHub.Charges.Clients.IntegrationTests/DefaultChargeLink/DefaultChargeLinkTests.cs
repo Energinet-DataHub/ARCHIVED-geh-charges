2050f05937e3ebfcda0a5c1c09aef830b48d1406
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2021-10-21 15:56:21 +0200

    DefaultChargeLinkClient create request integration test

---
+++ source/Energinet.Charges.Libraries/source/Energinet.DataHub.Charges.Clients.IntegrationTests/DefaultChargeLink/DefaultChargeLinkTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.Charges.Clients.CreateDefaultChargeMessagesLink.Tests.Fixtures;
+using Energinet.DataHub.Charges.Libraries.Common;
+using Energinet.DataHub.Charges.Libraries.DefaultChargeLink;
+using Energinet.DataHub.Charges.Libraries.Factories;
+using Energinet.DataHub.Charges.Libraries.Models;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ListenerMock;
+using FluentAssertions;
+using GreenEnergyHub.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.Charges.Clients.CreateDefaultChargeMessagesLink.Tests.DefaultChargeLink
+{
+    public static class DefaultChargeLinkTests
+    {
+        [Collection(nameof(ChargesClientsCollectionFixture))]
+        [SuppressMessage("ReSharper", "CA1034", Justification = "done")] // TODO: Why is this necessary here, but not in Charges.IntegrationTests.Health.HealthStatusTests?
+        public class CreateDefaultChargeLinksRequestAsync : FunctionAppTestBase<ChargesClientsFixture>, IAsyncLifetime
+        {
+            private readonly string _serviceBusConnectionString;
+            private readonly string _replyToQueueName;
+            private readonly string _requestQueueName;
+
+            public CreateDefaultChargeLinksRequestAsync(ChargesClientsFixture fixture, ITestOutputHelper testOutputHelper)
+                : base(fixture, testOutputHelper)
+            {
+                _serviceBusConnectionString = GetEnvironmentVariable(
+                    EnvironmentSettingNames.IntegrationEventSenderConnectionString, string.Empty);
+                _replyToQueueName = GetEnvironmentVariable(EnvironmentSettingNames.CreateLinkReplyQueueName, string.Empty);
+                _requestQueueName = GetEnvironmentVariable(EnvironmentSettingNames.CreateLinkRequestQueueName, string.Empty);
+            }
+
+            public Task InitializeAsync()
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task DisposeAsync()
+            {
+                Fixture.ServiceBusListenerMock.ResetMessageHandlersAndReceivedMessages();
+                return Task.CompletedTask;
+            }
+
+            [Theory]
+            [AutoDomainData]
+            public async Task When_CreateDefaultChargeLinksRequestAsync_Then_RequestIsSendToCharges(
+                CreateDefaultChargeLinksDto createDefaultChargeLinksDto, string correlationId)
+            {
+                // Arrange
+                var body = new BinaryData(Array.Empty<byte>());
+                string actualCorrelationId = string.Empty;
+                using var isMessageReceivedEvent = await Fixture.ServiceBusListenerMock
+                    .WhenAny()
+                    .VerifyOnceAsync(receivedMessage =>
+                    {
+                        body = receivedMessage.Body;
+                        actualCorrelationId = receivedMessage.CorrelationId;
+                        return Task.CompletedTask;
+                    }).ConfigureAwait(false);
+
+                var serviceBusRequestSenderFactory = new ServiceBusRequestSenderFactory();
+                await using var serviceBusClient = new ServiceBusClient(_serviceBusConnectionString);
+                await using var sut = new DefaultChargeLinkClient(
+                    serviceBusClient, serviceBusRequestSenderFactory, _replyToQueueName, _requestQueueName);
+
+                // Act
+                await sut.CreateDefaultChargeLinksRequestAsync(createDefaultChargeLinksDto, correlationId).ConfigureAwait(false);
+
+                // Assert
+                // => Service Bus (timeout should not be more than 5 secs).
+                var isMessageReceived = isMessageReceivedEvent.Wait(TimeSpan.FromSeconds(5));
+                isMessageReceived.Should().BeTrue();
+                actualCorrelationId.Should().Be(correlationId);
+
+                body.Should().NotBeNull();
+            }
+        }
+
+        private static string GetEnvironmentVariable(string name, string defaultValue)
+            => Environment.GetEnvironmentVariable(name) is { Length: > 0 } v ? v : defaultValue;
+    }
+}
