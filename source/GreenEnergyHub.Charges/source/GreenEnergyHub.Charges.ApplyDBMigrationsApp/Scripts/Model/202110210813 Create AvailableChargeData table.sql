454333eafb8ddd8b3f064a825e1fc597af02ed01
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-10-21 11:49:38 +0200

    make PK non clustered

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202110210813 Create AvailableChargeData table.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202110210813 Create AvailableChargeData table.sql
@@ -1,18 +1,25 @@
 CREATE TABLE [MessageHub].AvailableChargeData (
-	Id uniqueidentifier not null primary key,
-
+	Id uniqueidentifier not null,
     AvailableDataReferenceId uniqueidentifier not null,
     TaxIndicator bit not null,
     TransparentInvoicing bit not null,
     VatClassification int not null,
     RequestTime datetime2 not null,
-);
+    CONSTRAINT [PK_AvailableChargeData] PRIMARY KEY NONCLUSTERED
+        (
+         ID ASC
+            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 CREATE TABLE [MessageHub].AvailableChargeDataPoints (
-    Id uniqueidentifier not null primary key,
-    AvailableChargeDataId uniqueidentifier not null,
+    Id uniqueidentifier not null,
+    AvailableChargeDataId uniqueidentifier not null FOREIGN KEY REFERENCES [MessageHub].AvailableChargeData(Id),
     Position int not null,
     Price decimal(14,6) not null
-)
+        CONSTRAINT [PK_AvailableChargeDataPoints] PRIMARY KEY NONCLUSTERED
+            (
+             ID ASC
+                ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
