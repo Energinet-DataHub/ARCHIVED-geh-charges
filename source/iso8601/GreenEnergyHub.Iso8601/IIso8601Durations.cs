c1486bee3f24ae1354e6ffe0d7b214f1904c8ca9
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-10-25 11:47:51 +0200

    Add support for sticking to fixed duration intervals and upgrade Iso8601Durations to .NET 5.0

--- source/iso8601/GreenEnergyHub.Iso8601/IIso8601Durations.cs
+++ source/iso8601/GreenEnergyHub.Iso8601/IIso8601Durations.cs
@@ -29,5 +29,22 @@ namespace GreenEnergyHub.Iso8601
         /// <param name="numberOfDurations">Number of durations to add</param>
         /// <returns>End time for time interval</returns>
         Instant AddDuration(Instant startInstant, string duration, int numberOfDurations);
+
+        /// <summary>
+        /// Retrieves the time which occur by adding the required number of durations
+        /// to the start time, making sure that durations higher than 0 snap to normal
+        /// fixed intervals defined by the duration.
+        ///
+        /// If numberOfDurations is zero then the start is simply returned even though
+        /// it might not match a fixed duration start. This will allows us to support
+        /// irregular time series points where the first position might be off from the
+        /// otherwise fixed intervals
+        /// </summary>
+        /// <param name="startInstant">The start instant for the time interval</param>
+        /// <param name="duration">ISO 8601 duration</param>
+        /// <param name="numberOfDurations">Number of durations to add</param>
+        /// <returns>The time going forward to the fixed start of the interval by the
+        /// requested number of durations</returns>
+        Instant GetTimeFixedToDuration(Instant startInstant, string duration, int numberOfDurations);
     }
 }
