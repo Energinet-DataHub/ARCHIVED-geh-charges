d230af05d7ab5c3c33c3e9b7b255bd5b1d88d956
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-11-11 14:44:37 +0100

    build: add vars to functionhost

--- build/infrastructure/main/func-functionhost.tf
+++ build/infrastructure/main/func-functionhost.tf
@@ -24,56 +24,58 @@ module "func_functionhost" {
   application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
   app_settings                              = {
     # Region: Default Values
-    WEBSITE_ENABLE_SYNC_UPDATE_SITE                      = true
-    WEBSITE_RUN_FROM_PACKAGE                             = 1
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE                  = true
-    FUNCTIONS_WORKER_RUNTIME                             = "dotnet-isolated"
+    WEBSITE_ENABLE_SYNC_UPDATE_SITE                                 = true
+    WEBSITE_RUN_FROM_PACKAGE                                        = 1
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE                             = true
+    FUNCTIONS_WORKER_RUNTIME                                        = "dotnet-isolated"
 
-    LOCAL_TIMEZONENAME                                   = "Europe/Copenhagen"
-    CURRENCY                                             = "DKK"
-    CHARGE_DB_CONNECTION_STRING                          = local.CHARGE_DB_CONNECTION_STRING
-    DOMAINEVENT_SENDER_CONNECTION_STRING                 = module.sb_charges.primary_connection_strings["send"]
-    DOMAINEVENT_LISTENER_CONNECTION_STRING               = module.sb_charges.primary_connection_strings["listen"]
-    CHARGE_CREATED_TOPIC_NAME                            = data.azurerm_key_vault_secret.sbt_charge_created_name.value
-    CHARGE_PRICES_UPDATED_TOPIC_NAME                     = data.azurerm_key_vault_secret.sbt_charge_prices_updated_name.value
-    CHARGE_LINK_ACCEPTED_TOPIC_NAME                      = module.sbt_link_command_accepted.name
-    CHARGELINKACCEPTED_SUB_REPLIER                       = "chargelinkaccepted-sub-replier"
-    CHARGELINKACCEPTED_SUB_EVENTPUBLISHER                = "chargelinkaccepted-sub-eventpublisher"
-    CHARGELINKACCEPTED_SUB_DATAAVAILABLENOTIFIER         = "chargelinkaccepted-sub-dataavailablenotifier"
-    CHARGE_LINK_CREATED_TOPIC_NAME                       = data.azurerm_key_vault_secret.sbt_charge_link_created_name.value
-    CHARGE_LINK_RECEIVED_TOPIC_NAME                      = module.sbt_link_command_received.name
-    CHARGE_LINK_RECEIVED_SUBSCRIPTION_NAME               = "link-command-received-receiver"
-    COMMAND_ACCEPTED_TOPIC_NAME                          = module.sbt_command_accepted.name
-    COMMAND_ACCEPTED_RECEIVER_SUBSCRIPTION_NAME          = "charge-command-accepted-receiver"
-    CHARGEACCEPTED_SUB_DATAAVAILABLENOTIFIER             = "chargeaccepted-sub-dataavailablenotifier"
-    COMMAND_ACCEPTED_SUBSCRIPTION_NAME                   = "command-accepted"
-    COMMAND_RECEIVED_TOPIC_NAME                          = module.sbt_command_received.name
-    COMMAND_RECEIVED_SUBSCRIPTION_NAME                   = "command-received"
-    COMMAND_REJECTED_TOPIC_NAME                          = module.sbt_command_rejected.name
-    COMMAND_REJECTED_SUBSCRIPTION_NAME                   = "command-rejected"
-    CREATE_LINK_REQUEST_QUEUE_NAME                       = data.azurerm_key_vault_secret.sbq_create_link_request_name.value
-    CREATE_LINK_REPLY_QUEUE_NAME                         = data.azurerm_key_vault_secret.sbq_create_link_reply_name.value
-    CREATE_LINK_MESSAGES_REQUEST_QUEUE_NAME              = data.azurerm_key_vault_secret.sbq_create_link_messages_request_name.value
-    CREATE_LINK_MESSAGES_REPLY_QUEUE_NAME                = data.azurerm_key_vault_secret.sbq_create_link_messages_reply_name.value
-    CONSUMPTION_METERING_POINT_CREATED_TOPIC_NAME        = data.azurerm_key_vault_secret.sbt_consumption_metering_point_created_name.value
-    CONSUMPTION_METERING_POINT_CREATED_SUBSCRIPTION_NAME = data.azurerm_key_vault_secret.sbs_consumption_metering_point_created_sub_charges_name.value
+    LOCAL_TIMEZONENAME                                              = "Europe/Copenhagen"
+    CURRENCY                                                        = "DKK"
+    CHARGE_DB_CONNECTION_STRING                                     = local.CHARGE_DB_CONNECTION_STRING
+    DOMAINEVENT_SENDER_CONNECTION_STRING                            = module.sb_charges.primary_connection_strings["send"]
+    DOMAINEVENT_LISTENER_CONNECTION_STRING                          = module.sb_charges.primary_connection_strings["listen"]
+    CHARGE_CREATED_TOPIC_NAME                                       = data.azurerm_key_vault_secret.sbt_charge_created_name.value
+    CHARGE_PRICES_UPDATED_TOPIC_NAME                                = data.azurerm_key_vault_secret.sbt_charge_prices_updated_name.value
+    CHARGE_LINK_ACCEPTED_TOPIC_NAME                                 = module.sbt_link_command_accepted.name
+    CHARGELINKACCEPTED_SUB_REPLIER                                  = "chargelinkaccepted-sub-replier"
+    CHARGELINKACCEPTED_SUB_EVENTPUBLISHER                           = "chargelinkaccepted-sub-eventpublisher"
+    CHARGELINKACCEPTED_SUB_DATAAVAILABLENOTIFIER                    = "chargelinkaccepted-sub-dataavailablenotifier"
+    CHARGE_LINK_CREATED_TOPIC_NAME                                  = data.azurerm_key_vault_secret.sbt_charge_link_created_name.value
+    CHARGE_LINK_RECEIVED_TOPIC_NAME                                 = module.sbt_link_command_received.name
+    CHARGE_LINK_RECEIVED_SUBSCRIPTION_NAME                          = "link-command-received-receiver"
+    COMMAND_ACCEPTED_TOPIC_NAME                                     = module.sbt_command_accepted.name
+    COMMAND_ACCEPTED_RECEIVER_SUBSCRIPTION_NAME                     = "charge-command-accepted-receiver"
+    CHARGEACCEPTED_SUB_DATAAVAILABLENOTIFIER                        = "chargeaccepted-sub-dataavailablenotifier"
+    COMMAND_ACCEPTED_SUBSCRIPTION_NAME                              = "command-accepted"
+    COMMAND_RECEIVED_TOPIC_NAME                                     = module.sbt_command_received.name
+    COMMAND_RECEIVED_SUBSCRIPTION_NAME                              = "command-received"
+    COMMAND_REJECTED_TOPIC_NAME                                     = module.sbt_command_rejected.name
+    COMMAND_REJECTED_SUBSCRIPTION_NAME                              = "command-rejected"
+    DEFAULT_CHARGE_LINKS_DATA_AVAILABLE_NOTIFIED_TOPIC_NAME         = "sbt_default_charge_link_available_notified"
+    DEFAULT_CHARGE_LINKS_DATA_AVAILABLE_NOTIFIED_SUBSCRIPTION_NAME  = "default-charge-link-data-available-notified-sub-charges"
+    CREATE_LINK_REQUEST_QUEUE_NAME                                  = data.azurerm_key_vault_secret.sbq_create_link_request_name.value
+    CREATE_LINK_REPLY_QUEUE_NAME                                    = data.azurerm_key_vault_secret.sbq_create_link_reply_name.value
+    CREATE_LINK_MESSAGES_REQUEST_QUEUE_NAME                         = data.azurerm_key_vault_secret.sbq_create_link_messages_request_name.value
+    CREATE_LINK_MESSAGES_REPLY_QUEUE_NAME                           = data.azurerm_key_vault_secret.sbq_create_link_messages_reply_name.value
+    CONSUMPTION_METERING_POINT_CREATED_TOPIC_NAME                   = data.azurerm_key_vault_secret.sbt_consumption_metering_point_created_name.value
+    CONSUMPTION_METERING_POINT_CREATED_SUBSCRIPTION_NAME            = data.azurerm_key_vault_secret.sbs_consumption_metering_point_created_sub_charges_name.value
 
     # Shared resources
-    INTEGRATIONEVENT_SENDER_CONNECTION_STRING            = data.azurerm_key_vault_secret.sb_domain_relay_send_connection_string.value
-    INTEGRATIONEVENT_LISTENER_CONNECTION_STRING          = data.azurerm_key_vault_secret.sb_domain_relay_listen_connection_string.value
-    INTEGRATIONEVENT_MANAGER_CONNECTION_STRING           = data.azurerm_key_vault_secret.sb_domain_relay_manage_connection_string.value
+    INTEGRATIONEVENT_SENDER_CONNECTION_STRING                       = data.azurerm_key_vault_secret.sb_domain_relay_send_connection_string.value
+    INTEGRATIONEVENT_LISTENER_CONNECTION_STRING                     = data.azurerm_key_vault_secret.sb_domain_relay_listen_connection_string.value
+    INTEGRATIONEVENT_MANAGER_CONNECTION_STRING                      = data.azurerm_key_vault_secret.sb_domain_relay_manage_connection_string.value
 
     # Message Hub
-    POST_OFFICE_TOPIC_NAME                               = module.sbt_post_office.name
-    MESSAGEHUB_STORAGE_CONNECTION_STRING                 = data.azurerm_key_vault_secret.messagehub_storage_connection_string.value
-    MESSAGEHUB_STORAGE_CONTAINER                         = data.azurerm_key_vault_secret.messagehub_storage_container.value
-    MESSAGEHUB_DATAAVAILABLE_QUEUE                       = "dataavailable"
-    MESSAGEHUB_BUNDLEREQUEST_QUEUE                       = "charges"
-    MESSAGEHUB_BUNDLEREPLY_QUEUE                         = "charges-reply"
+    POST_OFFICE_TOPIC_NAME                                          = module.sbt_post_office.name
+    MESSAGEHUB_STORAGE_CONNECTION_STRING                            = data.azurerm_key_vault_secret.messagehub_storage_connection_string.value
+    MESSAGEHUB_STORAGE_CONTAINER                                    = data.azurerm_key_vault_secret.messagehub_storage_container.value
+    MESSAGEHUB_DATAAVAILABLE_QUEUE                                  = "dataavailable"
+    MESSAGEHUB_BUNDLEREQUEST_QUEUE                                  = "charges"
+    MESSAGEHUB_BUNDLEREPLY_QUEUE                                    = "charges-reply"
 	
   	# Hub identification
-    HUB_SENDER_ID                                        = "5790001330552"
-    HUB_SENDER_ROLE_INT_ENUM_VALUE                       = "7"
+    HUB_SENDER_ID                                                   = "5790001330552"
+    HUB_SENDER_ROLE_INT_ENUM_VALUE                                  = "7"
   }
   
   tags                                      = azurerm_resource_group.this.tags
