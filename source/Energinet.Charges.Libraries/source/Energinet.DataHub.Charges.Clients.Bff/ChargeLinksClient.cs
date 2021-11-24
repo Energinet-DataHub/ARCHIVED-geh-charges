5eb5e35ffe6343fb5ddfb42fa2e639246be136a3
Peter Tandrup <PTA@energinet.dk>
2021-11-24 13:07:09 +0100

    Add ChargeLinksClient

---
+++ source/Energinet.Charges.Libraries/source/Energinet.DataHub.Charges.Clients.Bff/ChargeLinksClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Energinet.DataHub.Charges.Clients.Abstractions;
+
+namespace Energinet.DataHub.Charges.Clients.Bff
+{
+    public sealed class ChargeLinksClient : IChargeLinksClient
+    {
+        private readonly HttpClient _httpClient;
+
+        internal ChargeLinksClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ChargeLinkDto?> GetChargeLinksByMeteringPointIdAsync(string meteringPointId)
+        {
+            var response = await _httpClient.GetAsync(new Uri($"ChargeLinks/GetChargeLinksByMeteringPointIdAsync/?meteringPointId={meteringPointId}", UriKind.Relative))
+                .ConfigureAwait(false);
+
+            return await response.Content.ReadFromJsonAsync<ChargeLinkDto>().ConfigureAwait(false);
+        }
+    }
+}
