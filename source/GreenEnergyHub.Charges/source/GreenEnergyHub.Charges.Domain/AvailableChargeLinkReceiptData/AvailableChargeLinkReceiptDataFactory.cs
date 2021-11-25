be4539bc1c8985144a0b9bb1bfbae7401bec8d41
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-11-25 15:41:19 +0100

    Add factory for charge link receipts

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/AvailableChargeLinkReceiptData/AvailableChargeLinkReceiptDataFactory.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GreenEnergyHub.Charges.Domain.AvailableData;
+using GreenEnergyHub.Charges.Domain.Dtos.ChargeLinksCommands;
+using GreenEnergyHub.Charges.Domain.Dtos.SharedDtos;
+using NodaTime;
+
+namespace GreenEnergyHub.Charges.Domain.AvailableChargeLinkReceiptData
+{
+    public class AvailableChargeLinkReceiptDataFactory : IAvailableChargeLinkReceiptDataFactory
+    {
+        public IReadOnlyCollection<AvailableChargeLinkReceiptData> CreateConfirmations(
+            ChargeLinksCommand command,
+            Instant requestDateTime,
+            Guid messageHubId)
+        {
+            return command.ChargeLinks.Select(
+                    link => new AvailableChargeLinkReceiptData(
+                        command.Document.Sender.Id, // The sender is now the recipient of the receipt
+                        command.Document.Sender.BusinessProcessRole,
+                        command.Document.BusinessReasonCode,
+                        requestDateTime,
+                        messageHubId,
+                        ReceiptStatus.Confirmed,
+                        link.OperationId,
+                        command.MeteringPointId,
+                        new List<AvailableChargeLinkReceiptDataReasonCode>()))
+                .ToList();
+        }
+    }
+}
