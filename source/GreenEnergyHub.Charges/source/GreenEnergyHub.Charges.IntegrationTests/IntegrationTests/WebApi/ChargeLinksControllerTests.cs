f1cdb7622d89bbeed2015c0599d3ba5188e496f8
Peter Tandrup <PTA@energinet.dk>
2021-11-25 08:34:27 +0100

    Added tests

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/IntegrationTests/WebApi/ChargeLinksControllerTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GreenEnergyHub.Charges.WebApi.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.IntegrationTests.IntegrationTests.WebApi
+{
+    [IntegrationTest]
+    public class ChargeLinksControllerTests : IClassFixture<WebApplicationFactory<Charges.WebApi.Startup>>
+    {
+        private readonly WebApplicationFactory<Charges.WebApi.Startup> _factory;
+
+        public ChargeLinksControllerTests(WebApplicationFactory<Charges.WebApi.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/ChargeLinks/GetChargeLinksByMeteringPointId?meteringPointId=1")]
+        public async Task GetChargeLinksByMeteringPointId_WhenIdHasChargeLinks_ReturnsOkAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var contentType = response.Content.Headers.ContentType!;
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", contentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/ChargeLinks/GetChargeLinksByMeteringPointId?meteringPointId=1")]
+        public async Task GetChargeLinksByMeteringPointId_WhenIdHasChargeLinks_ReturnsChargeLinks(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<List<ChargeLinkDto>>(jsonString, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            // Assert
+            Assert.Equal("40000", result![0].ChargeId);
+        }
+
+        [Theory]
+        [InlineData("/ChargeLinks/GetChargeLinksByMeteringPointId?meteringPointId=404")]
+        public async Task GetChargeLinksByMeteringPointId_WhenIdDoesNotExist_ReturnsNotFound(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/ChargeLinks/GetChargeLinksByMeteringPointId?meteringPointId=")]
+        public async Task GetChargeLinksByMeteringPointId_WhenNoIdInput_ReturnsBadRequest(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
