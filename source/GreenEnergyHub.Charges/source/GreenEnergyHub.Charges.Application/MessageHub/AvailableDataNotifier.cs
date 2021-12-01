18cdf76e6f948515da1e417d645767f9d7ce3251
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-12-01 13:45:31 +0100

    Add available data notifier

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/MessageHub/AvailableDataNotifier.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Energinet.DataHub.MessageHub.Client.DataAvailable;
+using Energinet.DataHub.MessageHub.Model.Model;
+using GreenEnergyHub.Charges.Domain.AvailableData;
+
+namespace GreenEnergyHub.Charges.Application.MessageHub
+{
+    public class AvailableDataNotifier<TAvailableData, TInputType> : IAvailableDataNotifier<TAvailableData, TInputType>
+        where TAvailableData : AvailableDataBase
+    {
+        private readonly IAvailableDataFactory<TAvailableData, TInputType> _availableDataFactory;
+        private readonly IAvailableDataRepository<TAvailableData> _availableDataRepository;
+        private readonly IAvailableDataNotificationFactory<TAvailableData> _availableDataNotificationFactory;
+        private readonly IDataAvailableNotificationSender _dataAvailableNotificationSender;
+        private readonly ICorrelationContext _correlationContext;
+
+        public AvailableDataNotifier(
+            IAvailableDataFactory<TAvailableData, TInputType> availableDataFactory,
+            IAvailableDataRepository<TAvailableData> availableDataRepository,
+            IAvailableDataNotificationFactory<TAvailableData> availableDataNotificationFactory,
+            IDataAvailableNotificationSender dataAvailableNotificationSender,
+            ICorrelationContext correlationContext)
+        {
+            _availableDataFactory = availableDataFactory;
+            _availableDataRepository = availableDataRepository;
+            _availableDataNotificationFactory = availableDataNotificationFactory;
+            _dataAvailableNotificationSender = dataAvailableNotificationSender;
+            _correlationContext = correlationContext;
+        }
+
+        public async Task NotifyAsync(TInputType input)
+        {
+            if (!ShouldSendMessage(input))
+                return;
+
+            var availableData = CreateAvailableData(input);
+
+            await StoreAvailableDataForLaterBundlingAsync(availableData).ConfigureAwait(false);
+
+            await NotifyMessageHubOfAvailableDataAsync(availableData).ConfigureAwait(false);
+        }
+
+        protected virtual bool ShouldSendMessage(TInputType availableData)
+        {
+            return true;
+        }
+
+        private IReadOnlyList<TAvailableData> CreateAvailableData(TInputType input)
+        {
+            return _availableDataFactory.Create(input);
+        }
+
+        private async Task StoreAvailableDataForLaterBundlingAsync(IReadOnlyList<TAvailableData> availableData)
+        {
+            await _availableDataRepository.StoreAsync(availableData).ConfigureAwait(false);
+        }
+
+        private async Task NotifyMessageHubOfAvailableDataAsync(IReadOnlyList<TAvailableData> availableData)
+        {
+            var notifications = CreateNotifications(availableData);
+
+            var dataAvailableNotificationSenderTasks = notifications
+                .Select(x => _dataAvailableNotificationSender.SendAsync(_correlationContext.Id, x));
+
+            await Task.WhenAll(dataAvailableNotificationSenderTasks).ConfigureAwait(false);
+        }
+
+        private IReadOnlyList<DataAvailableNotificationDto> CreateNotifications(
+            IReadOnlyList<TAvailableData> availableData)
+        {
+            return _availableDataNotificationFactory.Create(availableData);
+        }
+    }
+}
---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/MessageHub/AvailableDataNotifierTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using Energinet.DataHub.MessageHub.Client.DataAvailable;
+using Energinet.DataHub.MessageHub.Model.Model;
+using GreenEnergyHub.Charges.Application;
+using GreenEnergyHub.Charges.Application.MessageHub;
+using GreenEnergyHub.Charges.Domain.AvailableData;
+using GreenEnergyHub.Charges.TestCore.Attributes;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.Tests.Application.MessageHub
+{
+    [UnitTest]
+    public class CAvailableDataNotifierTests
+    {
+        [Theory]
+        [InlineAutoMoqData]
+        public async Task NotifyAsync_WhenGivenInput_Notifies(
+            [Frozen] Mock<IAvailableDataFactory<AvailableDataBase, object>> availableDataFactory,
+            [Frozen] Mock<IAvailableDataRepository<AvailableDataBase>> availableDataRepository,
+            [Frozen] Mock<IAvailableDataNotificationFactory<AvailableDataBase>> availableDataNotificationFactory,
+            [Frozen] Mock<IDataAvailableNotificationSender> dataAvailableNotificationSender,
+            [Frozen] Mock<ICorrelationContext> correlationContext,
+            object input,
+            List<AvailableDataBase> availableData,
+            IReadOnlyList<DataAvailableNotificationDto> notifications,
+            AvailableDataNotifier<AvailableDataBase, object> sut)
+        {
+            // Arrange
+            availableDataFactory.Setup(
+                    f => f.Create(input))
+                .Returns(availableData);
+
+            availableDataNotificationFactory.Setup(
+                    f => f.Create(availableData))
+                .Returns(notifications);
+
+            var correlationId = Guid.NewGuid().ToString();
+            correlationContext.Setup(c => c.Id)
+                .Returns(correlationId);
+
+            // Act
+            await sut.NotifyAsync(input);
+
+            // Assert
+            availableDataFactory.Verify(
+                f => f.Create(input),
+                Times.Once);
+
+            availableDataRepository.Verify(
+                r => r.StoreAsync(availableData),
+                Times.Once);
+
+            availableDataNotificationFactory.Verify(
+                f => f.Create(availableData),
+                Times.Once);
+
+            dataAvailableNotificationSender.Verify(
+                d => d.SendAsync(
+                    correlationId,
+                    It.IsAny<DataAvailableNotificationDto>()),
+                Times.Exactly(notifications.Count));
+        }
+    }
+}
