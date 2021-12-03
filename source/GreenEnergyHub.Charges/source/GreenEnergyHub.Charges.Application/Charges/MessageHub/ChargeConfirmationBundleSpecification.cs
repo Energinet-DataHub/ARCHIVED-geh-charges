34df1307103eead3f78ad3d3a7e484f563ae4782
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-12-03 10:56:51 +0100

    Add charge confirmation bundle specification

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/MessageHub/ChargeConfirmationBundleSpecification.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GreenEnergyHub.Charges.Application.MessageHub;
+using GreenEnergyHub.Charges.Domain.AvailableChargeReceiptData;
+using GreenEnergyHub.Charges.Domain.AvailableData;
+using GreenEnergyHub.Charges.Domain.Dtos.ChargeCommandAcceptedEvents;
+
+namespace GreenEnergyHub.Charges.Application.Charges.MessageHub
+{
+    public class ChargeConfirmationBundleSpecification : BundleSpecification<AvailableChargeReceiptData, ChargeCommandAcceptedEvent>
+    {
+        /// <summary>
+        /// The upper anticipated weight (kilobytes) contribution to the final bundle
+        /// for a single confirmation
+        /// </summary>
+        public const int MessageWeight = 2;
+
+        public ChargeConfirmationBundleSpecification()
+            : base(BundleType.ChargeConfirmationDataAvailable)
+        {
+        }
+
+        public override int GetMessageWeight(AvailableChargeReceiptData data)
+        {
+            return MessageWeight;
+        }
+    }
+}
