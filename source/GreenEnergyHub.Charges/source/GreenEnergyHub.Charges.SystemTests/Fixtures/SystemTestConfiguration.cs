23a4f3956bb539e9cb5cb027afd0155db29c7e31
Dan Stenrøjl <dst@systemate.dk>
2021-12-09 23:56:09 +0100

    What: PoC System Test

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.SystemTests/Fixtures/SystemTestConfiguration.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace GreenEnergyHub.Charges.SystemTests.Fixtures
+{
+    public class SystemTestConfiguration
+    {
+        public SystemTestConfiguration()
+        {
+            Configuration = BuildConfiguration();
+
+            Skip = Configuration.GetValue<bool>("SYSTEMTESTFACT_SKIP");
+            BaseAddress = new Uri(Configuration.GetValue<string>("MYDOMAIN_BASEADDRESS"));
+        }
+
+        public bool Skip { get; }
+
+        public Uri BaseAddress { get; }
+
+        private IConfigurationRoot Configuration { get; }
+
+        /// <summary>
+        /// Load settings from file if available, but also allow
+        /// those settings to be overriden using environment variables.
+        /// </summary>
+        private IConfigurationRoot BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("systemtest.local.settings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.SystemTests/MyFirstSystemTest.cs
@@ -0,0 +1,89 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.TestCommon;
+using FluentAssertions;
+using GreenEnergyHub.Charges.SystemTests.Fixtures;
+using Xunit;
+
+namespace GreenEnergyHub.Charges.SystemTests
+{
+    public class MyFirstSystemTest
+    {
+        public MyFirstSystemTest()
+        {
+            Configuration = new SystemTestConfiguration();
+        }
+
+        private SystemTestConfiguration Configuration { get; }
+
+#pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
+        [SystemTestFact(Skip = "Hi Peter, pls update this test for your scenario. :o)")]
+        public async Task When_TriggeringAction_Then_PeekReturnsExpectedContent()
+        {
+            // Arrange
+            using var httpClient = new HttpClient
+            {
+                BaseAddress = Configuration.BaseAddress,
+            };
+
+            // Flow
+            // => Trigger action
+            using var actionResponse = await httpClient.PostAsync("api/myActionEndpoint", new StringContent("myActionBody"));
+            actionResponse.StatusCode.Should().Be(HttpStatusCode.Accepted);
+
+            // => Peek
+            HttpResponseMessage? expectedResponse = null;
+            await Awaiter.WaitUntilConditionAsync(
+                async () =>
+                {
+                    expectedResponse = await httpClient.GetAsync("api/myPeekEndpoint");
+                    return expectedResponse.StatusCode == HttpStatusCode.OK;
+                },
+                timeLimit: TimeSpan.FromMinutes(1),
+                delay: TimeSpan.FromSeconds(2));
+
+            // Assert
+            var content = await expectedResponse!.Content.ReadAsStringAsync();
+            content.Should().Contain("myExpectedContent");
+        }
+
+        // This is just to be able to verify everything works with regards to settings and executing the tests after deployment.
+        // If needed, this test can be removed when the actual system test has been implemented.
+        // Or it can be used to actually verify the HealthStatus after deplyoment.
+        [SystemTestFact]
+        public async Task When_RequestHealthStatus_Then_ResponseIsOKAndDoesNotContainFalse()
+        {
+            // Arrange
+            using var httpClient = new HttpClient
+            {
+                BaseAddress = Configuration.BaseAddress,
+            };
+
+            // Act
+            using var actionResponse = await httpClient.GetAsync("api/HealthStatus");
+
+            // Assert
+            actionResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var content = await actionResponse.Content.ReadAsStringAsync();
+            content.Should().NotContain("false");
+        }
+#pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
+    }
+}
