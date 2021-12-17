a509d200e975ab92172265e48886ec98d1ca8d0d
BjarkeMeier <hej@bjarkemeier.dk>
2021-12-17 10:40:38 +0100

    Fix license header and analyzer violations of scaffolded files

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.QueryApi/Model/Charge.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.QueryApi/Model/Charge.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 
 #nullable disable
@@ -14,20 +28,33 @@ namespace GreenEnergyHub.Charges.QueryApi.Model
         }
 
         public Guid Id { get; set; }
+
         public string SenderProvidedChargeId { get; set; }
+
         public int Type { get; set; }
+
         public Guid OwnerId { get; set; }
+
         public bool TaxIndicator { get; set; }
+
         public int Resolution { get; set; }
+
         public bool TransparentInvoicing { get; set; }
+
         public string Description { get; set; }
+
         public string Name { get; set; }
+
         public int VatClassification { get; set; }
+
         public DateTime StartDateTime { get; set; }
+
         public DateTime? EndDateTime { get; set; }
 
         public virtual MarketParticipant Owner { get; set; }
+
         public virtual ICollection<ChargeLink> ChargeLinks { get; set; }
+
         public virtual ICollection<DefaultChargeLink> DefaultChargeLinks { get; set; }
     }
 }
