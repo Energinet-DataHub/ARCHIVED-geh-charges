69d208b4707dfe549b6d4ef791ffd773bd827ab6
Lasse Klitgaard <lki@energinet.dk>
2021-12-20 15:12:36 +0100

    re-add summary

--- source/Contracts/ChargeLink/ChargeLinkDto.cs
+++ source/Contracts/ChargeLink/ChargeLinkDto.cs
@@ -24,33 +24,63 @@ namespace Energinet.Charges.Contracts.ChargeLink
     /// </summary>
     public class ChargeLinkDto
     {
+        /// <summary>
+        /// The type of charge; tariff, fee or subscription
+        /// </summary>
         [Required]
         public ChargeType ChargeType { get; set; }
 
+        /// <summary>
+        /// A charge identifier provided by the market participant. Combined with charge owner and charge type it becomes unique
+        /// </summary>
         [Required]
         public string ChargeId { get; set; }
 
+        /// <summary>
+        /// Charge name provided by the market participant.
+        /// </summary>
         [Required]
         public string ChargeName { get; set; }
 
+        /// <summary>
+        /// A charge owner identification, e.g. the market participant's GLN or EIC number
+        /// </summary>
         [Required]
         public string ChargeOwner { get; set; }
 
+        /// <summary>
+        /// The market participant's company name
+        /// </summary>
         [Required]
         public string ChargeOwnerName { get; set; }
 
+        /// <summary>
+        /// Indicates whether a tariff is considered a tax or not
+        /// </summary>
         [Required]
         public bool TaxIndicator { get; set; }
 
+        /// <summary>
+        /// Indicates whether the charge owner wants the charge to be displayed on the customer invoice
+        /// </summary>
         [Required]
         public bool TransparentInvoicing { get; set; }
 
+        /// <summary>
+        /// The charge link's quantity
+        /// </summary>
         [Required]
         public int Quantity { get; set; }
 
+        /// <summary>
+        /// The charge link's start date time in UTC
+        /// </summary>
         [Required]
         public DateTimeOffset StartDate { get; set; }
 
+        /// <summary>
+        /// The charge link's end date time in UTC
+        /// </summary>
         public DateTimeOffset? EndDate { get; set; }
     }
 }
