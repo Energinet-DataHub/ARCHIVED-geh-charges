ca0792850921b31f4d45bff794d231f775dc0435
Peter Tandrup <PTA@energinet.dk>
2021-12-21 10:20:53 +0100

    Changed values in 'Add charge link data test data' sql

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Test/202112171600 Add charge link test data.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Test/202112171600 Add charge link test data.sql
@@ -5,8 +5,8 @@ DECLARE @meteringPointId VARCHAR(50)
 SELECT @chargeOwnerId = Id FROM charges.marketparticipant WHERE marketparticipantid = '8100000000016'
 SELECT @chargeId = NEWID()
 
-INSERT INTO charges.charge VALUES (@chargeId, '40000', 3, @chargeOwnerId, 0, 2, 0, 'Description...', 'Transmissions nettarif', 1, '1999-12-31 23:00:00', NULL)
+INSERT INTO charges.charge VALUES (@chargeId, 'TestTariff', 3, @chargeOwnerId, 0, 2, 0, 'Description...', 'Grid Access Provider test tariff', 1, '1999-12-31 23:00:00', NULL)
 
 SELECT @meteringPointId = Id FROM charges.meteringpoint WHERE meteringpointid = '571313180000000005'
 
-INSERT INTO charges.chargelink VALUES (newid(), @chargeId, @meteringPointId, '2019-12-31 23:00:00', '2021-12-31 23:00:00', 1)
+INSERT INTO charges.chargelink VALUES (newid(), @chargeId, @meteringPointId, '2019-12-31 23:00:00', '2022-12-31 23:00:00', 1)
