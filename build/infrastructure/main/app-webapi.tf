55f45bde86b609f4f053827e338f2716b301037d
Peter Tandrup <PTA@energinet.dk>
2022-01-05 11:14:41 +0100

    Remove appl insights in web api tf

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -25,10 +25,6 @@ resource "azurerm_app_service" "webapi" {
     }
   }
 
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${data.azurerm_key_vault_secret.appi_instrumentation_key.value}"
-  }
-
   connection_string {
     name  = "CHARGE_DB_CONNECTION_STRING"
     type  = "SQLServer"
