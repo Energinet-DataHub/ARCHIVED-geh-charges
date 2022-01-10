1ca919427ab8955291bad74eca5dcbe9fdcda1ce
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2022-01-10 11:34:00 +0100

    Remove unused TokensAttribute

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure.Core/Cim/ValidationErrors/CimValidationErrorTextTemplateMessages.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure.Core/Cim/ValidationErrors/CimValidationErrorTextTemplateMessages.cs
@@ -16,42 +16,40 @@ using GreenEnergyHub.Charges.Domain.Dtos.ChargeCommands.Validation;
 
 namespace GreenEnergyHub.Charges.Infrastructure.Core.Cim.ValidationErrors
 {
+    /// <summary>
+    /// This class contains templates for error messages texts for rejection messages to market actors.
+    /// Each public property contains the template for an error message corresponding to a specific
+    /// <see cref="ValidationRuleIdentifier"/> designated by the <see cref="ErrormessageForAttribute"/>.
+    /// The template texts can contain placeholders for data from the Charges domain. All valid
+    /// placeholders are defined in <see cref="CimValidationErrorTextToken"/>.
+    /// </summary>
     public static class CimValidationErrorTextTemplateMessages
     {
         [ErrormessageFor(ValidationRuleIdentifier.StartDateValidation)]
-        [Tokens(CimValidationErrorTextToken.ChargeStartDateTime)]
         public const string StartDateValidationErrorText =
             "Effectuation date {{ChargeStartDateTime}} incorrect: The information is not received within the correct time period (either too soon or too late)";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChangingTariffVatValueNotAllowed)]
-        [Tokens(CimValidationErrorTextToken.ChargeVatClass, CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChangingTariffVatValueNotAllowedErrorText =
             "VAT class {{ChargeVatClass}} not allowed: charge {{DocumentSenderProvidedChargeId}} cannot be updated with another entitlement for VAT";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChangingTariffTaxValueNotAllowed)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeTaxIndicator,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChangingTariffTaxValueNotAllowedErrorText =
             "Tax indicator {{ChargeTaxIndicator}} not allowed: charge {{DocumentSenderProvidedChargeId}} cannot be updated with another Tax indicator";
 
         [ErrormessageFor(ValidationRuleIdentifier.SenderIsMandatoryTypeValidation)]
-        [Tokens(CimValidationErrorTextToken.DocumentId)]
         public const string SenderIsMandatoryTypeValidationErrorText =
             "Sender is missing for message {{DocumentId}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.RecipientIsMandatoryTypeValidation)]
-        [Tokens(CimValidationErrorTextToken.DocumentId)]
         public const string RecipientIsMandatoryTypeValidationErrorText =
             "Recipient is missing for message {{DocumentId}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeOperationIdRequired)]
-        [Tokens(CimValidationErrorTextToken.DocumentId)]
         public const string ChargeOperationIdRequiredErrorText =
             "Identification is missing: transaction can not be processed for document {{DocumentId}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeIdLengthValidation)]
-        [Tokens(CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargeIdLengthValidationErrorText =
             "Charge ID {{DocumentSenderProvidedChargeId}} has a length that exceeds 10";
 
@@ -60,117 +58,74 @@ namespace GreenEnergyHub.Charges.Infrastructure.Core.Cim.ValidationErrors
             "Identification is missing, charge can not be processed";
 
         [ErrormessageFor(ValidationRuleIdentifier.DocumentTypeMustBeRequestUpdateChargeInformation)]
-        [Tokens(CimValidationErrorTextToken.DocumentType, CimValidationErrorTextToken.DocumentBusinessReasonCode)]
         public const string DocumentTypeMustBeRequestUpdateChargeInformationErrorText =
             "Document type {{DocumentType}} not allowed together with energy business process {{DocumentBusinessReasonCode}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.BusinessReasonCodeMustBeUpdateChargeInformation)]
-        [Tokens(CimValidationErrorTextToken.DocumentBusinessReasonCode, CimValidationErrorTextToken.DocumentType)]
         public const string BusinessReasonCodeMustBeUpdateChargeInformationErrorText =
             "Energy business process {{DocumentBusinessReasonCode}} not allowed together with document type {{DocumentType}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeTypeIsKnownValidation)]
-        [Tokens(CimValidationErrorTextToken.ChargeType, CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargeTypeIsKnownValidationErrorText =
             "Charge type {{ChargeType}} for charge {{DocumentSenderProvidedChargeId}} has wrong value (outside domain)";
 
         [ErrormessageFor(ValidationRuleIdentifier.VatClassificationValidation)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeVatClass,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string VatClassificationValidationErrorText =
             "VAT class {{ChargeVatClass}} for charge {{DocumentSenderProvidedChargeId}} has wrong value (outside domain)";
 
         [ErrormessageFor(ValidationRuleIdentifier.ResolutionTariffValidation)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeResolution,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeType)]
         public const string ResolutionTariffValidationErrorText =
             "Period type {{ChargeResolution}} not allowed: The specified resolution for charge {{DocumentSenderProvidedChargeId}} of type {{ChargeType}} must be Day or Hour";
 
         [ErrormessageFor(ValidationRuleIdentifier.ResolutionFeeValidation)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeResolution,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeType)]
         public const string ResolutionFeeValidationErrorText =
             "Period type {{ChargeResolution}} not allowed: The specified resolution for charge {{DocumentSenderProvidedChargeId}} of type {{ChargeType}} must be Day";
 
         [ErrormessageFor(ValidationRuleIdentifier.ResolutionSubscriptionValidation)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeResolution,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeType)]
         public const string ResolutionSubscriptionValidationErrorText =
             "Period type {{ChargeResolution}} not allowed: The specified resolution for charge {{DocumentSenderProvidedChargeId}} of type {{ChargeType}} must be Month";
 
         [ErrormessageFor(ValidationRuleIdentifier.StartDateTimeRequiredValidation)]
-        [Tokens(CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string StartDateTimeRequiredValidationErrorText =
             "Occurrence date is missing for charge {{DocumentSenderProvidedChargeId}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeOwnerIsRequiredValidation)]
-        [Tokens(CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargeOwnerIsRequiredValidationErrorText =
             "Owner is missing for charge {{DocumentSenderProvidedChargeId}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeNameHasMaximumLength)]
-        [Tokens(CimValidationErrorTextToken.ChargeName, CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargeNameHasMaximumLengthErrorText =
             "Name {{ChargeName}} for charge {{DocumentSenderProvidedChargeId}} has a length that exceeds 50";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeDescriptionHasMaximumLength)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargeDescription,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargeDescriptionHasMaximumLengthErrorText =
             "Description {{ChargeDescription}} for charge {{DocumentSenderProvidedChargeId}} has a length that exceeds 2048";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargeTypeTariffPriceCount)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargePointsCount,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeResolution)]
         public const string ChargeTypeTariffPriceCountErrorText =
             "The number of prices {{ChargePointsCount}} for charge {{DocumentSenderProvidedChargeId}} doesn't match period type {{ChargeResolution}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.MaximumPrice)]
-        [Tokens(CimValidationErrorTextToken.ChargePointPrice, CimValidationErrorTextToken.ChargePointPosition)]
         public const string MaximumPriceErrorText =
             "Price {{ChargePointPrice}} not allowed: The specified charge price for position {{ChargePointPosition}} is not plausible (too large)";
 
         [ErrormessageFor(ValidationRuleIdentifier.ChargePriceMaximumDigitsAndDecimals)]
-        [Tokens(CimValidationErrorTextToken.ChargePointPrice, CimValidationErrorTextToken.DocumentSenderProvidedChargeId)]
         public const string ChargePriceMaximumDigitsAndDecimalsErrorText =
             "Energy price {{ChargePointPrice}} for charge {{DocumentSenderProvidedChargeId}} contains a non-digit character, has a length that exceeds 15 or does not comply with format '99999999.999999'";
 
         [ErrormessageFor(ValidationRuleIdentifier.FeeMustHaveSinglePrice)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargePointsCount,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeResolution)]
         public const string FeeMustHaveSinglePriceErrorText =
             "The number of prices {{ChargePointsCount}} for charge {{DocumentSenderProvidedChargeId}} doesn't match period type {{ChargeResolution}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.SubscriptionMustHaveSinglePrice)]
-        [Tokens(
-            CimValidationErrorTextToken.ChargePointsCount,
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeResolution)]
         public const string SubscriptionMustHaveSinglePriceErrorText =
             "The number of prices {{ChargePointsCount}} for charge {{DocumentSenderProvidedChargeId}} doesn't match period type {{ChargeResolution}}";
 
         [ErrormessageFor(ValidationRuleIdentifier.CommandSenderMustBeAnExistingMarketParticipant)]
-        [Tokens(CimValidationErrorTextToken.DocumentSenderId, CimValidationErrorTextToken.DocumentId)]
         public const string CommandSenderMustBeAnExistingMarketParticipantErrorText =
             "Sender {{DocumentSenderId}} for message {{DocumentId}} is currently not an existing market party (company) or not active";
 
         [ErrormessageFor(ValidationRuleIdentifier.UpdateNotYetSupported)]
-        [Tokens(
-            CimValidationErrorTextToken.DocumentSenderProvidedChargeId,
-            CimValidationErrorTextToken.ChargeOwner,
-            CimValidationErrorTextToken.ChargeType)]
         public const string UpdateNotYetSupportedErrorText =
             "Charge ID {{DocumentSenderProvidedChargeId}} for owner {{ChargeOwner}} of type {{ChargeType}} cannot yet be updated or stopped. The functionality is not implemented yet.";
     }
