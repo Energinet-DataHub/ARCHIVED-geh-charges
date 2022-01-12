6c0c5a6175bf873d6b44d63ae4bf3f7f4cb959f0
Peter Tandrup <PTA@energinet.dk>
2022-01-12 16:11:46 +0100

    Remove config as param to AddApplicationInsightsTelemetry

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Startup.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Startup.cs
@@ -34,7 +34,7 @@ namespace GreenEnergyHub.Charges.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddApplicationInsightsTelemetry();
 
             services.AddControllers()
                 .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
