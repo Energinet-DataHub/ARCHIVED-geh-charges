abfd83af3608395c1c241e18808b558f45faa7ab
Peter Tandrup <PTA@energinet.dk>
2022-01-12 15:50:02 +0100

    Remove usings in Startup.cs

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Startup.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Startup.cs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using System.Text.Json.Serialization;
-using GreenEnergyHub.Charges.WebApi.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
