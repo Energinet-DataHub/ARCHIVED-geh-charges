7bde110fe7fcfc35f650407911cf639b95718f45
Mech0z <2316684+Mech0z@users.noreply.github.com>
2022-01-13 11:10:10 +0100

    rename default charge links

--- build/infrastructure/main/sbt-default-charge-links-data-available-notified.tf
+++ build/infrastructure/main/sbt-default-charge-links-data-available-notified.tf
@@ -12,15 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module "sbt_default_charge_link_available_notified" {
+module "sbt_default_charge_links_available_notified" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic?ref=5.1.0"
 
-  name                = "default-charge-link-available"
+  name                = "default-charge-links-available"
   namespace_name      = module.sb_charges.name
   resource_group_name = azurerm_resource_group.this.name
   subscriptions       = [
     {
-      name                = "default-charge-link-available-notified"
+      name                = "default-charge-links-available-notified"
       max_delivery_count  = 1
     },
   ]
