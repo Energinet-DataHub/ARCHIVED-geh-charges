59e12eadfa8106674321d1c33040110ad9358c12
BjarkeMeier <hej@bjarkemeier.dk>
2022-01-13 13:44:31 +0100

    SQL changes requested in review

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202201121500 Store sender of available data.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202201121500 Store sender of available data.sql
@@ -1,31 +1,92 @@
-ALTER TABLE [MessageHub].[AvailableChargeData]
-    ADD [SenderId] NVARCHAR(35) NOT NULL DEFAULT('5790001330552');
-GO
+-- Create SenderId and SenderRole columns
 
 ALTER TABLE [MessageHub].[AvailableChargeData]
-    ADD [SenderRole] INT NOT NULL DEFAULT(7);
-GO
+ADD [SenderId] NVARCHAR(35);
+
+ALTER TABLE [MessageHub].[AvailableChargeData]
+ADD [SenderRole] INT;
 
 ALTER TABLE [MessageHub].[AvailableChargeLinksReceiptData]
-    ADD [SenderId] NVARCHAR(35) NOT NULL DEFAULT('5790001330552');
-GO
+ADD [SenderId] NVARCHAR(35);
 
 ALTER TABLE [MessageHub].[AvailableChargeLinksReceiptData]
-    ADD [SenderRole] INT NOT NULL DEFAULT(7);
-GO
+ADD [SenderRole] INT;
 
 ALTER TABLE [MessageHub].[AvailableChargeLinksData]
-    ADD [SenderId] NVARCHAR(35) NOT NULL DEFAULT('5790001330552');
-GO
+ADD [SenderId] NVARCHAR(35);
 
 ALTER TABLE [MessageHub].[AvailableChargeLinksData]
-    ADD [SenderRole] INT NOT NULL DEFAULT(7);
-GO
+ADD [SenderRole] INT;
 
 ALTER TABLE [MessageHub].[AvailableChargeReceiptData]
-    ADD [SenderId] NVARCHAR(35) NOT NULL DEFAULT('5790001330552');
-GO
+ADD [SenderId] NVARCHAR(35);
 
 ALTER TABLE [MessageHub].[AvailableChargeReceiptData]
-    ADD [SenderRole] INT NOT NULL DEFAULT(7);
+ADD [SenderRole] INT;
+
 GO
+
+-- Set column values
+
+DECLARE @data_hub NVARCHAR(35) = '5790001330552'
+DECLARE @metering_point_administrator_role INT = 7
+
+UPDATE [MessageHub].[AvailableChargeData]
+SET SenderId = @data_hub 
+WHERE SenderId IS NULL
+
+UPDATE [MessageHub].[AvailableChargeData]
+SET SenderRole = @metering_point_administrator_role
+WHERE SenderRole IS NULL
+
+UPDATE [MessageHub].[AvailableChargeLinksReceiptData]
+SET SenderId = @data_hub
+WHERE SenderId IS NULL
+
+UPDATE [MessageHub].[AvailableChargeLinksReceiptData]
+SET SenderRole = @metering_point_administrator_role
+WHERE SenderRole IS NULL
+
+UPDATE [MessageHub].[AvailableChargeLinksData]
+SET SenderId = @data_hub
+WHERE SenderId IS NULL
+
+UPDATE [MessageHub].[AvailableChargeLinksData]
+SET SenderRole = @metering_point_administrator_role
+WHERE SenderRole IS NULL
+
+UPDATE [MessageHub].[AvailableChargeReceiptData]
+SET SenderId = @data_hub
+WHERE SenderId IS NULL
+
+UPDATE [MessageHub].[AvailableChargeReceiptData]
+SET SenderRole = @metering_point_administrator_role
+WHERE SenderRole IS NULL
+
+GO
+
+-- Enable NOT NULL constraints
+
+ALTER TABLE [MessageHub].[AvailableChargeData]
+ALTER COLUMN SenderId NVARCHAR(35) NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeData]
+ALTER COLUMN SenderRole INT NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeLinksReceiptData]
+ALTER COLUMN SenderId NVARCHAR(35) NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeLinksReceiptData]
+ALTER COLUMN SenderRole INT NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeLinksData]
+ALTER COLUMN SenderId NVARCHAR(35) NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeLinksData]
+ALTER COLUMN SenderRole INT NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeReceiptData]
+ALTER COLUMN SenderId NVARCHAR(35) NOT NULL;
+
+ALTER TABLE [MessageHub].[AvailableChargeReceiptData]
+ALTER COLUMN SenderRole INT NOT NULL;
