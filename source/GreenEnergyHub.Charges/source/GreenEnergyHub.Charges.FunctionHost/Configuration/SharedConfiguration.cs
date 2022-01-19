fffa38fc7f50ad4f9698792bb15975c1b7ab625e
Karsten M <xnetsrak@gmail.com>
2022-01-19 15:24:39 +0100

    adding request response logging middleware (#1061)
    re added request response logging middleware

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.FunctionHost/Configuration/SharedConfiguration.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.FunctionHost/Configuration/SharedConfiguration.cs
@@ -14,6 +14,7 @@
 
 using System;
 using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.Core.Logging.RequestResponseMiddleware.Storage;
 using Energinet.DataHub.Core.Messaging.Protobuf;
 using Energinet.DataHub.Core.Messaging.Transport;
 using Energinet.DataHub.MessageHub.Client;
@@ -51,6 +52,7 @@ using GreenEnergyHub.Iso8601;
 using GreenEnergyHub.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NodaTime;
 
 namespace GreenEnergyHub.Charges.FunctionHost.Configuration
@@ -81,6 +83,8 @@ namespace GreenEnergyHub.Charges.FunctionHost.Configuration
             var azureBlobStorageContainerName = EnvironmentHelper.GetEnv(EnvironmentSettingNames.MessageHubStorageContainer);
 
             var serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+
+            AddRequestResponseLogging(serviceCollection);
 
             AddCreateDefaultChargeLinksReplier(serviceCollection, serviceBusClient);
             AddPostOfficeCommunication(
@@ -185,6 +189,18 @@ namespace GreenEnergyHub.Charges.FunctionHost.Configuration
             serviceCollection.AddServiceBus(serviceBusConnectionString);
             serviceCollection.AddApplicationServices();
             serviceCollection.AddStorageHandler(storageServiceConnectionString);
+        }
+
+        private static void AddRequestResponseLogging(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IRequestResponseLogging>(provider =>
+            {
+                var requestResponseLogStorage = EnvironmentHelper.GetEnv(EnvironmentSettingNames.RequestResponseLoggingConnectionString);
+                var requestResponseLogContainerName = EnvironmentHelper.GetEnv(EnvironmentSettingNames.RequestResponseLoggingContainerName);
+
+                var storageLogging = provider.GetService<ILogger<RequestResponseLoggingBlobStorage>>();
+                return new RequestResponseLoggingBlobStorage(requestResponseLogStorage, requestResponseLogContainerName, storageLogging!);
+            });
         }
 
         private static void AddServiceBus(this IServiceCollection serviceCollection, string serviceBusConnectionString)
