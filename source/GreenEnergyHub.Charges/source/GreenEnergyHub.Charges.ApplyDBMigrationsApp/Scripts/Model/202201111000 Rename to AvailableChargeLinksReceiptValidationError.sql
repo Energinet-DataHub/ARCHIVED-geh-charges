9c954aa4c8e9e1f6a362eed486ac621753bf39b3
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2022-01-20 14:39:27 +0100

    Fix test data (#1067)

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202201111000 Rename to AvailableChargeLinksReceiptValidationError.sql
+++
@@ -1,5 +0,0 @@
-EXEC sp_rename 'MessageHub.AvailableChargeLinkReceiptData', 'AvailableChargeLinksReceiptData';
-GO
-
-EXEC sp_rename 'MessageHub.AvailableChargeLinkReceiptDataReasonCode', 'AvailableChargeLinksReceiptValidationError';
-GO
--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Seed/202108130611 System operator market participant.sql
+++
@@ -1,7 +0,0 @@
-﻿BEGIN /* We need to be careful here, the participant we are adding might already be on the server if the server includes test data */
-  IF NOT EXISTS (SELECT MarketParticipantId FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
-  BEGIN
-    INSERT INTO [Charges].[MarketParticipant] (MarketParticipantId, Name, Role) VALUES ('5790000432752', 'System Operator', 3); /* System Operator is used as a name for this participant, it is not by mistake */
-  END
-END
-GO
