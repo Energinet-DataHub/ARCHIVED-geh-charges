7fdb7651f94a1661ecbdeee31603366561f57fea
Lasse Klitgaard <lki@energinet.dk>
2022-01-20 09:32:15 +0100

    bump wait timespan for function executed assertion

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/TestCommon/FunctionExecuted.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/TestCommon/FunctionExecuted.cs
@@ -24,7 +24,7 @@ namespace GreenEnergyHub.Charges.IntegrationTests.TestCommon
     {
         public static async Task AssertAsync(FunctionAppHostManager hostManager, string functionName)
         {
-            var waitTimespan = TimeSpan.FromSeconds(10);
+            var waitTimespan = TimeSpan.FromSeconds(30);
 
             var functionExecuted = await Awaiter
                 .TryWaitUntilConditionAsync(
