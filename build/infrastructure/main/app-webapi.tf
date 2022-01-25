d32b91896cca9097859ae6145da9da690143c812
Dan Stenrøjl <dst@systemate.dk>
2022-01-25 15:03:31 +0100

    WHat: Use App Servie module

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -11,43 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_app_service" "webapi" {
-  name                = "app-webapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  app_service_plan_id = module.plan_webapi.id
+module "app_webapi" {
+  # TODO: Update ref branch
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=6.0.0-webapi"
 
-  site_config {
-    linux_fx_version = "DOTNETCORE|5.0"
-    dotnet_framework_version = "v5.0"
-    cors {
-      allowed_origins = ["*"]
-    }
+  name                                      = "webapi"
+  project_name                              = var.domain_name_short
+  environment_short                         = var.environment_short
+  environment_instance                      = var.environment_instance
+  resource_group_name                       = azurerm_resource_group.this.name
+  location                                  = azurerm_resource_group.this.location
+  vnet_integration_subnet_id                = module.vnet_integrations.id
+  app_service_plan_id                       = module.plan_webapi.id
+  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
+  connection_strings = {
+    "CHARGE_DB_CONNECTION_STRING" = local.CHARGE_DB_CONNECTION_STRING
   }
 
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value}"
-  }
-
-  connection_string {
-    name  = "CHARGE_DB_CONNECTION_STRING"
-    type  = "SQLServer"
-    value = local.CHARGE_DB_CONNECTION_STRING
-  }
-
-  tags              = azurerm_resource_group.this.tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }
+  tags                                      = azurerm_resource_group.this.tags
 }
 
 module "plan_webapi" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=5.1.0"
+  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=6.0.0"
 
   name                  = "webapi"
   project_name          = var.domain_name_short
@@ -62,14 +47,14 @@ module "plan_webapi" {
     size  = "B1"
   }
 
-  tags                = azurerm_resource_group.this.tags
+  tags                  = azurerm_resource_group.this.tags
 }
 
 module "kvs_app_charges_webapi_base_url" {
-  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=6.0.0"
 
   name          = "app-charges-webapi-base-url"
-  value         = "https://${azurerm_app_service.webapi.default_site_hostname}"
+  value         = "https://${module.app_webapi.default_site_hostname}"
   key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
 
   tags          = azurerm_resource_group.this.tags
