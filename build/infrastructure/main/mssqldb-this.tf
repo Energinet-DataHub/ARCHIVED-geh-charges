d891b5e79fe3062d87934195af03b9958a9b74e6
djorgensendk <32477395+djorgensendk@users.noreply.github.com>
2022-01-27 09:44:55 +0100

    Create SQL database based on azurerm_mssql_database TF resource (#1085)

---
+++ build/infrastructure/main/mssqldb-this.tf
@@ -0,0 +1,29 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+data "azurerm_mssql_server" "mssqlsrv" {
+  name                = data.azurerm_key_vault_secret.mssql_data_name.value
+  resource_group_name = data.azurerm_resource_group.shared_resources.name
+}
+
+module "mssqldb_charges" {
+  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=5.4.0"
+
+  name                  = "data"
+  project_name          = var.domain_name_short
+  environment_short     = var.environment_short
+  environment_instance  = var.environment_instance
+  server_id             = data.azurerm_mssql_server.mssqlsrv.id
+  
+  tags                  = azurerm_resource_group.this.tags
+}
