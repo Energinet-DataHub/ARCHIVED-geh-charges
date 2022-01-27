e79a84536084ec64105373d2e2480211f8873514
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2022-01-27 15:51:05 +0100

    Implement IValidationRuleWithExtendedData

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/ChargeLinksCommands/Validation/BusinessValidation/ValidationRules/ChargeLinksUpdateNotYetSupportedRule.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/ChargeLinksCommands/Validation/BusinessValidation/ValidationRules/ChargeLinksUpdateNotYetSupportedRule.cs
@@ -22,12 +22,14 @@ namespace GreenEnergyHub.Charges.Domain.Dtos.ChargeLinksCommands.Validation.Busi
     /// <summary>
     /// Temporary rule that stops both update and stops from taking place to charge links until that is implemented
     /// </summary>
-    public class ChargeLinksUpdateNotYetSupportedRule : IValidationRule
+    public class ChargeLinksUpdateNotYetSupportedRule : IValidationRuleWithExtendedData
     {
         private readonly ChargeLinksCommand _chargeLinksCommand;
         private readonly IReadOnlyCollection<ChargeLink> _existingChargeLinks;
 
-        public ChargeLinksUpdateNotYetSupportedRule(ChargeLinksCommand chargeLinksCommand, IReadOnlyCollection<ChargeLink> existingChargeLinks)
+        public ChargeLinksUpdateNotYetSupportedRule(
+            ChargeLinksCommand chargeLinksCommand,
+            IReadOnlyCollection<ChargeLink> existingChargeLinks)
         {
             _chargeLinksCommand = chargeLinksCommand;
             _existingChargeLinks = existingChargeLinks;
@@ -50,5 +52,9 @@ namespace GreenEnergyHub.Charges.Domain.Dtos.ChargeLinksCommands.Validation.Busi
 
             return true;
         }
+
+        public string TriggeredBy =>
+            _chargeLinksCommand.ChargeLinks
+                .First(ChargeLinkDateRangeIsNotOverlapping).SenderProvidedChargeId;
     }
 }
