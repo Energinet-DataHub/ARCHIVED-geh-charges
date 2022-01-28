61384d62f7bd2168a17d58b1ee752ab2f823ce59
Dan Stenrøjl <dst@systemate.dk>
2022-01-28 23:59:45 +0100

    What: Test CI trigger

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/Factories/ChargeCreatedEventFactory.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/Factories/ChargeCreatedEventFactory.cs
@@ -20,6 +20,7 @@ using GreenEnergyHub.Charges.Domain.Dtos.ChargeCommandAcceptedEvents;
 
 namespace GreenEnergyHub.Charges.Application.Charges.Factories
 {
+    // Test if build is triggered
     public class ChargeCreatedEventFactory : IChargeCreatedEventFactory
     {
         private readonly CurrencyConfigurationIso4217 _currencyConfigurationIso4217;
