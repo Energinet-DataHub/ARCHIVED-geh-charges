eb0457be9ad063d453c998ceebe86922515f6827
Lasse Nielsen <lrn@energinet.dk>
2022-02-03 09:20:03 +0100

    integration tests of ODataControllerTests.cs

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/IntegrationTests/WebApi/ODataControllerTests.cs
@@ -0,0 +1,80 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GreenEnergyHub.Charges.IntegrationTests.Fixtures.WebApi;
+using GreenEnergyHub.Charges.IntegrationTests.TestHelpers;
+using GreenEnergyHub.TestHelpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.IntegrationTests.IntegrationTests.WebApi
+{
+    [IntegrationTest]
+    [Collection(nameof(ChargesWebApiCollectionFixture))]
+    public class ODataControllerTests :
+        WebApiTestBase<ChargesWebApiFixture>,
+        IClassFixture<ChargesWebApiFixture>,
+        IClassFixture<WebApiFactory>,
+        IAsyncLifetime
+    {
+        private readonly HttpClient _client;
+        private readonly AuthenticationClient _authenticationClient;
+
+        public ODataControllerTests(
+            ChargesWebApiFixture chargesWebApiFixture,
+            WebApiFactory factory,
+            ITestOutputHelper testOutputHelper)
+            : base(chargesWebApiFixture, testOutputHelper)
+        {
+            _client = factory.CreateClient();
+            _authenticationClient = new AuthenticationClient(
+                chargesWebApiFixture.AuthorizationConfiguration.BackendAppScope,
+                chargesWebApiFixture.AuthorizationConfiguration.ClientCredentialsSettings,
+                chargesWebApiFixture.AuthorizationConfiguration.B2cTenantId);
+        }
+
+        public async Task InitializeAsync()
+        {
+            var authenticationResult = await _authenticationClient.GetAuthenticationTokenAsync();
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authenticationResult.AccessToken}");
+        }
+
+        [Theory]
+        [InlineAutoDomainData("/odata/charges")]
+        [InlineAutoDomainData("/odata/chargelinks")]
+        [InlineAutoDomainData("/odata/marketParticipants")]
+        [InlineAutoDomainData("/odata/meteringPoints")]
+        public async Task ODataControllerEndpoints_WhenCalled_ReturnsOkAndCorrectContentType(string uri)
+        {
+            // Act
+            var response = await _client.GetAsync(uri);
+
+            // Assert
+            var contentType = response.Content.Headers.ContentType!.ToString();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            contentType.Should().Be("application/json; charset=utf-8");
+        }
+
+        public Task DisposeAsync()
+        {
+            _client.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
