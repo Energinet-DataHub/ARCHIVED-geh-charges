84c72e509a026e3632709ad8b18d15e79f51f108
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2022-02-03 20:10:04 +0100

    Update source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Controllers/ChargeLinksController.cs
    Co-authored-by: Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Controllers/ChargeLinksController.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.WebApi/Controllers/ChargeLinksController.cs
@@ -80,7 +80,7 @@ namespace GreenEnergyHub.Charges.WebApi.Controllers
         /// </summary>
         /// <param name="meteringPointId">The 18-digits metering point identifier used by the Danish version of Green Energy Hub.
         /// Use 404 to get a "404 Not Found" response.</param>
-        /// <returns>Mocked charge links data or "404 Not Found"</returns>
+        /// <returns>Charge links data or "404 Not Found"</returns>
         [HttpGet("GetAsync")]
         [MapToApiVersion("2.0")]
         public async Task<IActionResult> GetAsync(string meteringPointId)
