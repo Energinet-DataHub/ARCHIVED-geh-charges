7c0e973d1dd25c8b42c56da715e5873cc778c544
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-16 08:30:35 +0100

    remove quotation marks from variables

--- build/infrastructure/main/variables.tf
+++ build/infrastructure/main/variables.tf
@@ -49,22 +49,22 @@ variable notification_email {
   description   = "Email address to send notifications to"
 }
 
-variable "subnet_internal_private_endpoints_address_space" {
+variable subnet_internal_private_endpoints_address_space {
   type          = string
   description   = "Address space of the subnet for internal private endpoints"
 }
 
-variable "subnet_external_private_endpoints_address_space" {
+variable subnet_external_private_endpoints_address_space {
   type          = string
   description   = "Address space of the subnet for external private endpoints"
 }
 
-variable "subnet_vnet_integrations_webapi_address_space" {
+variable subnet_vnet_integrations_webapi_address_space {
   type          = string
   description   = "Address space of the subnet for Virtual Network Integrations of WebAPIs"
 }
 
-variable "subnet_vnet_integrations_functions_address_space" {
+variable subnet_vnet_integrations_functions_address_space {
   type          = string
   description   = "Address space of the subnet for Virtual Network Integrations of Azure Functions"
 }
