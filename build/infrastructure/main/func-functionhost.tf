8ff896b72de9546446340f28bef87999b8fb380a
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-16 12:10:09 +0100

    use TF module 7.0.0 for functionapp - add PE

--- build/infrastructure/main/func-functionhost.tf
+++ build/infrastructure/main/func-functionhost.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "func_functionhost" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=6.0.0"
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=7.0.0"
 
   name                                      = "functionhost"
   project_name                              = var.domain_name_short
@@ -22,6 +22,7 @@ module "func_functionhost" {
   location                                  = azurerm_resource_group.this.location
   vnet_integration_subnet_id                = module.vnet_integrations_functionhost.id
   private_endpoint_subnet_id                = module.snet_internal_private_endpoints.id
+  external_private_endpoint_subnet_id       = module.snet_external_private_endpoints.id
   private_dns_resource_group_name           = data.azurerm_key_vault_secret.pdns_resource_group_name.value
   app_service_plan_id                       = module.plan_shared.id
   application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
