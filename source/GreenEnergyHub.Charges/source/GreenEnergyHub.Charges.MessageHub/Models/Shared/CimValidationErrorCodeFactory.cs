cdbe1fb316cfd8d797ab29f18bf74fdc0ceb2f3b
Peter Tandrup <PTA@energinet.dk>
2022-02-16 14:12:42 +0100

    Remove usings

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageHub/Models/Shared/CimValidationErrorCodeFactory.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.MessageHub/Models/Shared/CimValidationErrorCodeFactory.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using Energinet.DataHub.MessageHub.Model.Protobuf;
 using GreenEnergyHub.Charges.Domain.Dtos.Validation;
 using GreenEnergyHub.Charges.Infrastructure.Core.Cim;
 
