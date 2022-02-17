195c02c632d08c143903636eea77bb1022864d3d
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-17 09:47:30 +0100

    upgrade SKU of service plan

--- build/infrastructure/main/plan-shared.tf
+++ build/infrastructure/main/plan-shared.tf
@@ -22,8 +22,8 @@ module "plan_shared" {
   location              = azurerm_resource_group.this.location
   kind                  = "FunctionApp"
   sku                   = {
-    tier  = "Standard"
-    size  = "S1"
+    tier  = "Premium"
+    size  = "P1V2"
   }
 
   tags                = azurerm_resource_group.this.tags
