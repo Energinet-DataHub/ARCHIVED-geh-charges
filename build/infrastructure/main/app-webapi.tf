095898a1658a14a61abad7a2d85fb34f81f92cf4
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-17 14:44:29 +0100

    enable PE on web app

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "app_webapi" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=6.0.0"
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=7.0.0"
 
   name                                      = "webapi"
   project_name                              = var.domain_name_short
@@ -21,6 +21,8 @@ module "app_webapi" {
   resource_group_name                       = azurerm_resource_group.this.name
   location                                  = azurerm_resource_group.this.location
   vnet_integration_subnet_id                = module.vnet_integrations_webapi.id
+  external_private_endpoint_subnet_id       = module.snet_external_private_endpoints.id
+  private_dns_resource_group_name           = data.azurerm_key_vault_secret.pdns_resource_group_name.value
   app_service_plan_id                       = module.plan_webapi.id
   application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
 
@@ -53,8 +55,8 @@ module "plan_webapi" {
   kind                  = "Linux"
   reserved              = true
   sku                   = {
-    tier  = "Standard"
-    size  = "S1"
+    tier  = "PremiumV2"
+    size  = "P1v2"
   }
 
   tags                  = azurerm_resource_group.this.tags
