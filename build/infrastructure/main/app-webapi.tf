7c218e732365324868e73ca34ba4a301e2db75e3
Dan Stenrøjl <51327761+dstsyst@users.noreply.github.com>
2022-03-17 17:58:57 +0100

    PoC Health check (#1205)
    * Added simple live and ready endpoint without authN/Z
    
    * Initial configuration on liveness and readyness without authN/Z
    
    * Rename tag
    
    * Added using
    
    * Function health check tests (red)
    
    * Using standard Asp.NetCore Health Checks in functions
    
    * Rename function method
    
    * Web API health check: charge database
    
    * Web API health check tests updated to match database check
    
    * Function health check: update to check databases
    
    * Move name to top
    
    * Function health check: for service bus topics
    
    * Added comments on anonymous access
    
    * health check names
    
    * Web API health check for charge db test
    
    * Web API: Removed magic strings in test
    
    * Web API: Removed need for tags in health checks
    
    * Function health check test if database fails
    
    * Fix missing using
    
    * Fix test name
    
    * Simplify health check setup and execution by only using "self"
    
    * Move tests
    
    * Remove db dependency as it is in another domain
    
    * Use AddLiveCheck from package
    
    * Web API use packages
    
    * Reuse health check endpoint handler
    
    * Cleanup using
    
    * Use prerelease from NuGet.org
    
    * Added TOTO
    
    * Update namespace
    
    * Use released App packages
    
    * Use new TF module to set function halth check path
    
    * Use web api module
    
    * Module reference
    
    Co-authored-by: Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>

--- build/infrastructure/main/app-webapi.tf
+++ build/infrastructure/main/app-webapi.tf
@@ -11,18 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_app_service" "webapi" {
-  name                = "app-webapi-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  app_service_plan_id = data.azurerm_key_vault_secret.plan_shared_id.value
+module "app_webapi" {
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service?ref=5.8.0"
 
-  site_config {
-    dotnet_framework_version = "v5.0"
-    cors {
-      allowed_origins = ["*"]
-    }
-  }
+  name                                      = "webapi"
+  project_name                              = var.domain_name_short
+  environment_short                         = var.environment_short
+  environment_instance                      = var.environment_instance
+  resource_group_name                       = azurerm_resource_group.this.name
+  location                                  = azurerm_resource_group.this.location
+  app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
+  application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value
+  health_check_path                         = "/monitor/ready"
 
   app_settings = {
     APPINSIGHTS_INSTRUMENTATIONKEY = "${data.azurerm_key_vault_secret.appi_shared_instrumentation_key.value}",
@@ -30,28 +30,22 @@ resource "azurerm_app_service" "webapi" {
     FRONTEND_SERVICE_APP_ID = "${data.azurerm_key_vault_secret.frontend_service_app_id.value}"
   }
 
-  connection_string {
-    name  = "CHARGE_DB_CONNECTION_STRING"
-    type  = "SQLServer"
-    value = local.MS_CHARGE_DB_CONNECTION_STRING
-  }
+  connection_strings = [
+    {
+      name  = "CHARGE_DB_CONNECTION_STRING"
+      type  = "SQLAzure"
+      value = local.MS_CHARGE_DB_CONNECTION_STRING
+    }
+  ]
 
-  tags              = azurerm_resource_group.this.tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }
+  tags               = azurerm_resource_group.this.tags
 }
 
 module "kvs_app_charges_webapi_base_url" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
 
   name          = "app-charges-webapi-base-url"
-  value         = "https://${azurerm_app_service.webapi.default_site_hostname}"
+  value         = "https://${module.app_webapi.default_site_hostname}"
   key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
 
   tags          = azurerm_resource_group.this.tags
