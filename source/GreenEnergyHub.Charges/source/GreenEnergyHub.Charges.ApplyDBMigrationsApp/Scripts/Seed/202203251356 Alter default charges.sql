f0f20af336f256c0d09c36fbead2ddc7a345714f
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2022-03-28 12:46:55 +0200

    Remove unnecessary comments

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Seed/202203251356 Alter default charges.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Seed/202203251356 Alter default charges.sql
@@ -1,31 +1,31 @@
 /* Charge 40000 */
 update [Charges].[Charge]
-set [Name] = 'Transmissions nettarif', /* Name */
-    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne' /* Description */
+set [Name] = 'Transmissions nettarif',
+    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne'
 where SenderProvidedChargeId = '40000'
   and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752') /* MarketParticipantId */
+  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* Charge 40010 */
 update [Charges].[Charge]
-set [Name] = 'Indfødningstarif produktion', /* Name */
-    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne.' /* Description */
+set [Name] = 'Indfødningstarif produktion',
+    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne.'
 where SenderProvidedChargeId = '40010'
   and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752') /* MarketParticipantId */
+  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* Charge 41000 */
 update [Charges].[Charge]
-set [Name] = 'Systemtarif', /* Name */
-    [Description] = 'Systemafgiften dækker omkostninger til forsyningssikkerhed og elforsyningens kvalitet.' /* Description */
+set [Name] = 'Systemtarif',
+    [Description] = 'Systemafgiften dækker omkostninger til forsyningssikkerhed og elforsyningens kvalitet.'
 where SenderProvidedChargeId = '41000'
   and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752') /* MarketParticipantId */
+  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* EA-003 */
 update [Charges].[Charge]
-set [Name] = 'Negativt reduceret elafgift', /* Name */
-    [Description] = 'Negativ reduceret elafgift for elvarmekunder til brug for modregning af den almindelige reducerede elafgift for elvarmekunder. Bruges kun sammen med D14 målepunkter.' /* Description */
+set [Name] = 'Negativt reduceret elafgift',
+    [Description] = 'Negativ reduceret elafgift for elvarmekunder til brug for modregning af den almindelige reducerede elafgift for elvarmekunder. Bruges kun sammen med D14 målepunkter.'
 where SenderProvidedChargeId = 'EA-003'
   and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752') /* MarketParticipantId */
+  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
