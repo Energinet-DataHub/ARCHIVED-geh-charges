30a55036a37a388cdd31ea9f7b9e86793e2f03c4
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2022-03-28 13:37:23 +0200

    Name and description belongs to ChargePeriod

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Seed/202203251356 Alter default charges.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Seed/202203251356 Alter default charges.sql
@@ -1,31 +1,43 @@
 /* Charge 40000 */
-update [Charges].[Charge]
-set [Name] = 'Transmissions nettarif',
-    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne'
-where SenderProvidedChargeId = '40000'
-  and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
+update chp
+set chp.[Name] = 'Transmissions nettarif',
+    chp.[Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne'
+from [Charges].[ChargePeriod] as chp
+    inner join [Charges].[Charge] as chg
+    on chg.id = chp.ChargeId
+where chg.SenderProvidedChargeId = '40000'
+  and chg.[Type] = 3
+  and chg.[OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* Charge 40010 */
-update [Charges].[Charge]
-set [Name] = 'Indfødningstarif produktion',
-    [Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne.'
-where SenderProvidedChargeId = '40010'
-  and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
+update chp
+set chp.[Name] = 'Indfødningstarif produktion',
+    chp.[Description] = 'Netafgiften, for både forbrugere og producenter, dækker omkostninger til drift og vedligehold af det overordnede elnet (132/150 og 400 kv nettet) og drift og vedligehold af udlandsforbindelserne.'
+from [Charges].[ChargePeriod] as chp
+    inner join [Charges].[Charge] as chg
+    on chg.id = chp.ChargeId
+where chg.SenderProvidedChargeId = '40010'
+    and chg.[Type] = 3
+    and chg.[OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* Charge 41000 */
-update [Charges].[Charge]
-set [Name] = 'Systemtarif',
-    [Description] = 'Systemafgiften dækker omkostninger til forsyningssikkerhed og elforsyningens kvalitet.'
-where SenderProvidedChargeId = '41000'
-  and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
+update chp
+set chp.[Name] = 'Systemtarif',
+    chp.[Description] = 'Systemafgiften dækker omkostninger til forsyningssikkerhed og elforsyningens kvalitet.'
+from [Charges].[ChargePeriod] as chp
+    inner join [Charges].[Charge] as chg
+    on chg.id = chp.ChargeId
+where chg.SenderProvidedChargeId = '41000'
+    and chg.[Type] = 3
+    and chg.[OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
 
 /* EA-003 */
-update [Charges].[Charge]
-set [Name] = 'Negativt reduceret elafgift',
-    [Description] = 'Negativ reduceret elafgift for elvarmekunder til brug for modregning af den almindelige reducerede elafgift for elvarmekunder. Bruges kun sammen med D14 målepunkter.'
-where SenderProvidedChargeId = 'EA-003'
-  and [Type] = 3
-  and [OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
+update chp
+set chp.[Name] = 'Negativt reduceret elafgift',
+    chp.[Description] = 'Negativ reduceret elafgift for elvarmekunder til brug for modregning af den almindelige reducerede elafgift for elvarmekunder. Bruges kun sammen med D14 målepunkter.'
+from [Charges].[ChargePeriod] as chp
+    inner join [Charges].[Charge] as chg
+    on chg.id = chp.ChargeId
+where chg.SenderProvidedChargeId = 'EA-003'
+  and chg.[Type] = 3
+  and chg.[OwnerId] = (SELECT Id FROM [Charges].[MarketParticipant] WHERE MarketParticipantId = '5790000432752')
