06c34634356d90ecc41a09984df9f92c975edf39
Peter Tandrup <PTA@energinet.dk>
2022-03-29 11:16:18 +0200

    Add UniqueConstraint on SenderProvidedChargeID Type and ownerID

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202203242050 Add Unique Constraint on Charge table.sql
@@ -0,0 +1,7 @@
+﻿------------------------------------------------------------------------------------------------------------------------
+-- Add unique constraint on Charges.Charge to reject concurrent inserts for the same unique charge,
+-- identified by SenderProvidedChargeId, Type and OwnerId
+------------------------------------------------------------------------------------------------------------------------
+ALTER TABLE [Charges].[Charge]
+    ADD CONSTRAINT [UC_SenderProvidedChargeId_Type_OwnerId] UNIQUE (SenderProvidedChargeId, Type, OwnerId);
+GO
