e7cd70857f103fafdd9e2d1ba342ac4edfae19ff
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-03-30 14:29:31 +0200

    add code and mock test
    need to validate the business logic and test

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Domain/Dtos/ChargeCommands/Validation/BusinessValidation/ValidationRules/UpdateChargeMustHaveEffectiveDateBeforeOrOnStopDateRuleTests.cs
@@ -0,0 +1,78 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using Energinet.DataHub.Core.TestCommon.AutoFixture.Attributes;
+using FluentAssertions;
+using GreenEnergyHub.Charges.Domain.Charges;
+using GreenEnergyHub.Charges.Domain.Dtos.ChargeCommands.Validation.BusinessValidation.ValidationRules;
+using GreenEnergyHub.Charges.Tests.Builders.Command;
+using NodaTime;
+using NodaTime.Text;
+using Xunit;
+
+namespace GreenEnergyHub.Charges.Tests.Domain.Dtos.ChargeCommands.Validation.BusinessValidation.ValidationRules
+{
+    public class UpdateChargeMustHaveEffectiveDateBeforeOrOnStopDateRuleTests
+    {
+        [Theory]
+        // Valid earlier than stop
+        [InlineAutoMoqData("2020-05-10T13:00:00Z", "2020-05-10T21:59:59Z", true)]
+        // Valid same as stop
+        [InlineAutoMoqData("2020-05-10T13:00:00Z", "2020-05-10T22:00:00Z", true)]
+        // Valid same as stop where stop is infinite
+        [InlineAutoMoqData("2020-05-10T13:00:00Z", "2020-05-13T21:59:59Z", true)]
+        // InValid later than stop date
+        [InlineAutoMoqData("2020-05-10T13:00:00Z", "2020-05-13T22:00:00Z", false)]
+        public void IsValid_WhenExpectedDateIsWithinInterval_IsTrue(
+            string existingChargeEndDateIsoString,
+            string incommingCommandEndDateIsoString,
+            bool expectedIsValid)
+        {
+            // Arrange
+            var existingStopDate = InstantPattern.General.Parse(existingChargeEndDateIsoString).Value;
+            var incomingCommandEndDate = InstantPattern.General.Parse(incommingCommandEndDateIsoString).Value;
+
+            var existingCharge = new ChargeBuilder().WithPeriods(CreateExistingChargePeriods(existingStopDate)).Build();
+            var chargeCommand = new ChargeCommandBuilder().WithEndDateTime(incomingCommandEndDate).Build();
+
+            var sut = new UpdateChargeMustHaveEffectiveDateBeforeOrOnStopDateRule(
+                existingCharge,
+                chargeCommand);
+
+            // Act
+            var isValid = sut.IsValid;
+
+            // Assert
+            isValid.Should().Be(expectedIsValid);
+        }
+
+        private List<ChargePeriod> CreateExistingChargePeriods(Instant existingStopDate)
+        {
+            var existingPeriod1 = new ChargePeriodBuilder()
+                .WithStartDateTime(existingStopDate
+                    .Minus(Duration.FromDays(20)))
+                .WithEndDateTime(existingStopDate
+                    .Minus(Duration.FromDays(10)))
+                .Build();
+            var existingPeriod2 = new ChargePeriodBuilder()
+                .WithStartDateTime(existingStopDate
+                    .Minus(Duration.FromDays(10)))
+                .WithEndDateTime(existingStopDate)
+                .Build();
+
+            return new List<ChargePeriod> { existingPeriod1, existingPeriod2 };
+        }
+    }
+}
