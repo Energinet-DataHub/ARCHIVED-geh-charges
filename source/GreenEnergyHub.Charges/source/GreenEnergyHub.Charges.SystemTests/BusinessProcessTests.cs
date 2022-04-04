ab9bae75944d69426da464258cf07252159fb801
Peter Tandrup <PTA@energinet.dk>
2022-04-04 11:25:45 +0200

    Temp changing system test assert so it will fail in CD pipeline

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.SystemTests/BusinessProcessTests.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.SystemTests/BusinessProcessTests.cs
@@ -84,7 +84,7 @@ namespace GreenEnergyHub.Charges.SystemTests
 
             // Assert
             var content = await peekResponse!.Content.ReadAsStringAsync();
-            content.Should().Contain("ConfirmRequestChangeOfPriceList_MarketDocument");
+            content.Should().Contain("TemporarilyMakeSystemTestFailInCdPipelineOnPurpose");
             content.Should().Contain(expectedOpId);
 
             // Dequeue - throws XUnitException if dequeue not succeeding
