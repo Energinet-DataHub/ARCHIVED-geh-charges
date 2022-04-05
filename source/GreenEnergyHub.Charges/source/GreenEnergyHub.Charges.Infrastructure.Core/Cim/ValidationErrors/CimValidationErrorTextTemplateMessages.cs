f22b7a7cd4dacf9e5095e2aaf0b1a5f672dc4261
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2022-04-05 13:25:07 +0200

    No whitespace

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure.Core/Cim/ValidationErrors/CimValidationErrorTextTemplateMessages.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Infrastructure.Core/Cim/ValidationErrors/CimValidationErrorTextTemplateMessages.cs
@@ -136,7 +136,7 @@ namespace GreenEnergyHub.Charges.Infrastructure.Core.Cim.ValidationErrors
         [ErrorMessageFor(ValidationRuleIdentifier.UpdateChargeMustHaveEffectiveDateBeforeOrOnStopDate)]
         public const string UpdateChargeMustHaveEffectiveDateBeforeOrOnStopDateErrorText =
             "Charge ID {{DocumentSenderProvidedChargeId}} has been stopped and thus cannot be updated as per {{ChargeStartDateTime}}.";
-            
+
         [ErrorMessageFor(ValidationRuleIdentifier.SubsequentBundleOperationsFail)]
         public const string SubsequentBundleOperationsFail =
             "Transaction for Charge ID {{DocumentSenderProvidedChargeId}} is not completed, because a previous transaction for Charge ID {{TriggeredBySenderProvidedId}}, failed its validation";
