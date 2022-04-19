cde5529f3ca91c3d24b37c6390e5bc56fde9e940
Peter Tandrup <PTA@energinet.dk>
2022-04-19 14:46:25 +0200

    Drop table, use temporary duplicates, add UQ constraint

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202204191415 Ensure unique charges by removing duplicates and adding constraint.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202204191415 Ensure unique charges by removing duplicates and adding constraint.sql
@@ -1,47 +1,52 @@
 ﻿------------------------------------------------------------------------------------------------------------------------
--- Ensure charge data consistency, before adding a unique constraint to the Charge table
+-- Drop dbo.Duplicates table that was created in a previous script 
+-- '202202151610 Move charge periods to ChargePeriod table'
 ------------------------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS dbo.Duplicates
+    GO
 
+------------------------------------------------------------------------------------------------------------------------
+-- Ensure charge data consistency by removing charge duplicates (and any charge links to them)
+------------------------------------------------------------------------------------------------------------------------
 begin tran
 
 select
     Id,
     row_number() over(partition by SenderProvidedChargeId, [Type], OwnerId order by SenderProvidedChargeId, [Type], OwnerId) as rownumber
-into Duplicates1
+into #Duplicates
 from Charges.Charge
 
-delete cl
+    delete cl
     from Charges.ChargeLink as cl
-        inner join Duplicates1 as dpl
+        inner join #Duplicates as dpl
         on dpl.Id = cl.ChargeId
     where dpl.rownumber > 1
 
     delete cp
     from Charges.ChargePoint as cp
-        inner join Duplicates1 as dpl
+        inner join #Duplicates as dpl
         on dpl.Id = cp.ChargeId
     where dpl.rownumber > 1
 
 	delete cpd
     from Charges.ChargePeriod as cpd
-        inner join Duplicates1 as dpl
+        inner join #Duplicates as dpl
         on dpl.Id = cpd.ChargeId
     where dpl.rownumber > 1
 
     delete c
     from Charges.Charge as c
-        inner join Duplicates1 as dpl
+        inner join #Duplicates as dpl
         on dpl.Id = c.Id
     where dpl.rownumber > 1
-
-select
-    Id,
-    row_number() over(partition by SenderProvidedChargeId, [Type], OwnerId order by SenderProvidedChargeId, [Type], OwnerId) as rownumber
-from Charges.Charge
-order by rownumber desc
 
 commit
 
 ------------------------------------------------------------------------------------------------------------------------
--- Add unique constraint on Charge table entries; Charge must be unique on SenderProvidedChargeId, Type and OwnerId
+-- Add unique constraint on Charges.Charge to reject inserts for the same unique charge,
+-- identified by SenderProvidedChargeId, Type and OwnerId
 ------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE [Charges].[Charge]
+    ADD CONSTRAINT [UC_SenderProvidedChargeId_Type_OwnerId] UNIQUE (SenderProvidedChargeId, Type, OwnerId);
+GO
