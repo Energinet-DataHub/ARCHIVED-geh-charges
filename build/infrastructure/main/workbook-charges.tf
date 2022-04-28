a0bcdfef8c8e6366b4ae9755e795bd5fa7b00597
Jens Kloster <jmk@energinet.dk>
2022-04-28 13:50:34 +0200

    added version 0.1 for workbook

---
+++ build/infrastructure/main/workbook-charges.tf
@@ -0,0 +1,31 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+data "template_file" "workbook_charges_template" {
+  template = file("${path.module}/workbook-charges-template.json")
+  vars = {
+    subscription_id          = data.azurerm_subscription.this.subscription_id
+    resouce_group_name       = azurerm_resource_group.this.name
+    application_insight_name = data.azurerm_key_vault_secret.appi_shared_name.value
+  }
+}
+
+resource "azurerm_template_deployment" "workbook_charges" {
+  name                  = "workbook-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  resource_group_name   = azurerm_resource_group.this.name
+  location              = azurerm_resource_group.this.location
+  deployment_mode       = incremental
+  template_body         = data.template_file.workbook_charges_template.rendered
+  
+  tags                  = azurerm_resource_group.this.tags
+}
