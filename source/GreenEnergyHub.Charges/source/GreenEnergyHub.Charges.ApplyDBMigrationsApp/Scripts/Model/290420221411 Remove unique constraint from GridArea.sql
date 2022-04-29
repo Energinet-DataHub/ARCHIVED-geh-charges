9559f1b4082b4f432a17783ccbeb25b7e2178904
Jan Duelund <jdd@energinet.dk>
2022-04-29 14:15:08 +0200

    drop key constraint from gridarea

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/290420221411 Remove unique constraint from GridArea.sql
@@ -0,0 +1,6 @@
+﻿------------------------------------------------------------------------------------------------------------------------
+-- Drop constraints to marketparticpant table, allowing creation of gridareas without a marketparticipant
+------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE [Charges].[GridArea]
+DROP CONSTRAINT FK_GridArea_MarketParticipant;
