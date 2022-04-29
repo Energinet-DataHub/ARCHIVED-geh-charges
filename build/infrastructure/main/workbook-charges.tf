82b60448a0e4a7896b3c0a671da6a6cf5bb5b651
Jørn Justesen <jornjustesen@gmail.com>
2022-04-29 12:28:33 +0200

    shared_resouce_group_name was not working, trying other way of referencing it

--- build/infrastructure/main/workbook-charges.tf
+++ build/infrastructure/main/workbook-charges.tf
@@ -17,7 +17,7 @@ data "template_file" "workbook_charges_template" {
     subscription_id          = data.azurerm_subscription.this.subscription_id
     resouce_group_name       = azurerm_resource_group.this.name
     application_insight_name = data.azurerm_key_vault_secret.appi_shared_name.value
-    shared_resouce_group_name = data.azurerm_key_vault_secret.shared_resources_resource_group_name.value
+    shared_resouce_group_name = var.shared_resources_resource_group_name
 
   }
 }
