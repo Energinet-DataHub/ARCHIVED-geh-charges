74449fdb4dc85bb1281d975eb08b139103beca16
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-06-01 12:01:24 +0200

    feat: seperate charge information and prices integration events(#1385)
    Removes that we always sends Charge Information, now we
    only send the ChargeCreatedEvent if the message is a D18
    and only ChargePricesUpdatedEvent if it is a D08
    
    Due to a schema update the 2 flows are separate and this
    is the expected behavior.
    
    #1297

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/ChargeCommands/Validation/BusinessValidation/Factories/ChargeOperationBusinessValidationRulesFactory.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/ChargeCommands/Validation/BusinessValidation/Factories/ChargeOperationBusinessValidationRulesFactory.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using GreenEnergyHub.Charges.Core.DateTime;
 using GreenEnergyHub.Charges.Domain.Charges;
@@ -62,7 +63,8 @@ namespace GreenEnergyHub.Charges.Domain.Dtos.ChargeCommands.Validation.BusinessV
                 return rules;
             }
 
-            if (chargeOperationDto.Type == ChargeType.Tariff)
+            var isChargePrices = chargeOperationDto.Points.Any();
+            if (chargeOperationDto.Type == ChargeType.Tariff && isChargePrices is false)
             {
                 rules.AddRange(AddTariffOnlyRules(chargeOperationDto, charge));
             }
