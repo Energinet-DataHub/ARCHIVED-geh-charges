d72fcd3674e4cfcb80666a0507f0475e1ae0d0bd
Jan Duelund <jdd@energinet.dk>
2022-06-02 10:30:44 +0200

    database changes

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202206020915 Remove GridArea table.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202206020915 Remove GridArea table.sql
@@ -8,22 +8,22 @@ ALTER TABLE [Charges].[GridAreaLink]
 GO
 
 ALTER TABLE [Charges].[GridAreaLink]
-    DROP CONSTRAINT FK_GridAreaLink_GridArea;
-GO
-
-ALTER TABLE [Charges].[GridAreaLink]
-    ADD CONSTRAINT FK_OwnerId_
-    FOREIGN KEY (OwnerId) REFERENCES Persons(PersonID);
+DROP CONSTRAINT FK_GridAreaLink_GridArea;
 GO
 
 UPDATE [Charges].[GridAreaLink]
-    SET [Charges].[GridAreaLink].[OwnerId] = [Charges].[GridArea].[GridAccessProviderId]
-    FROM [Charges].[GridAreaLink]
+SET [Charges].[GridAreaLink].[OwnerId] = [Charges].[GridArea].[GridAccessProviderId]
+FROM [Charges].[GridAreaLink]
     JOIN [Charges].[GridArea] ON [Charges].[GridArea].[Id] = [Charges].[GridAreaLink].[GridAreaId]
 GO
 
 ALTER TABLE [Charges].[GridAreaLink]
     ADD [OwnerId] [uniqueidentifier] NOT NULL
+GO
+
+ALTER TABLE [Charges].[GridAreaLink] WITH CHECK
+    ADD CONSTRAINT [FK_GridAreaLink_MarketParticipant]
+    FOREIGN KEY ([OwnerId]) REFERENCES [Charges].[MarketParticipant] (Id);
 GO
 
 ------------------------------------------------------------------------------------------------------------------------
