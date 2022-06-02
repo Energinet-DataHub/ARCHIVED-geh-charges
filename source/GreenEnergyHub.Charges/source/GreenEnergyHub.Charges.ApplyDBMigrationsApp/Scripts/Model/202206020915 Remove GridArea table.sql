6af6b69810e3f797a54fce2c1d4486008ffdb0cc
Jan Duelund <jdd@energinet.dk>
2022-06-02 10:21:10 +0200

    database changes

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202206020915 Remove GridArea table.sql
@@ -0,0 +1,35 @@
+﻿------------------------------------------------------------------------------------------------------------------------
+-- Add OwnerId to GridAreaLink and populate with GridAccessProvider from GridArea
+------------------------------------------------------------------------------------------------------------------------
+BEGIN TRAN
+
+ALTER TABLE [Charges].[GridAreaLink]
+    ADD [OwnerId] [uniqueidentifier] NULL
+GO
+
+ALTER TABLE [Charges].[GridAreaLink]
+    DROP CONSTRAINT FK_GridAreaLink_GridArea;
+GO
+
+ALTER TABLE [Charges].[GridAreaLink]
+    ADD CONSTRAINT FK_OwnerId_
+    FOREIGN KEY (OwnerId) REFERENCES Persons(PersonID);
+GO
+
+UPDATE [Charges].[GridAreaLink]
+    SET [Charges].[GridAreaLink].[OwnerId] = [Charges].[GridArea].[GridAccessProviderId]
+    FROM [Charges].[GridAreaLink]
+    JOIN [Charges].[GridArea] ON [Charges].[GridArea].[Id] = [Charges].[GridAreaLink].[GridAreaId]
+GO
+
+ALTER TABLE [Charges].[GridAreaLink]
+    ADD [OwnerId] [uniqueidentifier] NOT NULL
+GO
+
+------------------------------------------------------------------------------------------------------------------------
+-- Drop Charges.GridArea table
+------------------------------------------------------------------------------------------------------------------------
+DROP TABLE [Charges].[GridArea]
+GO
+
+COMMIT TRAN
--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/290420221411 Remove unique constraint from GridArea.sql
+++
@@ -1,6 +0,0 @@
-﻿------------------------------------------------------------------------------------------------------------------------
--- Drop constraints to marketparticpant table, allowing creation of gridareas without a marketparticipant
-------------------------------------------------------------------------------------------------------------------------
-
-ALTER TABLE [Charges].[GridArea]
-DROP CONSTRAINT FK_GridArea_MarketParticipant;
