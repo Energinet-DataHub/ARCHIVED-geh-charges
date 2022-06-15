a6371a40e548f556ea4d3fad93a04440cc06f37c
HenrikSommer <54191286+HenrikSommer@users.noreply.github.com>
2022-06-15 10:42:08 +0200

    Publish and receive ChargePriceCommandReceivedEvent

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.FunctionHost/Charges/ChargePriceCommandReceiverEndpoint.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using GreenEnergyHub.Charges.Application.Charges.Handlers;
+using GreenEnergyHub.Charges.Domain.Dtos.ChargePriceCommandReceivedEvents;
+using GreenEnergyHub.Charges.FunctionHost.Common;
+using GreenEnergyHub.Charges.Infrastructure.Core.MessagingExtensions.Serialization;
+using Microsoft.Azure.Functions.Worker;
+
+namespace GreenEnergyHub.Charges.FunctionHost.Charges
+{
+    public class ChargePriceCommandReceiverEndpoint
+    {
+        private const string FunctionName = nameof(ChargePriceCommandReceiverEndpoint);
+        private readonly IChargePriceEventHandler _chargePriceEventHandler;
+        private readonly JsonMessageDeserializer<ChargePriceCommandReceivedEvent> _deserializer;
+
+        public ChargePriceCommandReceiverEndpoint(
+            IChargePriceEventHandler chargePriceEventHandler,
+            JsonMessageDeserializer<ChargePriceCommandReceivedEvent> deserializer)
+        {
+            _chargePriceEventHandler = chargePriceEventHandler;
+            _deserializer = deserializer;
+        }
+
+        [Function(FunctionName)]
+        public async Task RunAsync(
+            [ServiceBusTrigger(
+                "%" + EnvironmentSettingNames.PriceCommandReceivedTopicName + "%",
+                "%" + EnvironmentSettingNames.PriceCommandReceivedSubscriptionName + "%",
+                Connection = EnvironmentSettingNames.DomainEventListenerConnectionString)]
+            byte[] message)
+        {
+            var receivedEvent = (ChargePriceCommandReceivedEvent)await _deserializer.FromBytesAsync(message).ConfigureAwait(false);
+            // TODO call: _chargePriceEventHandler.HandleAsync(receivedEvent);
+        }
+    }
+}
