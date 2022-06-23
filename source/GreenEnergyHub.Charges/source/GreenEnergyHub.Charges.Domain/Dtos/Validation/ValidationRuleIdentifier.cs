11ee3a262825280f2c1eb8540ff829d6f39533d7
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-06-23 13:26:56 +0200

    fix enum

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/Validation/ValidationRuleIdentifier.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/Validation/ValidationRuleIdentifier.cs
@@ -53,6 +53,6 @@ namespace GreenEnergyHub.Charges.Domain.Dtos.Validation
         ChargeDescriptionIsRequired = 39, // VR911 / E0H
         ResolutionIsRequired = 40, // VR912 / E0H
         ChargeOwnerHasLengthLimits = 41, // VR913 / E86
-        TransparentInvoicingIsRequired = 41, // VR915 / E0H
+        TransparentInvoicingIsRequired = 42, // VR915 / E0H
     }
 }
