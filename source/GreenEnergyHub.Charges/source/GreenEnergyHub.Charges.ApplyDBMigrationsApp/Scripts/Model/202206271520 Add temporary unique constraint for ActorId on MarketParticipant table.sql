8768997485cf51a18805e261cddbd135515e797f
Peter Tandrup <PTA@energinet.dk>
2022-06-28 07:33:27 +0200

    Minor commit for re-running github checks

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202206271520 Add temporary unique constraint for ActorId on MarketParticipant table.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202206271520 Add temporary unique constraint for ActorId on MarketParticipant table.sql
@@ -1,5 +1,5 @@
 ﻿------------------------------------------------------------------------------------------------------------------------
--- Add unique constraint on ActorId to Charges.MarketParticipant
+-- Add unique constraint on ActorId to Charges.MarketParticipant.
 -- This is considered a temporary guard, which will be beneficial to have while transitioning to integration events
 -- published by the MarketParticipant domain. Especially, when invoking 'force send integration events'.  
 -- Temporary until Charges allow multiple business process roles per ActorId. 
