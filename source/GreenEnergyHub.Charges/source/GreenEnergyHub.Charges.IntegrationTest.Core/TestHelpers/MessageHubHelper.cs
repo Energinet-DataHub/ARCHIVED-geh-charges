429958edfc4795ac96558766924ab1314cd4775e
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2022-06-29 23:33:18 +0200

    MessageHubHelper returns peek message results (#1442)
    The returnet peek message result greatly improve the developer's experience using the MessageHubSimulation for peek'ing messages delivered to MessageHub, and enhances the quality of the integration tests using MessageHubSimulation.

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/TestHelpers/MessageHubHelper.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/TestHelpers/MessageHubHelper.cs
@@ -13,8 +13,13 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Energinet.DataHub.MessageHub.IntegrationTesting;
+using GreenEnergyHub.Charges.IntegrationTest.Core.Fixtures.FunctionApp;
 
 namespace GreenEnergyHub.Charges.IntegrationTest.Core.TestHelpers
 {
@@ -23,22 +28,19 @@ namespace GreenEnergyHub.Charges.IntegrationTest.Core.TestHelpers
         /// <summary>
         /// Listen for dataAvailable events, initiates a peek and assert that a reply is received.
         /// </summary>
-        public static async Task AssertPeekReceivesRepliesAsync(
+        public static async Task<List<string>> AssertPeekReceivesRepliesAsync(
             this MessageHubSimulation messageHub,
             string correlationId,
             int noOfMessageTypes = 1)
         {
             var noOfReceivedMessages = 0;
+            var peekResults = new List<string>();
 
             for (var i = 0; i < noOfMessageTypes; i++)
             {
                 try
                 {
-                    // Throws if expected data available message (by correlation ID) is not received
-                    await messageHub.WaitForNotificationsInDataAvailableQueueAsync(correlationId);
-
-                    // Invokes the domain and ensures that a reply to the peek request is received for each message type
-                    await messageHub.PeekAsync(); // Throws if corresponding peek reply is not received
+                    peekResults.Add(await WaitForDataAvailableAndPeek(messageHub, correlationId));
                     noOfReceivedMessages++;
                 }
                 catch (Exception ex) when (ex is TaskCanceledException or TimeoutException)
@@ -51,6 +53,39 @@ namespace GreenEnergyHub.Charges.IntegrationTest.Core.TestHelpers
                     messageHub.Clear();
                 }
             }
+
+            return peekResults;
+        }
+
+        private static async Task<string> WaitForDataAvailableAndPeek(
+            MessageHubSimulation messageHub, string correlationId)
+        {
+            // Throws if expected data available message (by correlation ID) is not received
+            await messageHub.WaitForNotificationsInDataAvailableQueueAsync(correlationId);
+
+            // Invokes the domain and ensures that a reply to the peek request is received for each message type
+            var peekSimulationResponseDto = await messageHub.PeekAsync(); // Throws if corresponding peek reply is not received
+            return await DownloadPeekResult(peekSimulationResponseDto);
+        }
+
+        private static async Task<string> DownloadPeekResult(PeekSimulationResponseDto peekSimulationResponseDto)
+        {
+            ArgumentNullException.ThrowIfNull(peekSimulationResponseDto);
+            ArgumentNullException.ThrowIfNull(peekSimulationResponseDto.Content!.Path);
+
+            var uri = peekSimulationResponseDto.Content.Path;
+            var availableDataReferenceId = uri.Segments.Last().TrimEnd('/');
+
+            const string connectionString = ChargesServiceBusResourceNames.MessageHubStorageConnectionString;
+            const string blobContainerName = ChargesServiceBusResourceNames.MessageHubStorageContainerName;
+
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            var blobClient = blobServiceClient
+                .GetBlobContainerClient(blobContainerName)
+                .GetBlobClient(availableDataReferenceId);
+
+            BlobDownloadResult downloadResult = await blobClient.DownloadContentAsync();
+            return downloadResult.Content.ToString();
         }
     }
 }
