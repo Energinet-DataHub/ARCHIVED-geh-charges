fb4d33a9f9f3fd413c93c532ad43195e35dddd30
Peter Tandrup <71757561+prtandrup@users.noreply.github.com>
2022-07-01 08:42:51 +0200

    Clean-up: `ChargeTests.cs` (#1440)
    If applied, refactors/cleans-up ChargeTests.cs after ChargeBuilder was refactored to 
    only allow Charge creation through the domain model Charge.cs.

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Charges/Charge.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Charges/Charge.cs
@@ -256,8 +256,10 @@ namespace GreenEnergyHub.Charges.Domain.Charges
 
         private void StopExistingPeriod(Instant stopDate)
         {
-            var previousPeriod = _periods.OrderByDescending(p => p.StartDateTime)
-                .FirstOrDefault(p =>
+            // When implementing this validation:
+            // 'Cannot perform a stop charge when the charge is already stopped on the same date',
+            // then p.EndDateTime = stopDate can be removed from conditions below, simplifying the code.
+            var previousPeriod = _periods.FirstOrDefault(p =>
                     p.EndDateTime >= stopDate &&
                     p.StartDateTime <= stopDate);
 
