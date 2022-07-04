5d3f66e1ba941f387118ca5a6a159cb77ed8cba1
Henrik Sommer <XHESO@energinet.dk>
2022-07-04 16:11:27 +0200

    Docs: minor improvements

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.FunctionHost/Configuration/ChargePriceCommandReceiverConfiguration.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.FunctionHost/Configuration/ChargePriceCommandReceiverConfiguration.cs
@@ -23,6 +23,7 @@ namespace GreenEnergyHub.Charges.FunctionHost.Configuration
     {
         internal static void ConfigureServices(IServiceCollection serviceCollection)
         {
+            // TODO: Change to new ChargePriceEventHandler
             serviceCollection.AddScoped<IChargePriceEventHandler, ChargePriceEventHandlerDeprecated>();
             ConfigureMessaging(serviceCollection);
         }
