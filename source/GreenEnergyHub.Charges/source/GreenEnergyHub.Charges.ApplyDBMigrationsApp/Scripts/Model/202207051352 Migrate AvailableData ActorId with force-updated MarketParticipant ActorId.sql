a37dba201ccf00abbf8e7f989ccb3a3e30ae0edf
Peter Tandrup <PTA@energinet.dk>
2022-07-05 13:59:37 +0200

    Remove DataAvailable tables foreign keys

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202207051352 Migrate AvailableData ActorId with force-updated MarketParticipant ActorId.sql
@@ -0,0 +1,26 @@
+﻿
+begin tran
+ 
+    ------------------------------------------------------------------------------------------------------------------------
+    -- Remove DataAvailable tables' foreign key.
+    -- This is needed for successfully force updating market participants data in Charges through integration events
+    -- from the Market Participant domain)
+    ------------------------------------------------------------------------------------------------------------------------
+
+    alter table MessageHub.AvailableChargeData
+    drop constraint FK_AvailableChargeData_ActorId
+        go
+    
+    alter table MessageHub.AvailableChargeReceiptData
+    drop constraint FK_AvailableChargeReceiptData_ActorId
+        go
+    
+    alter table MessageHub.AvailableChargeLinksData
+    drop constraint FK_AvailableChargeLinksData_ActorId
+        go
+    
+    alter table MessageHub.AvailableChargeLinksReceiptData
+    drop constraint FK_AvailableChargeLinksReceiptData_ActorId
+    go
+   
+commit
