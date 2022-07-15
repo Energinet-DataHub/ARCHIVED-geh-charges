87b7af1b6df3752427dd65f19c8775856dba7c1b
Søren Søndergaard <s.sondergaard@icloud.com>
2022-07-15 08:09:48 +0200

    Test from dotnet

---
+++ source/azlogin/IConfigurationExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.Extensions.Configuration;
+
+namespace Azlogin
+{
+    public static class IConfigurationExtensions
+    {
+        /// <summary>
+        /// This extension method first try to read the setting value as if it was stored in "local.setting.json";
+        /// if the value is empty, it then try to retrieve it directly in the configuration container.
+        ///
+        /// NOTICE: Azure Function App settings in "local.settings.json" are stored in the section "Values".
+        /// </summary>
+        public static string GetValue(this IConfiguration configuration, string settingName)
+        {
+            var value = configuration[$"Values:{settingName}"];
+            return string.IsNullOrEmpty(value)
+                ? configuration[settingName]
+                : value;
+        }
+    }
+}
---
+++ source/azlogin/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azlogin;
+using Microsoft.Extensions.Configuration;
+
+var envConfiguration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var keyVaultUrl = envConfiguration.GetValue("AZURE_KEYVAULT_URL");
+
+if (keyVaultUrl.StartsWith("https://kv-integrationtest-u-002", StringComparison.InvariantCultureIgnoreCase))
+{
+    Console.WriteLine("Url matches");
+}
+else
+{
+    Console.WriteLine("Url does not match the expected");
+}
+
+var keyVaultConfiguration = new ConfigurationBuilder()
+    .AddAuthenticatedAzureKeyVault(keyVaultUrl)
+    .Build();
+
+Console.WriteLine("Built configuration from keyvault");
