05bdc49a2913e3aa8c398c821d78da6b3df1ec18
Peter Tandrup <PTA@energinet.dk>
2022-08-11 10:55:47 +0200

    Initial OutboxMessageFactoryTests.cs

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Infrastructure/Outbox/OutboxMessageFactoryTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using System.Linq;
+using Albedo;
+using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
+using FluentAssertions;
+using GreenEnergyHub.Charges.Application.Charges.Events;
+using GreenEnergyHub.Charges.Domain.Dtos.ChargePriceCommands;
+using GreenEnergyHub.Charges.Domain.Dtos.SharedDtos;
+using GreenEnergyHub.Charges.Domain.Dtos.Validation;
+using GreenEnergyHub.Charges.Infrastructure.Outbox;
+using GreenEnergyHub.Charges.Tests.Builders.Command;
+using GreenEnergyHub.Charges.Tests.Domain.Dtos.ChargeCommands.Validation;
+using GreenEnergyHub.Json;
+using GreenEnergyHub.TestHelpers;
+using Microsoft.OpenApi.Validations;
+using NodaTime;
+using Xunit;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.Tests.Infrastructure.Outbox
+{
+    [UnitTest]
+    public class OutboxMessageFactoryTests
+    {
+        [Theory]
+        [AutoDomainData]
+        public void WhenOperationsRejectedEvent_ReturnsOutboxMessage_WithSerializedData(
+            JsonSerializer jsonSerializer,
+            IClock clock,
+            ICorrelationContext context,
+            ChargePriceOperationDtoBuilder builder)
+        {
+            var operationList = new List<ChargePriceOperationDto> { builder.Build() };
+            var rejectionRulesList = new List<IValidationRuleContainer>
+            {
+                new OperationValidationRuleContainer(
+                    new TestValidationRule(false, ValidationRuleIdentifier.MaximumPrice),
+                    operationList.First().Id),
+            };
+            var rejectedEvent = new OperationsRejectedEvent(
+                operationList,
+                "1337",
+                MarketParticipantRole.GridAccessProvider,
+                rejectionRulesList);
+
+            var factory = new OutboxMessageFactory(jsonSerializer, clock, context);
+
+            var message = factory.CreateFrom(rejectedEvent);
+
+            message.Should().NotBeNull();
+        }
+    }
+}
