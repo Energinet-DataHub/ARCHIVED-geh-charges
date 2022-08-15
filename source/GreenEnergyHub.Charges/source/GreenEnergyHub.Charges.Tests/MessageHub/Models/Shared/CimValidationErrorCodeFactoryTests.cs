a90be2ab91c4c9487c8d146918a409d2757c0c96
Peter Tandrup <PTA@energinet.dk>
2022-08-15 15:48:51 +0200

    Remove - FeeMustHaveSinglePrice validation rule 'leftovers'

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/MessageHub/Models/Shared/CimValidationErrorCodeFactoryTests.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/MessageHub/Models/Shared/CimValidationErrorCodeFactoryTests.cs
@@ -39,7 +39,6 @@ namespace GreenEnergyHub.Charges.Tests.MessageHub.Models.Shared
         [InlineAutoMoqData(ValidationRuleIdentifier.ChargeOwnerIsRequiredValidation, ReasonCode.E0H)]
         [InlineAutoMoqData(ValidationRuleIdentifier.ChargeTypeIsKnownValidation, ReasonCode.E86)]
         [InlineAutoMoqData(ValidationRuleIdentifier.ChargeTypeTariffPriceCount, ReasonCode.E87)]
-        [InlineAutoMoqData(ValidationRuleIdentifier.FeeMustHaveSinglePrice, ReasonCode.E87)]
         [InlineAutoMoqData(ValidationRuleIdentifier.RecipientIsMandatoryTypeValidation, ReasonCode.D02)]
         [InlineAutoMoqData(ValidationRuleIdentifier.SenderIsMandatoryTypeValidation, ReasonCode.D02)]
         [InlineAutoMoqData(ValidationRuleIdentifier.StartDateTimeRequiredValidation, ReasonCode.E0H)]
