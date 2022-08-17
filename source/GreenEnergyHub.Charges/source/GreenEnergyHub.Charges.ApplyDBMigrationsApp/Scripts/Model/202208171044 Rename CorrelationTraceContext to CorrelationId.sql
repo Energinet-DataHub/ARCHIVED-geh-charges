d72a954f5be807e73855fdd6f21ebaf652a7b83c
Jan Duelund <jdd@energinet.dk>
2022-08-17 11:11:39 +0200

    another db fix

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202208171044 Rename CorrelationTraceContext to CorrelationId.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202208171044 Rename CorrelationTraceContext to CorrelationId.sql
@@ -2,4 +2,4 @@
 -- Rename column
 ------------------------------------------------------------------------------------------------------------------------
 
-EXEC SP_RENAME 'Charges.OutboxMessage.CorrelationTraceContext', 'Charges.OutboxMessage.OutboxMessage.CorrelationId', 'COLUMN'
+EXEC SP_RENAME 'Charges.OutboxMessage.CorrelationTraceContext', 'CorrelationId', 'COLUMN'
