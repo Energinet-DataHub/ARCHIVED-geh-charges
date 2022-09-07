95c0417da4c261aa0afe1f61da340d1d7325a9ee
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2022-09-07 15:24:03 +0200

    Update source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/ChargeLinks/Handlers/ChargeLinksReceivedEventHandlerTests.cs
    Co-authored-by: Peter Tandrup <71757561+prtandrup@users.noreply.github.com>

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/ChargeLinks/Handlers/ChargeLinksReceivedEventHandlerTests.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Tests/Application/ChargeLinks/Handlers/ChargeLinksReceivedEventHandlerTests.cs
@@ -154,7 +154,7 @@ namespace GreenEnergyHub.Charges.Tests.Application.ChargeLinks.Handlers
 
         [Theory]
         [InlineAutoMoqData]
-        public async Task GivenHandleAsync_WhenValidationFails_ShouldLogValidationErrors(
+        public async Task HandleAsync_WhenValidationFails_ShouldLogValidationErrors(
             Mock<ILoggerFactory> loggerFactory,
             Mock<ILogger> logger,
             Mock<IChargeLinksReceiptService> chargeLinksReceiptService,
