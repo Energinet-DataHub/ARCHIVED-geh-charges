64f1217c82617ea433e618aaf9cd91fe2d84f5be
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2022-09-07 15:07:49 +0200

    Update source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/Fixtures/FunctionApp/ChargesServiceBusResourceNames.cs
    Co-authored-by: Peter Tandrup <71757561+prtandrup@users.noreply.github.com>

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/Fixtures/FunctionApp/ChargesServiceBusResourceNames.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/Fixtures/FunctionApp/ChargesServiceBusResourceNames.cs
@@ -70,7 +70,7 @@ namespace GreenEnergyHub.Charges.IntegrationTest.Core.Fixtures.FunctionApp
         public const string ChargePriceRejectedTopicKey = "charge-price-rejected";
         public const string ChargePriceRejectedSubscriptionName = "charge-price-rejected";
 
-        // Internal, charge price operations, rejected
+        // Internal, charge price operations, confirmed
         public const string ChargePriceConfirmedTopicKey = "charge-price-confirmed";
         public const string ChargePriceConfirmedSubscriptionName = "charge-price-confirmed";
 
