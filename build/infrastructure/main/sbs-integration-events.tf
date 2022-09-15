b8e18c7d16a9de8ccc8778a5c172c9ec77d6f5ca
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-09-15 13:47:44 +0200

    fix sbs tf module version

--- build/infrastructure/main/sbs-integration-events.tf
+++ build/infrastructure/main/sbs-integration-events.tf
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 module "sbs_int_events_metering_point_created" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v8"
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v9"
   name                = "metering-point-created"
   project_name        = var.domain_name_short
   topic_id            = data.azurerm_key_vault_secret.sbt_domainrelay_integrationevent_received_id.value
@@ -26,7 +26,7 @@ module "sbs_int_events_metering_point_created" {
 }
 
 module "sbs_int_events_market_participant_changed" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v8"
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic-subscription?ref=v9"
   name                = "market-participant-changed"
   project_name        = var.domain_name_short
   topic_id            = data.azurerm_key_vault_secret.sbt_domainrelay_integrationevent_received_id.value
