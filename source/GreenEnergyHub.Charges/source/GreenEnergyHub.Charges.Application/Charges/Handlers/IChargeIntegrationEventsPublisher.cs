e8338da5795557ec084fa8588ef65566fe0c801f
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2022-09-20 14:44:15 +0200

    Update source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/Handlers/IChargeIntegrationEventsPublisher.cs
    Co-authored-by: Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/Handlers/IChargeIntegrationEventsPublisher.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/Charges/Handlers/IChargeIntegrationEventsPublisher.cs
@@ -19,6 +19,6 @@ namespace GreenEnergyHub.Charges.Application.Charges.Handlers
 {
     public interface IChargeIntegrationEventsPublisher
     {
-        Task PublishAsync(ChargeInformationOperationsAcceptedEvent chargeInformationCommandAcceptedEvent);
+        Task PublishAsync(ChargeInformationOperationsAcceptedEvent chargeInformationOperationsAcceptedEvent);
     }
 }
