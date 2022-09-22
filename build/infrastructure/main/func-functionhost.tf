e8562bebf9e551d4997c74c66191ad0c95fbe50c
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2022-09-22 10:35:08 +0200

    impl

--- build/infrastructure/main/func-functionhost.tf
+++ build/infrastructure/main/func-functionhost.tf
@@ -57,7 +57,7 @@ module "func_functionhost" {
     CHARGE_INFORMATION_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                = module.sbts_charges_charge_command_accepted.name
     CHARGE_INFORMATION_OPERATIONS_REJECTED_SUBSCRIPTION_NAME                = module.sbts_charges_charge_command_rejected.name
     CHARGE_PRICE_COMMAND_RECEIVED_SUBSCRIPTION_NAME                         = module.sbts_charges_charge_price_command_received.name
-    CHARGE_PRICE_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTION_NAME        = module.sbts_charges_charge_price_command_confirmed_dataavailable
+    CHARGE_PRICE_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTION_NAME        = module.sbts_charges_charge_price_command_confirmed_dataavailable.name
     CHARGE_PRICE_OPERATIONS_ACCEPTED_PUBLISH_SUBSCRIPTION_NAME              = module.sbts_charges_charge_price_confirmed_publish.name
     CHARGE_PRICE_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_confirmed.name
     CHARGE_PRICE_OPERATIONS_REJECTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_rejected.name
