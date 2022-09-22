3a83f157a1e072e9f3a3d6d399a1fdd802aed9a5
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2022-09-22 10:29:47 +0200

    Reverted removal of wrong sbt subscription

--- build/infrastructure/main/func-functionhost.tf
+++ build/infrastructure/main/func-functionhost.tf
@@ -51,15 +51,16 @@ module "func_functionhost" {
     CHARGE_LINKS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTION_NAME                   = module.sbts_charges_charge_links_accepted_dataavailable.name
     CHARGE_LINKS_ACCEPTED_CONFIRMATION_SUBSCRIPTION_NAME                    = module.sbts_charges_charge_links_accepted_confirmation.name
     CHARGE_LINKS_COMMAND_RECEIVED_SUBSCRIPTION_NAME                         = module.sbts_charges_charge_links_command_received.name
-    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_PUBLISH_SUBSCRIPTION_NAME        = module.sbts_charges_charge_command_accepted_publish.name
-    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTIONNAME   = module.sbts_charges_charge_accepted_dataavailable.name
-    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                = module.sbts_charges_charge_command_accepted.name
     CHARGE_INFORMATION_COMMAND_RECEIVED_SUBSCRIPTION_NAME                   = module.sbts_charges_charge_command_received.name
+    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTIONNAME   = module.sbts_charges_charge_accepted_dataavailable.name
+    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_PUBLISH_SUBSCRIPTION_NAME        = module.sbts_charges_charge_command_accepted_publish.name
+    CHARGE_INFORMATION_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                = module.sbts_charges_charge_command_accepted.name
     CHARGE_INFORMATION_OPERATIONS_REJECTED_SUBSCRIPTION_NAME                = module.sbts_charges_charge_command_rejected.name
-    CHARGE_PRICE_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTION_NAME        = module.sbts_charges_charge_price_command_received.name
-    CHARGE_PRICE_OPERATIONS_REJECTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_rejected.name
-    CHARGE_PRICE_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_confirmed.name
+    CHARGE_PRICE_COMMAND_RECEIVED_SUBSCRIPTION_NAME                         = module.sbts_charges_charge_price_command_received.name
+    CHARGE_PRICE_OPERATIONS_ACCEPTED_DATAAVAILABLE_SUBSCRIPTION_NAME        = module.sbts_charges_charge_price_command_confirmed_dataavailable
     CHARGE_PRICE_OPERATIONS_ACCEPTED_PUBLISH_SUBSCRIPTION_NAME              = module.sbts_charges_charge_price_confirmed_publish.name
+    CHARGE_PRICE_OPERATIONS_ACCEPTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_confirmed.name
+    CHARGE_PRICE_OPERATIONS_REJECTED_SUBSCRIPTION_NAME                      = module.sbts_charges_charge_price_command_rejected.name
     DEFAULT_CHARGE_LINKS_DATAAVAILABLE_SUBSCRIPTION_NAME                    = module.sbts_charges_default_charge_links_dataavailable.name
 
     # Integration
