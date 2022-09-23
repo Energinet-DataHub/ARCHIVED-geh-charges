746272a4ae9c14159fd5601eaf8a164e6f52bca8
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-09-23 11:09:20 +0200

    split charge ingestion test into 2 files based on d08 or d18

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/DomainTests/ChargeIngestionTestsChargeInformation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using GreenEnergyHub.Charges.IntegrationTest.Core.Fixtures.FunctionApp;
+using GreenEnergyHub.Charges.IntegrationTest.Core.TestFiles.Charges;
+using GreenEnergyHub.Charges.IntegrationTest.Core.TestHelpers;
+using GreenEnergyHub.Charges.IntegrationTests.Fixtures;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace GreenEnergyHub.Charges.IntegrationTests.DomainTests
+{
+    [IntegrationTest]
+    public class ChargeIngestionTestChargeInformation
+    {
+        private const string EndpointUrl = "api/ChargeIngestion";
+        private const int SecondsToWaitForIntegrationEvents = 15;
+
+        [Collection(nameof(ChargesFunctionAppCollectionFixture))]
+        public class RunAsync : FunctionAppTestBase<ChargesFunctionAppFixture>, IAsyncLifetime
+        {
+            public RunAsync(ChargesFunctionAppFixture fixture, ITestOutputHelper testOutputHelper)
+                : base(fixture, testOutputHelper)
+            {
+            }
+
+            public Task InitializeAsync()
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task DisposeAsync()
+            {
+                Fixture.ChargeCreatedListener.Reset();
+                Fixture.ChargePricesUpdatedListener.Reset();
+                Fixture.MessageHubMock.Clear();
+
+                // We need to clear host log after each test is done to ensure that we can assert on function executed
+                // on each test run because we only check on function name.
+                Fixture.HostManager.ClearHostLog();
+                return Task.CompletedTask;
+            }
+
+            [Fact]
+            public async Task When_ChargeInformationIsReceived_Then_ChargeCreatedIntegrationEventIsPublished()
+            {
+                // Arrange
+                var (request, correlationId) = Fixture.AsSystemOperator.PrepareHttpPostRequestWithAuthorization(
+                    EndpointUrl, ChargeDocument.TariffBundleWithCreateAndUpdate);
+                using var eventualChargeCreatedEvent = await Fixture
+                    .ChargeCreatedListener
+                    .ListenForMessageAsync(correlationId)
+                    .ConfigureAwait(false);
+
+                // Act
+                await Fixture.HostManager.HttpClient.SendAsync(request);
+
+                // Assert
+                var isChargeCreatedReceived = eventualChargeCreatedEvent
+                    .MessageAwaiter!
+                    .Wait(TimeSpan.FromSeconds(SecondsToWaitForIntegrationEvents));
+                isChargeCreatedReceived.Should().BeTrue();
+            }
+
+            [Fact]
+            public async Task Given_NewMessage_When_SenderIdDoesNotMatchAuthenticatedId_Then_ShouldReturnErrorMessage()
+            {
+                // Arrange
+                var (request, _) = Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
+                    EndpointUrl, ChargeDocument.ChargeDocumentWhereSenderIdDoNotMatchAuthorizedActorId);
+
+                // Act
+                var actual = await Fixture.HostManager.HttpClient.SendAsync(request);
+
+                // Assert
+                actual.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+                var errorMessage = await actual.Content.ReadAsStreamAsync();
+                var document = await XDocument.LoadAsync(errorMessage, LoadOptions.None, CancellationToken.None);
+                document.Element("Message")?.Value.Should().Be(ErrorMessageConstants.ActorIsNotWhoTheyClaimToBeErrorMessage);
+            }
+
+            [Fact(Skip = "Used for debugging ChargeCommandReceivedEventHandler")]
+            public async Task Given_UpdateRequest_When_GridAccessProviderPeeks_Then_MessageHubReceivesReply()
+            {
+                // Arrange - Create
+                var (createReq, correlationId) = Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
+                    EndpointUrl, ChargeDocument.CreateTariff);
+
+                var response = await Fixture.HostManager.HttpClient.SendAsync(createReq);
+                response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+                await Fixture.MessageHubMock.AssertPeekReceivesRepliesAsync(correlationId);
+
+                // Arrange - Update
+                var (updateReq, updateCorrelationId) =
+                    Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
+                        EndpointUrl, ChargeDocument.UpdateTariff);
+
+                // Act
+                var actual = await Fixture.HostManager.HttpClient.SendAsync(updateReq);
+
+                // Assert
+                actual.StatusCode.Should().Be(HttpStatusCode.Accepted);
+
+                using var assertionScope = new AssertionScope();
+                // * Expect one for the confirmation (update)
+                var peekResults = await Fixture.MessageHubMock.AssertPeekReceivesRepliesAsync(updateCorrelationId);
+                peekResults.Should().ContainMatch("*ConfirmRequestChangeOfPriceList_MarketDocument*");
+            }
+        }
+    }
+}
--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/DomainTests/ChargeIngestionTestsChargePrices.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTests/DomainTests/ChargeIngestionTestsChargePrices.cs
@@ -41,7 +41,7 @@ using Xunit.Categories;
 namespace GreenEnergyHub.Charges.IntegrationTests.DomainTests
 {
     [IntegrationTest]
-    public class ChargeIngestionTests
+    public class ChargeIngestionTestsChargePrices
     {
         private const string EndpointUrl = "api/ChargeIngestion";
         private const int SecondsToWaitForIntegrationEvents = 15;
@@ -83,23 +83,6 @@ namespace GreenEnergyHub.Charges.IntegrationTests.DomainTests
             }
 
             [Fact]
-            public async Task Given_NewMessage_When_SenderIdDoesNotMatchAuthenticatedId_Then_ShouldReturnErrorMessage()
-            {
-                // Arrange
-                var (request, _) = Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
-                    EndpointUrl, ChargeDocument.ChargeDocumentWhereSenderIdDoNotMatchAuthorizedActorId);
-
-                // Act
-                var actual = await Fixture.HostManager.HttpClient.SendAsync(request);
-
-                // Assert
-                actual.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-                var errorMessage = await actual.Content.ReadAsStreamAsync();
-                var document = await XDocument.LoadAsync(errorMessage, LoadOptions.None, CancellationToken.None);
-                document.Element("Message")?.Value.Should().Be(ErrorMessageConstants.ActorIsNotWhoTheyClaimToBeErrorMessage);
-            }
-
-            [Fact]
             public async Task When_ChargeIsReceived_Then_AHttp202ResponseWithEmptyBodyIsReturned()
             {
                 var request = Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
@@ -110,27 +93,6 @@ namespace GreenEnergyHub.Charges.IntegrationTests.DomainTests
 
                 actualResponse.StatusCode.Should().Be(HttpStatusCode.Accepted);
                 responseBody.Should().BeEmpty();
-            }
-
-            [Fact]
-            public async Task When_ChargeInformationIsReceived_Then_ChargeCreatedIntegrationEventIsPublished()
-            {
-                // Arrange
-                var (request, correlationId) = Fixture.AsSystemOperator.PrepareHttpPostRequestWithAuthorization(
-                    EndpointUrl, ChargeDocument.TariffBundleWithCreateAndUpdate);
-                using var eventualChargeCreatedEvent = await Fixture
-                    .ChargeCreatedListener
-                    .ListenForMessageAsync(correlationId)
-                    .ConfigureAwait(false);
-
-                // Act
-                await Fixture.HostManager.HttpClient.SendAsync(request);
-
-                // Assert
-                var isChargeCreatedReceived = eventualChargeCreatedEvent
-                    .MessageAwaiter!
-                    .Wait(TimeSpan.FromSeconds(SecondsToWaitForIntegrationEvents));
-                isChargeCreatedReceived.Should().BeTrue();
             }
 
             [Fact]
@@ -380,34 +342,6 @@ namespace GreenEnergyHub.Charges.IntegrationTests.DomainTests
                 notification.Should().NotContain("<cim:VATPayer>");
                 notification.Should().NotContain("<cim:transparentInvoicing>");
                 notification.Should().NotContain("<cim:taxIndicator>");
-            }
-
-            [Fact(Skip = "Used for debugging ChargeCommandReceivedEventHandler")]
-            public async Task Given_UpdateRequest_When_GridAccessProviderPeeks_Then_MessageHubReceivesReply()
-            {
-                // Arrange - Create
-                var (createReq, correlationId) = Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
-                    EndpointUrl, ChargeDocument.CreateTariff);
-
-                var response = await Fixture.HostManager.HttpClient.SendAsync(createReq);
-                response.StatusCode.Should().Be(HttpStatusCode.Accepted);
-                await Fixture.MessageHubMock.AssertPeekReceivesRepliesAsync(correlationId);
-
-                // Arrange - Update
-                var (updateReq, updateCorrelationId) =
-                    Fixture.AsGridAccessProvider.PrepareHttpPostRequestWithAuthorization(
-                    EndpointUrl, ChargeDocument.UpdateTariff);
-
-                // Act
-                var actual = await Fixture.HostManager.HttpClient.SendAsync(updateReq);
-
-                // Assert
-                actual.StatusCode.Should().Be(HttpStatusCode.Accepted);
-
-                using var assertionScope = new AssertionScope();
-                // * Expect one for the confirmation (update)
-                var peekResults = await Fixture.MessageHubMock.AssertPeekReceivesRepliesAsync(updateCorrelationId);
-                peekResults.Should().ContainMatch("*ConfirmRequestChangeOfPriceList_MarketDocument*");
             }
 
             [Fact]
