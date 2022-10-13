0cf35b9553b6bef1d3e93373d0592955f699a1e8
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-10-13 10:11:17 +0200

    feat: add price series validation rule for end date and stop date (#1725)
    A monthly price series' END date time must be 1st of month, unless it matches the Charge's STOP date.
    
    #1400

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Domain/Dtos/ChargePriceCommands/Validation/BusinessValidation/ValidationRules/MonthlyPriceSeriesEndDateMustBeFirstOfMonthOrEqualChargeStopDateRule.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GreenEnergyHub.Charges.Core.DateTime;
+using GreenEnergyHub.Charges.Domain.Charges;
+using GreenEnergyHub.Charges.Domain.Dtos.Validation;
+using NodaTime;
+
+namespace GreenEnergyHub.Charges.Domain.Dtos.ChargePriceCommands.Validation.BusinessValidation.ValidationRules
+{
+    public class MonthlyPriceSeriesEndDateMustBeFirstOfMonthOrEqualChargeStopDateRule : IValidationRule
+    {
+        private readonly Resolution _resolution;
+        private readonly Instant _pointsIntervalEndDate;
+        private readonly Instant _chargeStopDate;
+
+        public MonthlyPriceSeriesEndDateMustBeFirstOfMonthOrEqualChargeStopDateRule(Resolution resolution, Instant pointsIntervalEndDate, Instant chargeStopDate)
+        {
+            _resolution = resolution;
+            _pointsIntervalEndDate = pointsIntervalEndDate;
+            _chargeStopDate = chargeStopDate;
+        }
+
+        public bool IsValid =>
+            _resolution is not Resolution.P1M ||
+            _pointsIntervalEndDate == _chargeStopDate ||
+            IsFirstLocalDayInMonth(_pointsIntervalEndDate);
+
+        public ValidationRuleIdentifier ValidationRuleIdentifier =>
+            ValidationRuleIdentifier.MonthlyPriceSeriesEndDateMustBeFirstOfMonthOrEqualChargeStopDate;
+
+        private static bool IsFirstLocalDayInMonth(Instant priceEndDate)
+        {
+            return priceEndDate != InstantExtensions.GetEndDefault() &&
+                   priceEndDate.Plus(Duration.FromDays(1)).InUtc().Day == 1;
+        }
+    }
+}
