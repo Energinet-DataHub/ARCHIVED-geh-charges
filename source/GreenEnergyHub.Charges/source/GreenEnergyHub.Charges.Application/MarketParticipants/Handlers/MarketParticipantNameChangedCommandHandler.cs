016a6c4d9120c913dddca75ed0596d962db7a243
Mads Skipper <2316684+Mech0z@users.noreply.github.com>
2022-10-21 11:27:04 +0200

    feat: add consume ActorNameChanged event
    This changes the name of the Market Participant based on the received event
    
    #1732

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.Application/MarketParticipants/Handlers/MarketParticipantNameChangedCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using GreenEnergyHub.Charges.Domain.Dtos.Events;
+using GreenEnergyHub.Charges.Domain.MarketParticipants;
+
+namespace GreenEnergyHub.Charges.Application.MarketParticipants.Handlers
+{
+    public class MarketParticipantNameChangedCommandHandler : IMarketParticipantNameChangedCommandHandler
+    {
+        private readonly IMarketParticipantRepository _marketParticipantRepository;
+
+        public MarketParticipantNameChangedCommandHandler(
+            IMarketParticipantRepository marketParticipantRepository)
+        {
+            _marketParticipantRepository = marketParticipantRepository;
+        }
+
+        public async Task HandleAsync(MarketParticipantNameChangedCommand command)
+        {
+            var marketParticipant =
+                await _marketParticipantRepository.GetByActorIdAsync(command.ActorId).ConfigureAwait(false);
+
+            if (marketParticipant == null)
+            {
+                throw new InvalidOperationException(
+                    $"Could not update name. Market participant does not exist with id {command.ActorId}");
+            }
+
+            marketParticipant.UpdateName(command.Name);
+        }
+    }
+}
