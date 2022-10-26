b31c384cb59929b5758ad38e567785e7c15657d8
Henrik Sommer <XHESO@energinet.dk>
2022-10-26 09:51:40 +0200

    MessageType and MessageDateTime should not have default values

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202210251454 Add MessageType and MessageDateTime columns to ChargeMessage table.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202210251454 Add MessageType and MessageDateTime columns to ChargeMessage table.sql
@@ -6,3 +6,7 @@ ALTER TABLE Charges.ChargeMessage
 ADD [MessageType] [int] not null default 0,
     [MessageDateTime] [datetime2](7) NOT NULL default getdate()
 GO
+
+ALTER TABLE Charges.ChargeMessage ALTER COLUMN [MessageType] [int] not null
+ALTER TABLE Charges.ChargeMessage ALTER COLUMN [MessageDateTime] [datetime2](7) NOT NULL
+GO
