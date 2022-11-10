2f7382aca87b01145cb7dc9812a131626f532e95
Henrik Sommer <XHESO@energinet.dk>
2022-11-10 17:55:09 +0100

    Add system versioning to ChargePeriod

---
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Model/202211101708 Add system verioning for ChargePeriod.sql
@@ -0,0 +1,13 @@
+﻿------------------------------------------------------------------------------------------------------------------------
+-- Add System versioning to ChargePeriod table
+------------------------------------------------------------------------------------------------------------------------
+
+ALTER TABLE [Charges].[ChargePeriod] ADD [ValidFrom] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+ALTER TABLE [Charges].[ChargePeriod] ADD [ValidTo] datetime2 NOT NULL DEFAULT '9999-12-31T23:59:59.9999999';
+GO
+
+ALTER TABLE [Charges].[ChargePeriod] ADD PERIOD FOR SYSTEM_TIME ([ValidFrom], [ValidTo])
+ALTER TABLE [Charges].[ChargePeriod] ALTER COLUMN [ValidFrom] ADD HIDDEN
+ALTER TABLE [Charges].[ChargePeriod] ALTER COLUMN [ValidTo] ADD HIDDEN
+DECLARE @historyTableSchema sysname = SCHEMA_NAME()
+EXEC(N'ALTER TABLE [Charges].[ChargePeriod] SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [' + @historyTableSchema + '].[ChargePeriodHistory]))')
