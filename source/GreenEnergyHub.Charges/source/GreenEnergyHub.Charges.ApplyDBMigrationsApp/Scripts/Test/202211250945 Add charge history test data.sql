efa73048fb7237ea0969c032bc9af92d5838fb3a
Peter Tandrup <PTA@energinet.dk>
2022-11-25 14:19:21 +0100

    Remove duplication in test data script

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Test/202211250945 Add charge history test data.sql
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.ApplyDBMigrationsApp/Scripts/Test/202211250945 Add charge history test data.sql
@@ -2,9 +2,13 @@
 -- Add charge history test data
 ------------------------------------------------------------------------------------------------------------------------
 
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TestTar001', 3, '8100000000030', 'Name', 'Description', 2, 0, 0, 1, '2021-12-31 23:00:00', '2022-12-31 23:00:00', '2021-10-31 23:00:00')                                        
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TariffA', 3, '8100000000030', 'Name A0', 'Description', 2, 0, 0, 1, '2022-01-31 23:00:00', '9999-12-31 23:59:59', '2022-01-01 07:02:00')
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TariffA', 3, '8100000000030', 'Name A1', 'Description', 2, 0, 0, 1, '2022-01-31 23:00:00', '9999-12-31 23:59:59', '2022-01-01 07:15:00')
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TariffA', 3, '8100000000030', 'Name B', 'Description', 2, 0, 0, 1, '2022-03-31 22:00:00', '9999-12-31 23:59:59', '2022-01-03 21:00:00')
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TariffA', 3, '8100000000030', 'Name A2', 'Description', 2, 0, 0, 1, '2022-01-31 23:00:00', '2022-03-31 22:00:00', '2022-01-04 08:00:00')
-INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TariffA', 3, '8100000000030', 'Name future', 'Description', 2, 0, 0, 1, '2023-12-31 23:00:00', '2022-12-31 23:00:00', '2022-01-05 04:01:00')
+DECLARE @chargeOwner nvarchar(255) = '8100000000030';
+DECLARE @endDefaultDateTime nvarchar(20) = '9999-12-31 23:59:59';
+DECLARE @tariffA nvarchar(35) = 'TariffA'
+
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), 'TestTar001', 3, @chargeOwner, 'Name', 'Description', 2, 0, 0, 1, '2021-12-31 23:00:00', '2022-12-31 23:00:00', '2021-10-31 23:00:00')                                        
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), @tariffA, 3, @chargeOwner, 'Name A0', 'Description A0', 2, 0, 0, 1, '2022-01-31 23:00:00', @endDefaultDateTime, '2022-01-01 07:02:00')
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), @tariffA, 3, @chargeOwner, 'Name A1', 'Description A1', 2, 0, 0, 1, '2022-01-31 23:00:00', @endDefaultDateTime, '2022-01-01 07:15:00')
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), @tariffA, 3, @chargeOwner, 'Name B', 'Description B', 2, 0, 0, 1, '2022-03-31 22:00:00', @endDefaultDateTime, '2022-01-03 21:00:00')
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), @tariffA, 3, @chargeOwner, 'Name A2', 'Description A2', 2, 0, 0, 1, '2022-01-31 23:00:00', '2022-03-31 22:00:00', '2022-01-04 08:00:00')
+INSERT INTO [ChargesQuery].[ChargeHistory] VALUES (NEWID(), @tariffA, 3, @chargeOwner, 'Name future', 'Description future', 2, 0, 0, 1, '2023-12-31 23:00:00', '2022-12-31 23:00:00', '2022-01-05 04:01:00')
