45ef346762d9110553e337b877fe7adc15f333a5
Alexei Mihalchuk <AIU@energinet.dk>
2022-12-05 09:50:12 +0100

    Update test to specify required env. variables.

--- source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/Fixtures/WebApi/ChargesWebApiFixture.cs
+++ source/GreenEnergyHub.Charges/source/GreenEnergyHub.Charges.IntegrationTest.Core/Fixtures/WebApi/ChargesWebApiFixture.cs
@@ -65,9 +65,9 @@ namespace GreenEnergyHub.Charges.IntegrationTest.Core.Fixtures.WebApi
                 $"CONNECTIONSTRINGS:{EnvironmentSettingNames.ChargeDbConnectionString}",
                 DatabaseManager.ConnectionString);
 
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.ExternalOpenIdUrl, "mocked");
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.InternalOpenIdUrl, "mocked");
-            Environment.SetEnvironmentVariable(EnvironmentSettingNames.BackendAppId, "mocked");
+            Environment.SetEnvironmentVariable(EnvironmentSettingNames.ExternalOpenIdUrl, "disabled");
+            Environment.SetEnvironmentVariable(EnvironmentSettingNames.InternalOpenIdUrl, "disabled");
+            Environment.SetEnvironmentVariable(EnvironmentSettingNames.BackendAppId, "disabled");
             Environment.SetEnvironmentVariable(EnvironmentSettingNames.LocalTimeZoneName, "Europe/Copenhagen");
 
             Environment.SetEnvironmentVariable(EnvironmentSettingNames.DataHubSenderConnectionString, ServiceBusResourceProvider.ConnectionString);
